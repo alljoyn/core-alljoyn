a5d4828b8c6fe08673d5301d32f8f939cdc42303
Tony Wallace <awallace@qce.qualcomm.com>
2014-01-17 15:25:20 -0800

    Copyright year 2014
    Change-Id: Id8e82a7109419ee6bad42f68da092ea1475b4bb1
    Signed-off-by: Tony Wallace <awallace@qce.qualcomm.com>

--- build_core/tools/scons/javadoc.py
+++ build_core/tools/scons/javadoc.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 - 2013, AllSeen Alliance. All rights reserved.
+# Copyright (c) 2010 - 2014, AllSeen Alliance. All rights reserved.
 #
 #    Permission to use, copy, modify, and/or distribute this software for any
 #    purpose with or without fee is hereby granted, provided that the above
@@ -81,7 +81,7 @@ def javadoc_generator(source, target, env, for_signature):
    try:
       copyright = env['PROJECT_COPYRIGHT']
    except KeyError:
-      copyright = "Copyright &copy; 2010-2013 AllSeen Alliance.<br/><b>THIS DOCUMENT AND ALL INFORMATION CONTAIN HEREIN ARE PROVIDED ON AN \"AS-IS\" BASIS WITHOUT WARRANTY OF ANY KIND</b>.<br/><b>MAY CONTAIN U.S. AND INTERNATIONAL EXPORT CONTROLLED INFORMATION</b>"
+      copyright = "Copyright &copy; 2010-2014 AllSeen Alliance.<br/><b>THIS DOCUMENT AND ALL INFORMATION CONTAIN HEREIN ARE PROVIDED ON AN \"AS-IS\" BASIS WITHOUT WARRANTY OF ANY KIND</b>.<br/><b>MAY CONTAIN U.S. AND INTERNATIONAL EXPORT CONTROLLED INFORMATION</b>"
    javadoc_bottom = '-bottom \"' + "<small>%s %s ($(%s$))<br/>%s<br/></small>" % (env['PROJECT_LONG_NAME'], env['PROJECT_NUMBER'], datetime.now().strftime('%a %b %d %H:%M:%S %Y'), copyright) + '\"'
    javadoc_overview = ''
    for s in source:
