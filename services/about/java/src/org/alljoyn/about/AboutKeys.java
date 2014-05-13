acea919ebacb29024debebdd81161aa092bcb01d
George Nash <georgen@qce.qualcomm.com>
2014-05-12 17:33:09 -0700

    ASACORE-188 Fixed the SupportUrl About key string
    The SupportUrl About key string used the wrong capitalization.
    
    This also adds the initial unit tests that were added when the
    error was found.
    
    Change-Id: Ib93a4ef3ae17a90b25b89535e1c626411c370826
    Signed-off-by: George Nash <georgen@qce.qualcomm.com>

--- services/about/java/src/org/alljoyn/about/AboutKeys.java
+++ services/about/java/src/org/alljoyn/about/AboutKeys.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2013, AllSeen Alliance. All rights reserved.
+ * Copyright (c) 2013-2014, AllSeen Alliance. All rights reserved.
  *
  *    Permission to use, copy, modify, and/or distribute this software for any
  *    purpose with or without fee is hereby granted, provided that the above
@@ -85,7 +85,7 @@ public interface AboutKeys
 	/**
 	 * Optional. The support URL to be populated by device OEM
 	 */
-	public final static String ABOUT_SUPPORT_URL = "SupportURL"; 
+	public final static String ABOUT_SUPPORT_URL = "SupportUrl"; 
 	
 	/**
 	 * Required. This field returns the list of supported languages by the device. 
