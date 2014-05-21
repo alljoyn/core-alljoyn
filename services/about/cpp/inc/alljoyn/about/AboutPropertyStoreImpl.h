alljoyn/core-alljoyn

commit d32c2b1eabcc0dd0623acc4170b29e673334a754

Add back in deprecated setDeviceName that does not specify a language.
When the language is not specified, the default language is assumed.

Change-Id: I2369046af9f7a8832b715147ef9cebe38e4ef06b
Signed-off-by: Todd Malsbary <tmalsbar@qce.qualcomm.com>
Signed-off-by: George Nash <georgen@qce.qualcomm.com>

services/about/cpp/inc/alljoyn/about/AboutPropertyStoreImpl.h
@@ -20,6 +20,7 @@
 #include <stdio.h>
 #include <iostream>
 
+#include <qcc/platform.h>
 #include <alljoyn/about/AboutServiceApi.h>
 #include <alljoyn/about/PropertyStore.h>
 #include <alljoyn/about/PropertyStoreProperty.h>
@@ -116,6 +117,19 @@ class AboutPropertyStoreImpl : public PropertyStore {
      */
     QStatus setDeviceId(qcc::String const& deviceId, bool isPublic = true, bool isWritable = false, bool isAnnouncable = true);
 
+    /**
+     * @deprecated
+     *
+     * setDeviceName
+     * @param[out] deviceName
+     * @param[out] isPublic
+     * @param[out] isWritable
+     * @param[out] isAnnouncable
+     * @return QStatus.
+     */
+    QCC_DEPRECATED(QStatus setDeviceName(qcc::String const & deviceName, bool isPublic = true, bool isWritable = true, bool isAnnouncable = true)) {
+        return setDeviceName(deviceName, "", isPublic, isWritable, isAnnouncable);
+    }
     /**
      * setDeviceName
      * @param[out] deviceName

services/about/cpp/src/AboutPropertyStoreImpl.cc
@@ -205,9 +205,23 @@ PropertyStoreProperty* AboutPropertyStoreImpl::getProperty(PropertyStoreKey prop
 
 PropertyStoreProperty* AboutPropertyStoreImpl::getProperty(PropertyStoreKey propertyKey, qcc::String const& language)
 {
+    PropertyMap::iterator defaultLang = m_Properties.find(DEFAULT_LANG);
+    qcc::String defaultLanguage = "";
+    if (defaultLang != m_Properties.end()) {
+        char* tempdefLang;
+        defaultLang->second.getPropertyValue().Get("s", &tempdefLang);
+        defaultLanguage.assign(tempdefLang);
+    }
+
     std::pair<PropertyMap::iterator, PropertyMap::iterator> iter = m_Properties.equal_range(propertyKey);
     for (PropertyMap::iterator it = iter.first; it != iter.second; it++) {
-        if (it->second.getLanguage().compare(language) == 0) {
+        /*
+         * The first check is solely for backwards compatibility with the
+         * deprecated setDeviceName.  An empty language string implies the
+         * default language for that case.
+         */
+        if ((it->second.getLanguage().empty() && (language == defaultLanguage)) ||
+            (it->second.getLanguage().compare(language) == 0)) {
             return &it->second;
         }
     }

services/about/cpp/unit_test/PropertyStoreImplTest.cc
@@ -135,7 +135,7 @@ TEST(PropertyStoreImplTest, setAppId)
     status = arg.Get("ay", &num, &appIdBuffer);
     EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
 
-    ASSERT_EQ(16, num);
+    ASSERT_EQ(16U, num);
     for (size_t i = 0; i < num - 1; ++i) {
         EXPECT_EQ(i, appIdBuffer[i]);
     }
@@ -235,7 +235,7 @@ TEST(PropertyStoreImplTest, setSupportedLangs)
     size_t num;
     status = arg.Get("as", &num, &out);
     EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
-    ASSERT_EQ(3, num);
+    ASSERT_EQ(3U, num);
     const char* lang;
     status = out[0].Get("s", &lang);
     EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
@@ -532,7 +532,7 @@ TEST(PropertyStoreImplTest, ReadAll_announce)
     EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
     ASSERT_EQ(16u, appIdNum);
     for (size_t i = 0; i < appIdNum - 1; ++i) {
-        EXPECT_EQ(i, appIdOut[i]);
+        EXPECT_EQ((int8_t)i, appIdOut[i]);
     }
     EXPECT_EQ(12, appIdOut[appIdNum - 1]);
 
@@ -679,7 +679,7 @@ TEST(PropertyStoreImplTest, ReadAll_read)
     EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
     ASSERT_EQ(16u, appIdNum);
     for (size_t i = 0; i < appIdNum - 1; ++i) {
-        EXPECT_EQ(i, appIdOut[i]);
+        EXPECT_EQ((int8_t)i, appIdOut[i]);
     }
     EXPECT_EQ(12, appIdOut[appIdNum - 1]);
 
@@ -731,7 +731,7 @@ TEST(PropertyStoreImplTest, ReadAll_read)
     size_t num;
     status = args->Get("as", &num, &languageArg);
     EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
-    ASSERT_EQ(3, num);
+    ASSERT_EQ(3U, num);
     const char* lang;
     status = languageArg[0].Get("s", &lang);
     EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);

commit 4d11491c0f3af5cd01e5ed5b04641f4310db6d1b

ASACORE-214: setDeviceName with const char* not calling correct function.
C++ will happily convert a const char* to a bool before it considers
the user-defined qcc::String conversion when considering which
overloaded function to call.

This is a regression introduced in
d32c2b1eabcc0dd0623acc4170b29e673334a754.

Change-Id: I464c096d7df3275b58e5b0d0e8ea434e0cfdc558
Signed-off-by: Todd Malsbary <tmalsbar@qce.qualcomm.com>

services/about/cpp/inc/alljoyn/about/AboutPropertyStoreImpl.h
@@ -141,6 +141,16 @@ class AboutPropertyStoreImpl : public PropertyStore {
      */
     QStatus setDeviceName(qcc::String const& deviceName, qcc::String const& language, bool isPublic = true, bool isWritable = true, bool isAnnouncable = true);
     /**
+     * setDeviceName
+     * @param[out] deviceName
+     * @param[out] language
+     * @param[out] isPublic
+     * @param[out] isWritable
+     * @param[out] isAnnouncable
+     * @return QStatus.
+     */
+    QStatus setDeviceName(qcc::String const& deviceName, const char* language, bool isPublic = true, bool isWritable = true, bool isAnnouncable = true);
+    /**
      * setAppId
      * @param[out] appId
      * @param[out] isPublic

services/about/cpp/src/AboutPropertyStoreImpl.cc
@@ -304,6 +304,11 @@ QStatus AboutPropertyStoreImpl::setDeviceName(const qcc::String& deviceName, qcc
     return setProperty(DEVICE_NAME, deviceName, language, isPublic, isWritable, isAnnouncable);
 }
 
+QStatus AboutPropertyStoreImpl::setDeviceName(const qcc::String& deviceName, const char* language, bool isPublic, bool isWritable, bool isAnnouncable)
+{
+    return setProperty(DEVICE_NAME, deviceName, language, isPublic, isWritable, isAnnouncable);
+}
+
 QStatus AboutPropertyStoreImpl::setAppName(const qcc::String& appName, bool isPublic, bool isWritable, bool isAnnouncable)
 {
     return setProperty(APP_NAME, appName, isPublic, isWritable, isAnnouncable);

services/about/cpp/unit_test/PropertyStoreImplTest.cc
@@ -100,7 +100,7 @@ TEST(PropertyStoreImplTest, setDeviceName)
     QStatus status;
     AboutPropertyStoreImpl ps;
 
-    status = ps.setDeviceName("MyDeviceName");
+    status = ps.setDeviceName("MyDeviceName", "en");
     EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
 
     PropertyStoreProperty* psp = ps.getProperty(DEVICE_NAME);
@@ -454,7 +454,7 @@ TEST(PropertyStoreImplTest, ReadAll_announce)
 
     status = ps.setDeviceId("1231232145667745675477");
     EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
-    status = ps.setDeviceName("MyDeviceName");
+    status = ps.setDeviceName("MyDeviceName", "en");
     EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
     status = ps.setAppId("000102030405060708090A0B0C0D0E0C");
     EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
@@ -587,7 +587,7 @@ TEST(PropertyStoreImplTest, ReadAll_read)
 
     status = ps.setDeviceId("1231232145667745675477");
     EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
-    status = ps.setDeviceName("MyDeviceName");
+    status = ps.setDeviceName("MyDeviceName", "en");
     EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
     status = ps.setAppId("000102030405060708090A0B0C0D0E0C");
     EXPECT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);