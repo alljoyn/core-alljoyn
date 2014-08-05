4bac1f2a5efe42f5a8617ed1231a930d30e36318
Craig Dowell <cdowell@qce.qualcomm.com>
2014-08-05 15:53:13 -0700

    Add Condition Variable support for Posix and corresponding unit test
    Change-Id: I60744ab13f5d0158f95b6057e4e661804840c84c
    Signed-off-by: Craig Dowell <cdowell@qce.qualcomm.com>

---
+++ common/inc/qcc/Condition.h
@@ -0,0 +1,37 @@
+/**
+ * @file
+ *
+ * Declare a class that abstracts a Mesa Semantics Condition Variable.
+ */
+
+/******************************************************************************
+ * Copyright (c) 2014, AllSeen Alliance. All rights reserved.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for any
+ *    purpose with or without fee is hereby granted, provided that the above
+ *    copyright notice and this permission notice appear in all copies.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ ******************************************************************************/
+#ifndef _QCC_CONDITION_H
+#define _QCC_CONDITION_H
+
+#include <qcc/platform.h>
+
+#if defined(QCC_OS_GROUP_POSIX)
+#include <qcc/posix/Condition.h>
+#elif defined(QCC_OS_GROUP_WINDOWS)
+#include <qcc/windows/Condition.h>
+#elif defined(QCC_OS_GROUP_WINRT)
+#include <qcc/winrt/Condition.h>
+#else
+#error No OS GROUP defined.
+#endif
+
+#endif
