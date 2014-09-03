3079c12be59777f55f3118ddd84cb1f4269ffb7d
Meggan Ehret <meggan@qeo.org>
2014-09-03 15:27:37 +0200

    Added a first draft Autopinger (still work in progress) feature to core.
    This used by the security manager to monitor active applications.
    The Autopinger is a utility on top of the Ping feature. This can be used by other application as well,
    so we propose to make this part of core.
    
    Change-Id: I351a768e2862808ae34f79f6a2b1a2b3d880021f
    Signed-off-by: Meggan Ehret <meggan@qeo.org>

---
+++ alljoyn_core/inc/alljoyn/AutoPinger.h
@@ -0,0 +1,158 @@
+/**
+ * @file
+ *
+ * AutoPinger
+ */
+
+/******************************************************************************
+ * Copyright (c) 2014, AllSeen Alliance. All rights reserved.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for any
+ *    purpose with or without fee is hereby granted, provided that the above
+ *    copyright notice and this permission notice appear in all copies.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ ******************************************************************************/
+
+#ifndef AUTOPINGER_H_
+#define AUTOPINGER_H_
+
+#ifndef __cplusplus
+#error Only include AutoPinger.h in C++ code.
+#endif
+
+#include <map>
+#include <vector>
+#include <memory>
+#include <qcc/Timer.h>
+#include <qcc/String.h>
+#include <qcc/Mutex.h>
+#include <qcc/Debug.h>
+#include <alljoyn/Status.h>
+#include <alljoyn/PingListener.h>
+
+#define QCC_MODULE "AUTOPINGER"
+
+namespace ajn {
+/// @cond ALLJOYN_DEV
+/** @internal Forward references */
+class BusAttachment;
+struct PingGroup;
+/// @endcond
+
+/**
+ * AutoPinger class
+ */
+class AutoPinger : public qcc::AlarmListener {
+  public:
+
+
+    /**
+     * Create instance of autopinger
+     *
+     */
+    AutoPinger(ajn::BusAttachment& busAttachment);
+
+    /**
+     * Destructor
+     */
+    virtual ~AutoPinger();
+
+    /**
+     * Pause all ping actions
+     */
+    void Pause();
+
+    /**
+     * Resume ping actions
+     */
+    void Resume();
+
+    /**
+     * Define new ping group
+     *
+     * @param  group Ping group name
+     * @param  listener Listener called when a change was detected in the reachability of a destination
+     * @param  pingInterval Ping interval in seconds
+     */
+    void AddPingGroup(const qcc::String& group, PingListener& listener, uint32_t pingInterval = 5);
+
+    /**
+     * Remove complete ping group, including all destinations
+     *
+     * @param  group Ping group name
+     */
+    void RemovePingGroup(const qcc::String& group);
+
+    /**
+     * Set ping interval of the specified group
+     *
+     * @param  group Ping group name
+     * @param  pingInterval Ping interval in seconds
+     * @return QStatus ER_OK: Interval updated
+     *                 ER_FAIL: group did not exist
+     */
+    QStatus SetPingInterval(const qcc::String& group, uint32_t pingInterval);
+
+    /**
+     * Add a destination to the specified ping group
+     * Destinations are refcounted and must be removed N times if they were added N times
+     *
+     * @param  group Ping group name
+     * @param  destination Destination name to be pinged
+     * @return QStatus ER_OK: destination added
+     *                 ER_FAIL: group did not exist
+     */
+    QStatus AddDestination(const qcc::String& group, const qcc::String& destination);
+
+    /**
+     * Remove a destination from the specified ping group
+     * This will lower the refcount by one and only remove the destination when the refcount reaches zero
+     *
+     * @param  group Ping group name
+     * @param  destination Destination name to be removed
+     * @param  removeAll Rather than decrementing the refcount by one, set refcount to zero and remove
+     * @return QStatus ER_OK: destination removed or was not present
+     *                 ER_FAIL: group did not exist
+     */
+    QStatus RemoveDestination(const qcc::String& group, const qcc::String& destination, bool removeAll = false);
+
+  private:
+    class AutoPingAsyncCB;
+    friend class AutoPingAsyncCB;
+    friend struct Destination;
+    friend class PingAsyncContext;
+
+    enum class PingState :
+        int8_t {
+        UNKNOWN,
+        LOST,
+        AVAILABLE
+    };
+
+    AutoPinger(const AutoPinger&);
+    void operator=(const AutoPinger&);
+
+    bool UpdatePingStateOfDestination(const qcc::String& group, const qcc::String& destination, const AutoPinger::PingState state);
+    void PingGroupDestinations(const qcc::String& group);
+    bool IsRunning();
+    void AlarmTriggered(const qcc::Alarm& alarm, QStatus reason);
+
+    qcc::Timer timer; /* Single Timerthread */
+    ajn::BusAttachment& busAttachment;
+    qcc::Mutex pingerMutex;
+    //std::map<qcc::String, std::unique_ptr<PingGroup> > pingGroups; /* Temporarily disable unique_ptr because not available on all platforms */
+    std::map<qcc::String, PingGroup*> pingGroups;
+
+    bool pausing;
+    AutoPingAsyncCB* pingCallback;
+};
+}
+#undef QCC_MODULE
+#endif /* AUTOPINGER_H_ */
---
+++ alljoyn_core/unit_test/AutoPingerTest.cc
@@ -0,0 +1,180 @@
+/******************************************************************************
+ * Copyright (c) 2012-2014, AllSeen Alliance. All rights reserved.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for any
+ *    purpose with or without fee is hereby granted, provided that the above
+ *    copyright notice and this permission notice appear in all copies.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ ******************************************************************************/
+#include <alljoyn/BusAttachment.h>
+#include <alljoyn/AutoPinger.h>
+#include <gtest/gtest.h>
+
+using namespace ajn;
+
+class AutoPingerTest : public testing::Test {
+  public:
+    BusAttachment bus;
+    AutoPinger autoPinger;
+
+    AutoPingerTest() : bus("BusAttachmentTest", false), autoPinger(bus) { };
+
+    virtual void SetUp() {
+        QStatus status = ER_OK;
+        status = bus.Start();
+        ASSERT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
+        ASSERT_FALSE(bus.IsConnected());
+        status = bus.Connect();
+        ASSERT_EQ(ER_OK, status) << "  Actual Status: " << QCC_StatusText(status);
+        ASSERT_TRUE(bus.IsConnected());
+    }
+
+    virtual void TearDown() {
+        bus.Stop();
+        bus.Join();
+    }
+
+};
+
+
+class TestPingListener : public PingListener {
+
+  private:
+    std::set<qcc::String> found;
+    qcc::Mutex foundmutex;
+
+    std::set<qcc::String> lost;
+    qcc::Mutex lostmutex;
+
+    virtual void DestinationLost(const qcc::String& group, const qcc::String& destination) {
+
+        std::cout << "on lost " << destination  << std::endl;
+        lostmutex.Lock();
+        lost.insert(destination);
+        lostmutex.Unlock();
+
+    }
+
+    virtual void DestinationFound(const qcc::String& group, const qcc::String& destination) {
+
+        std::cout << "on found " << destination  << std::endl;
+        foundmutex.Lock();
+        found.insert(destination);
+        foundmutex.Unlock();
+
+    }
+
+  public:
+    void WaitUntilFound(const qcc::String& destination) {
+
+        std::cout << "Wait until we see " << destination << std::endl;
+        foundmutex.Lock();
+        while (found.find(destination) == found.end()) {
+            foundmutex.Unlock();
+            qcc::Sleep(10);
+            foundmutex.Lock();
+        }
+        foundmutex.Unlock();
+
+    }
+
+    void WaitUntilLost(const qcc::String& destination) {
+
+        std::cout << "Wait until " << destination << "is gone " << std::endl;
+        lostmutex.Lock();
+        while (lost.find(destination) == lost.end()) {
+            lostmutex.Unlock();
+            qcc::Sleep(10);
+            lostmutex.Lock();
+        }
+        lostmutex.Unlock();
+
+    }
+};
+
+TEST_F(AutoPingerTest, Basic) {
+
+    BusAttachment bus2("app", false);
+    EXPECT_EQ(ER_OK, bus2.Start());
+    EXPECT_EQ(ER_OK, bus2.Connect());
+
+    TestPingListener tpl;
+
+    autoPinger.AddPingGroup("testgroup", tpl, 1);
+    qcc::String uniqueName = bus2.GetUniqueName();
+    EXPECT_EQ(ER_FAIL, autoPinger.AddDestination("badgroup", uniqueName));
+    EXPECT_EQ(ER_OK, autoPinger.AddDestination("testgroup", uniqueName));
+    EXPECT_EQ(ER_OK, autoPinger.AddDestination("testgroup", uniqueName));
+
+    tpl.WaitUntilFound(uniqueName);
+    bus2.Disconnect();
+    tpl.WaitUntilLost(uniqueName);
+
+    autoPinger.RemoveDestination("badgroup", uniqueName);
+    autoPinger.RemoveDestination("testgroup", uniqueName);
+    autoPinger.RemoveDestination("testgroup", uniqueName);
+
+    EXPECT_EQ(ER_FAIL, autoPinger.SetPingInterval("badgroup", 2));
+    EXPECT_EQ(ER_OK, autoPinger.SetPingInterval("testgroup", 2)); /* no real test on updated interval */
+
+
+    autoPinger.Pause();
+    autoPinger.Pause();
+    autoPinger.Resume();
+    autoPinger.Resume();
+
+    bus2.Connect();
+
+    uniqueName = bus2.GetUniqueName();
+    EXPECT_EQ(ER_OK, autoPinger.AddDestination("testgroup", uniqueName));
+    tpl.WaitUntilFound(uniqueName);
+
+    autoPinger.RemovePingGroup("badgroup");
+    autoPinger.RemovePingGroup("testgroup");
+
+}
+
+
+TEST_F(AutoPingerTest, Multibus) {
+
+    const int G = 2;
+    TestPingListener tpl[G];
+    qcc::String groupNames[G] = { "evengroup", "oddgroup" };
+
+    for (size_t i = 0; i < G; ++i) {
+        autoPinger.AddPingGroup(groupNames[i], tpl[i], 1);
+    }
+
+    const int N = 10;
+    std::vector<BusAttachment*> busses(N);
+    std::vector<qcc::String> uniqueNames(N);
+    for (size_t i = 0; i < busses.size(); ++i) {
+        busses[i] = new BusAttachment("test");
+        busses[i]->Start();
+        busses[i]->Connect();
+        uniqueNames[i] = busses[i]->GetUniqueName();
+        autoPinger.AddDestination(groupNames[i % G], uniqueNames[i]);
+    }
+
+    for (size_t i = 0; i < busses.size(); ++i) {
+        tpl[i % G].WaitUntilFound(uniqueNames[i]);
+        busses[i]->Disconnect();
+    }
+
+    for (size_t i = 0; i < busses.size(); ++i) {
+        tpl[i % G].WaitUntilLost(uniqueNames[i]);
+    }
+
+    for (size_t i = 0; i < busses.size(); ++i) {
+        delete busses[i];
+    }
+
+}
+
