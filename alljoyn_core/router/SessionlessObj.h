183b4ab6b083f04a3fd2710cf654ccfefb3e1d4f
Todd Malsbary <tmalsbar@qce.qualcomm.com>
2014-09-05 15:31:52 -0700

    ASACORE-915: Add implicit match rules for Announce signals to receive them when interface of interest is removed from the objectDescriptions arg.
    Add some extra bookkeeping in InternalAnnounceHandler.
    Add unit test.
    
    Change-Id: I7ff6d76c2bcf79d22081b661b09fa1fb5ecdacb6
    Signed-off-by: Todd Malsbary <tmalsbar@qce.qualcomm.com>
    Signed-off-by: Meggan Ehret <meggan@qeo.org>

--- alljoyn_core/router/SessionlessObj.h
+++ alljoyn_core/router/SessionlessObj.h
@@ -362,6 +362,9 @@ class SessionlessObj : public BusObject, public NameListener, public SessionList
     /** Find the remote cache work item with the matching session ID */
     RemoteCaches::iterator FindRemoteCache(SessionId sid);
 
+    /** Erase info associated with the remote cache */
+    void EraseRemoteCache(RemoteCaches::iterator cit);
+
     qcc::Mutex lock;            /**< Mutex that protects this object's data structures */
     uint32_t curChangeId;       /**< Change id assoc with current pushed signal(s) */
     bool isDiscoveryStarted;    /**< True when FindAdvetiseName is ongoing */
@@ -437,7 +440,8 @@ class SessionlessObj : public BusObject, public NameListener, public SessionList
      * @param[in] fromRulesId Beginning of rules ID range (inclusive)
      * @param[in] toRulesId End of rules ID range (exclusive)
      */
-    void SendMatchingThroughEndpoint(SessionId sid, Message msg, uint32_t fromRulesId, uint32_t toRulesId);
+    void SendMatchingThroughEndpoint(SessionId sid, Message msg, uint32_t fromRulesId, uint32_t toRulesId,
+                                     bool onlySendIfImplicit = false);
 
     /**
      * A match rule that includes a change ID for recording when it was entered
@@ -453,6 +457,68 @@ class SessionlessObj : public BusObject, public NameListener, public SessionList
 
     /** Rule iterator */
     typedef std::multimap<qcc::String, TimestampedRule>::iterator RuleIterator;
+
+    /**
+     * An implicit match rule that includes a list of explicit rules that it is
+     * associated with.
+     */
+    struct ImplicitRule : public Rule {
+        ImplicitRule(const Rule& rule, const RuleIterator& explicitRule) : Rule(rule) { explicitRules.push_back(explicitRule); }
+        std::vector<RuleIterator> explicitRules;
+    };
+
+    /** List of implicit rules. */
+    std::vector<ImplicitRule> implicitRules;
+
+    /** Implicit rule iterator */
+    typedef std::vector<ImplicitRule>::iterator ImplicitRuleIterator;
+
+    /**
+     * Add an implicit, explicit rule entry.  Each implicit rule is associated
+     * with at least one explicit rule.
+     *
+     * @param[in] implicitRule the implicit rule
+     * @param[in] explicitRule the iterator of the explicit rule in rules
+     */
+    void AddImplicitRule(const Rule& implicitRule, const RuleIterator& explicitRule);
+
+    /**
+     * Remove explicit rules associated with an endpoint from the implicit
+     * rules.  When all explicit associations are removed, the implicit rule is
+     * removed.
+     *
+     * @param[in] epName the name of the endpoint
+     */
+    void RemoveImplicitRules(const qcc::String& epName);
+
+    /**
+     * Remove an explicit rule from the implicit, explicit rule entry.  When all
+     * explicit associations are removed, the implicit rule is removed.
+     *
+     * @param[in] explicitRule the iterator of the explicit rule in rules
+     */
+    void RemoveImplicitRules(const RuleIterator& explicitRule);
+
+    /**
+     * Remove implicit rules that have the sender value of the remote cache.
+     *
+     * @param[in] cache the remote cache
+     */
+    void RemoveImplicitRules(const RemoteCache& cache);
+
+    /**
+     * Returns true if the message matches any implicit rules associated with
+     * an endpoint.
+     *
+     * @param[in] epName the name of the endpoint
+     * @param[in] msg the Message to compare with the implicit rules
+     * @param[in] eraseIfMatch true to erase the implicit match rule that
+     *            matches
+     *
+     * @return true if the Message matches any implicit rules associated with
+     *              the endpoint
+     */
+    bool IsImplictMatch(const qcc::String& epName, Message& msg, bool eraseIfMatch = false);
 
     /*
      * Advertise or cancel the SL advertisements.
