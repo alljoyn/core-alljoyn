e2f1fdabd69665bed4fe533f659cda8458f7610a
Todd Malsbary <tmalsbar@qce.qualcomm.com>
2014-09-15 15:57:03 -0700

    ASACORE-921: Support <year>.<month>.<feature><optional bug-letter> version format.
    Change-Id: I875f82317ada446d5a77a7e34cebf9d6d2ec0950
    Signed-off-by: Todd Malsbary <tmalsbar@qce.qualcomm.com>

--- alljoyn_core/inc/alljoyn/version.h
+++ alljoyn_core/inc/alljoyn/version.h
@@ -4,7 +4,7 @@
  */
 
 /******************************************************************************
- * Copyright (c) 2010-2011, AllSeen Alliance. All rights reserved.
+ * Copyright (c) 2010-2011,2014 AllSeen Alliance. All rights reserved.
  *
  *    Permission to use, copy, modify, and/or distribute this software for any
  *    purpose with or without fee is hereby granted, provided that the above
@@ -24,15 +24,55 @@
 #include <qcc/platform.h>
 
 /** Macro to compute the version number into a single value. */
+#define GenerateNumericVersionValue(_year, _month, _feature, _bugfix) (((_year) << 24) | ((_month) << 16) | ((_feature) << 8) | (_bugfix))
+
+/** Macro to extract the year from unified version value. */
+#define GetVersionYear(_ver)    (((_ver) >> 24) & 0xff)
+
+/** Macro to extract the month from unified version value. */
+#define GetVersionMonth(_ver)   (((_ver) >> 16) & 0xff)
+
+/** Macro to extract the feature from unified version value. */
+#define GetVersionFeature(_ver) (((_ver) >>  8) & 0xff)
+
+/** Macro to extract the bugfix from unified version value. */
+#define GetVersionBugfix(_ver)  (((_ver) >>  0) & 0xff)
+
+/**
+ * Macro to compute the version number into a single value.
+ *
+ * @deprecated
+ *
+ * @see GenerateNumericVersionValue()
+ */
 #define GenerateVersionValue(_arch, _api, _rel) (((_arch) << 24) | ((_api) << 16) | (_rel))
 
-/** Macro to extract the architecture level from unified version value. */
+/**
+ * Macro to extract the architecture level from unified version value.
+ *
+ * @deprecated
+ *
+ * @see GetVersionYear()
+ */
 #define GetVersionArch(_ver) (((_ver) >> 24) & 0xff)
 
-/** Macro to extract the API level from unified version value. */
+/**
+ * Macro to extract the API level from unified version value.
+ *
+ * @deprecated
+ *
+ * @see GetVersionMonth()
+ */
 #define GetVersionAPILevel(_ver) (((_ver) >> 16) & 0xff)
 
-/** Macro to extract the release from unified version value. */
+/**
+ * Macro to extract the release from unified version value.
+ *
+ * @deprecated
+ *
+ * @see GetVersionFeature()
+ * @see GetVersionBugfix()
+ */
 #define GetVersionRelease(_ver) ((_ver) & 0xffff)
 
 namespace ajn {
