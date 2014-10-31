8e5b8c92c32dd38fd0cdae33ea64504194a81508
Nikhil Dabhade <ndabhade@qce.qualcomm.com>
2014-10-31 16:33:52 -0700

    ASACORE:1093 C++ unit tests for about failed to build on OSX with the new about rework
    The unit tests could not find operator << function which is a part of String.h as a global
    function but not a part of the qcc namespace. The solution was to declare and include String.h
    before we include other unit test framework header files
    
    Change-Id: I7c77e8bc50fbadc73ae1eebb38468f2e736cc768
    Signed-off-by: Nikhil Dabhade <ndabhade@qce.qualcomm.com>

--- alljoyn_core/unit_test/AboutObjTest.cc
+++ alljoyn_core/unit_test/AboutObjTest.cc
@@ -13,7 +13,8 @@
  *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  ******************************************************************************/
- #include <gtest/gtest.h>
+#include <qcc/String.h>
+#include <gtest/gtest.h>
 
 #include <alljoyn/AboutData.h>
 #include <alljoyn/AboutListener.h>
@@ -24,6 +25,7 @@
 
 #include <qcc/Thread.h>
 #include <qcc/GUID.h>
+
 
 /*
  * This test uses the GUID128 in multiple places to generate a random string.
