d952e63892d6f0710fc016716e42befd19c3268c
Dan Mihai <DMihai@microsoft.com>
2014-11-21 14:58:20 -0800

    ASACORE-1186 Add support for detecting idle RNs.
    Add ability to detect if all LN streams have been stopped a given
    period of time ago.
    
    Change-Id: Id69792f62a20955532ee057e4e2be1e480f4a1e6
    Signed-off-by: Dan Mihai <DMihai@microsoft.com>

--- common/inc/qcc/IODispatch.h
+++ common/inc/qcc/IODispatch.h
@@ -322,7 +322,23 @@ class IODispatch : public Thread, public AlarmListener {
      */
     virtual ThreadReturn STDCALL Run(void* arg);
 
+    /**
+     * Indicate whether the router node (current process) did not have any
+     * connected leaf nodes for a specified time.
+     * @param minTime          Minimum idle time in milliseconds.
+     * @return true if the router has been idle for at least that time, otherwise false.
+     */
+    static bool IsRouterIdle(uint64_t minTime);
+
   private:
+
+    /**
+     * Update the router idle information when a stream is starting or stopping.
+     * @param isStarting       false - if a stream is being stopped
+     *                         true - if a stream is being started
+     */
+    static void UpdateIdleInformation(bool isStarting);
+
     Timer timer;                                /* The timer used to add and process callbacks */
     Mutex lock;                                 /* Lock for mutual exclusion of dispatchEntries */
     std::map<Stream*, IODispatchEntry> dispatchEntries; /* map holding details of various streams registered with this IODispatch */
@@ -335,6 +351,8 @@ class IODispatch : public Thread, public AlarmListener {
      */
     bool crit;
     static int32_t iodispatchCnt;
+    static int32_t activeStreamsCnt;              /* Number of streams that have been started and not stopped yet */
+    static volatile uint64_t stopStreamTimestamp; /* Timestamp of the last stream stop, in milliseconds */
 };
 
 
