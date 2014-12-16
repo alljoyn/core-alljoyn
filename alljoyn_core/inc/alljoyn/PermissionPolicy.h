0bdac7c9db2205767c7ae6f433aa8b68b20016ff
Meggan Ehret <meggan@qeo.org>
2014-12-16 16:37:06 +0100

    Add equals operators on PermissionPolicy elements
    made the ToString functions const.
    
    Change-Id: I966c8b48c65f85a0106c07600c698da20da5d59d
    Signed-off-by: Meggan Ehret <meggan@qeo.org>

--- alljoyn_core/inc/alljoyn/PermissionPolicy.h
+++ alljoyn_core/inc/alljoyn/PermissionPolicy.h
@@ -136,7 +136,28 @@ class PermissionPolicy {
                 return mutualAuth;
             }
 
-            qcc::String ToString();
+            qcc::String ToString() const;
+
+            bool operator==(const Member& m) const
+            {
+                if (memberName != m.memberName) {
+                    return false;
+                }
+
+                if (memberType != m.memberType) {
+                    return false;
+                }
+
+                if (actionMask != m.actionMask) {
+                    return false;
+                }
+
+                if (mutualAuth != m.mutualAuth) {
+                    return false;
+                }
+
+                return true;
+            }
 
           private:
             qcc::String memberName;
@@ -216,7 +237,30 @@ class PermissionPolicy {
             return membersSize;
         }
 
-        qcc::String ToString();
+        qcc::String ToString() const;
+
+        bool operator==(const Rule& r) const
+        {
+            if (objPath != r.objPath) {
+                return false;
+            }
+
+            if (interfaceName != r.interfaceName) {
+                return false;
+            }
+
+            if (membersSize != r.membersSize) {
+                return false;
+            }
+
+            for (size_t i = 0; i < membersSize; i++) {
+                if (!(members[i] == r.members[i])) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
 
       private:
         qcc::String objPath;
@@ -326,7 +370,32 @@ class PermissionPolicy {
             return ID;
         }
 
-        qcc::String ToString();
+        qcc::String ToString() const;
+
+        bool operator==(const Peer& p) const
+        {
+            if (level != p.level) {
+                return false;
+            }
+
+            if (type != p.type) {
+                return false;
+            }
+
+            if (IDLen != p.IDLen) {
+                return false;
+            }
+
+            if (ID == NULL || p.ID == NULL) {
+                return ID == p.ID;
+            }
+
+            if (memcmp(ID, p.ID, IDLen) != 0) {
+                return false;
+            }
+
+            return true;
+        }
 
       private:
         PeerAuthLevel level;
@@ -410,7 +479,32 @@ class PermissionPolicy {
             return rules;
         }
 
-        qcc::String ToString();
+        qcc::String ToString() const;
+
+        bool operator==(const Term& t) const
+        {
+            if (peersSize != t.peersSize) {
+                return false;
+            }
+
+            for (size_t i = 0; i < peersSize; i++) {
+                if (!(peers[i] == t.peers[i])) {
+                    return false;
+                }
+            }
+
+            if (rulesSize != t.rulesSize) {
+                return false;
+            }
+
+            for (size_t i = 0; i < rulesSize; i++) {
+                if (!(rules[i] == t.rules[i])) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
 
       private:
         size_t peersSize;
@@ -559,7 +653,7 @@ class PermissionPolicy {
         return terms;
     }
 
-    qcc::String ToString();
+    qcc::String ToString() const;
 
     /**
      * Serialize the permission policy to a byte array.
