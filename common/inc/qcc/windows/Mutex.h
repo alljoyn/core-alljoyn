15b210b951c5a8ba95e0ea8606a23417ed540d08
Todd Malsbary <tmalsbar@qce.qualcomm.com>
2015-01-08 14:29:15 -0800

    ASACORE-1368: Remove private utility.h from public Mutex.h.
    Change-Id: I630a5804082377275dee7a2ca568a83a34ada996
    Signed-off-by: Todd Malsbary <tmalsbar@qce.qualcomm.com>

--- common/inc/qcc/windows/Mutex.h
+++ common/inc/qcc/windows/Mutex.h
@@ -5,7 +5,7 @@
  */
 
 /******************************************************************************
- * Copyright (c) 2009-2011, 2014, AllSeen Alliance. All rights reserved.
+ * Copyright (c) 2009-2011, 2014-2015, AllSeen Alliance. All rights reserved.
  *
  *    Permission to use, copy, modify, and/or distribute this software for any
  *    purpose with or without fee is hereby granted, provided that the above
@@ -25,7 +25,6 @@
 #include <qcc/platform.h>
 
 #include <qcc/String.h>
-#include <qcc/windows/utility.h>
 
 #include <Status.h>
 
