c7ddbba4e2db7ee4fa6610887f5989f1fd520837
Ry Jones <c_lawren@qce.qualcomm.com>
2015-01-21 14:13:48 -0800

    ASACORE-1413: Prep. Release 14.12a
    Change-Id: I1e5d6e1fbaee2a2d3aac00704c5b526063d70a40
    Signed-off-by: Ry Jones <c_lawren@qce.qualcomm.com>

--- alljoyn_js/jni/npapi/np.cc
+++ alljoyn_js/jni/npapi/np.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2014, AllSeen Alliance. All rights reserved.
+ * Copyright (c) 2011-2015, AllSeen Alliance. All rights reserved.
  *
  *    Permission to use, copy, modify, and/or distribute this software for any
  *    purpose with or without fee is hereby granted, provided that the above
@@ -98,7 +98,7 @@ NP_EXPORT(char*) NP_GetPluginVersion()
 {
     InitializeDebug();
     QCC_DbgPrintf(("%s", __FUNCTION__));
-    return const_cast<char*>("14.12.00");
+    return const_cast<char*>("14.12.00a");
 }
 
 NP_EXPORT(char*) NP_GetMIMEDescription()
