6854d89011b4a54d0b641eefbe927f034e35bbb3
Way Vadhanasin <wayakorn.vadhanasin@microsoft.com>
2015-01-28 12:23:33 -0800

    ASACORE-1354 Change AJ timer to use Windows Thread Pool
    AllJoyn timer could be improved on Windows platform by utilizing
    the OS's Thread Pool.
    
    Benefits include:
    - Better memory utilization (this change saves about 200KB, which
    is significant for mobile devices).
    - Improved performance especially on single-core. The issue outlined
    by ASACORE-1076 can be avoided.
    
    Change-Id: Ieb6bdb9d79d2963485bfb2bf4b309c6276cdf3ba
    Signed-off-by: Way Vadhanasin <wayvad@microsoft.com>

--- alljoyn_core/src/LocalTransport.cc
+++ alljoyn_core/src/LocalTransport.cc
@@ -6,7 +6,7 @@
  */
 
 /******************************************************************************
- * Copyright (c) 2009-2014, AllSeen Alliance. All rights reserved.
+ * Copyright (c) 2009-2015, AllSeen Alliance. All rights reserved.
  *
  *    Permission to use, copy, modify, and/or distribute this software for any
  *    purpose with or without fee is hereby granted, provided that the above
@@ -422,7 +422,7 @@ bool _LocalEndpoint::IsReentrantCall()
     if (!dispatcher) {
         return false;
     }
-    return dispatcher->ThreadHoldsLock();
+    return dispatcher->IsHoldingReentrantLock();
 
 }
 
