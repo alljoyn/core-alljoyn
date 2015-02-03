03593f8317bd2c895d6274f95b4bb97a170d4040
Meggan Ehret <meggan@qeo.org>
2015-02-03 10:52:19 +0100

    Introduction of Observer API
    Change-Id: I8aaf4c2f2a01599e421118b501b6f55f4c21042d
    Signed-off-by: Meggan Ehret <meggan@qeo.org>

---
+++ alljoyn_core/src/Observer.cc
@@ -0,0 +1,394 @@
+/******************************************************************************
+ * Copyright (c) 2015, AllSeen Alliance. All rights reserved.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for any
+ *    purpose with or without fee is hereby granted, provided that the above
+ *    copyright notice and this permission notice appear in all copies.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ ******************************************************************************/
+
+#include <set>
+
+#include <qcc/String.h>
+#include <qcc/Mutex.h>
+
+#include <alljoyn/Observer.h>
+#include <alljoyn/InterfaceDescription.h>
+
+#include "BusInternal.h"
+#include "ObserverInternal.h"
+#include "ObserverManager.h"
+
+#include <qcc/Debug.h>
+#define QCC_MODULE "OBSERVER"
+
+using namespace std;
+
+namespace ajn {
+
+Observer::Internal::Internal(BusAttachment& bus,
+                             Observer* observer,
+                             InterfaceSet mandatory,
+                             InterfaceSet optional) :
+    bus(bus),
+    observer(observer),
+    mandatory(mandatory),
+    optional(optional)
+{
+    ObserverManager& obsmgr = bus.GetInternal().GetObserverManager();
+    obsmgr.RegisterObserver(this);
+}
+
+Observer::Internal::~Internal()
+{
+}
+
+void Observer::Internal::Detach()
+{
+    UnregisterAllListeners();
+    observer = NULL;
+
+    ObserverManager& obsmgr = bus.GetInternal().GetObserverManager();
+    obsmgr.UnregisterObserver(this);
+}
+
+void Observer::Internal::RegisterListener(Observer::Listener& listener, bool triggerOnExisting)
+{
+    Observer::Listener* pListener = &listener;
+    WrappedListener* wListener = new WrappedListener(pListener, !triggerOnExisting);
+    ProtectedObserverListener protectedListener(wListener);
+    listenersLock.Lock(MUTEX_CONTEXT);
+    listeners.insert(protectedListener);
+    listenersLock.Unlock(MUTEX_CONTEXT);
+
+    if (triggerOnExisting) {
+        /* We don't want to do the callbacks from this thread, as it's
+         * most probably an application thread. Let the ObserverManager
+         * do this for us from the dispatcher thread. To avoid confusion,
+         * we leave this listener disabled so that other announcements on
+         * the work queue don't get reported out of order.
+         */
+        ObserverManager& obsmgr = bus.GetInternal().GetObserverManager();
+        obsmgr.EnablePendingListeners(this);
+    }
+}
+
+void Observer::Internal::UnregisterListener(Observer::Listener& listener)
+{
+    listenersLock.Lock(MUTEX_CONTEXT);
+
+    /* Look for listener on ListenerSet */
+    ObserverListenerSet::iterator it;
+    for (it = listeners.begin(); it != listeners.end(); ++it) {
+        if ((**it)->listener == &listener) {
+            break;
+        }
+    }
+
+    /* Wait for all refs to this ProtectedObserverListener to exit */
+    while ((it != listeners.end()) && (it->GetRefCount() > 1)) {
+        ProtectedObserverListener l = *it;
+        listenersLock.Unlock(MUTEX_CONTEXT);
+        qcc::Sleep(5);
+        listenersLock.Lock(MUTEX_CONTEXT);
+        it = listeners.find(l);
+    }
+
+    /* Delete the listeners entry */
+    if (it != listeners.end()) {
+        ProtectedObserverListener l = *it;
+        listeners.erase(it);
+        delete (*l);
+    }
+    listenersLock.Unlock(MUTEX_CONTEXT);
+}
+
+void Observer::Internal::EnablePendingListeners()
+{
+    /* make a list of all pending listeners */
+    vector<ProtectedObserverListener> pendingListeners;
+    listenersLock.Lock(MUTEX_CONTEXT);
+    ObserverListenerSet::iterator lit;
+    for (lit = listeners.begin(); lit != listeners.end(); ++lit) {
+        ProtectedObserverListener pol = *lit;
+        if ((*pol)->enabled == false) {
+            pendingListeners.push_back(pol);
+        }
+    }
+    listenersLock.Unlock(MUTEX_CONTEXT);
+
+    /* for each pending listener, invoke ObjectDiscovered for all known proxies */
+    proxiesLock.Lock(MUTEX_CONTEXT);
+    vector<ProtectedObserverListener>::iterator plit;
+    for (plit = pendingListeners.begin(); plit != pendingListeners.end(); ++plit) {
+        ProtectedObserverListener pol = *plit;
+        (*pol)->enabled = true;
+
+        ObjectMap::iterator it = proxies.begin();
+        while (it != proxies.end()) {
+            ObjectId id = it->first;
+            ManagedProxyBusObject proxy = it->second;
+            proxiesLock.Unlock(MUTEX_CONTEXT);
+            (*pol)->listener->ObjectDiscovered(proxy);
+            proxiesLock.Lock(MUTEX_CONTEXT);
+            it = proxies.upper_bound(id);
+        }
+
+    }
+
+    proxiesLock.Unlock(MUTEX_CONTEXT);
+}
+
+void Observer::Internal::UnregisterAllListeners()
+{
+    listenersLock.Lock(MUTEX_CONTEXT);
+
+    /* Look for listener on ListenerSet */
+    ObserverListenerSet::iterator it = listeners.begin();
+    while (it != listeners.end()) {
+        /* Wait for all refs to ProtectedBusListener to exit */
+        while ((it != listeners.end()) && (it->GetRefCount() > 1)) {
+            ProtectedObserverListener l = *it;
+            listenersLock.Unlock(MUTEX_CONTEXT);
+            qcc::Sleep(5);
+            listenersLock.Lock(MUTEX_CONTEXT);
+            it = listeners.find(l);
+        }
+
+        /* Delete the listeners entry */
+        if (it != listeners.end()) {
+            ProtectedObserverListener l = *it;
+            listeners.erase(it);
+        }
+        it = listeners.begin();
+    }
+    listenersLock.Unlock(MUTEX_CONTEXT);
+}
+
+ManagedProxyBusObject Observer::Internal::Get(const ObjectId& oid)
+{
+    ManagedProxyBusObject obj;
+    if (!oid.IsValid()) {
+        return obj;
+    }
+    proxiesLock.Lock(MUTEX_CONTEXT);
+    ObjectMap::iterator it = proxies.find(oid);
+    if (it != proxies.end()) {
+        obj = it->second;
+    }
+    proxiesLock.Unlock(MUTEX_CONTEXT);
+    return obj;
+}
+
+ManagedProxyBusObject Observer::Internal::GetFirst()
+{
+    ManagedProxyBusObject obj;
+    proxiesLock.Lock(MUTEX_CONTEXT);
+    ObjectMap::iterator it = proxies.begin();
+    if (it != proxies.end()) {
+        obj = it->second;
+    }
+    proxiesLock.Unlock(MUTEX_CONTEXT);
+    return obj;
+}
+
+ManagedProxyBusObject Observer::Internal::GetNext(const ObjectId& oid)
+{
+    ManagedProxyBusObject obj;
+    if (!oid.IsValid()) {
+        return obj;
+    }
+    proxiesLock.Lock(MUTEX_CONTEXT);
+    ObjectMap::iterator it = proxies.upper_bound(oid);
+    if (it != proxies.end()) {
+        obj = it->second;
+    }
+    proxiesLock.Unlock(MUTEX_CONTEXT);
+    return obj;
+}
+
+void Observer::Internal::ObjectDiscovered(const ObjectId& oid,
+                                          const std::set<qcc::String>& interfaces,
+                                          SessionId sessionid)
+{
+    /* create proxy object */
+    //TODO figure out what to do with secure bus objects
+    const char* busname = oid.uniqueBusName.c_str();
+    const char* path = oid.objectPath.c_str();
+    QCC_DbgTrace(("ObjectDiscovered(%s:%s)", busname, path));
+
+    ManagedProxyBusObject proxy(bus, busname, path, sessionid);
+    InterfaceSet::iterator it;
+    for (it = mandatory.begin(); it != mandatory.end(); ++it) {
+        proxy->AddInterface(it->c_str());
+    }
+    for (it = optional.begin(); it != optional.end(); ++it) {
+        if (interfaces.find(*it) != interfaces.end()) {
+            proxy->AddInterface(it->c_str());
+        }
+    }
+
+    /* insert in proxy map */
+    proxiesLock.Lock(MUTEX_CONTEXT);
+    proxies[oid] = proxy;
+    proxiesLock.Unlock(MUTEX_CONTEXT);
+
+    /* alert listeners */
+    listenersLock.Lock(MUTEX_CONTEXT);
+    ObserverListenerSet::iterator lit = listeners.begin();
+    while (lit != listeners.end()) {
+        ProtectedObserverListener pol = *lit;
+        if ((*pol)->enabled == false) {
+            ++lit;
+            continue;
+        }
+        listenersLock.Unlock(MUTEX_CONTEXT);
+        (*pol)->listener->ObjectDiscovered(proxy);
+        listenersLock.Lock(MUTEX_CONTEXT);
+        lit = listeners.upper_bound(pol);
+    }
+    listenersLock.Unlock(MUTEX_CONTEXT);
+}
+
+void Observer::Internal::ObjectLost(const ObjectId& oid)
+{
+    /* remove from proxy map */
+    bool found = false;
+    ManagedProxyBusObject proxy;
+
+    proxiesLock.Lock(MUTEX_CONTEXT);
+    ObjectMap::iterator it = proxies.find(oid);
+    if (it != proxies.end()) {
+        found = true;
+        proxy = it->second;
+        proxies.erase(it);
+    }
+    proxiesLock.Unlock(MUTEX_CONTEXT);
+
+    /* alert listeners */
+    if (found) {
+        listenersLock.Lock(MUTEX_CONTEXT);
+        ObserverListenerSet::iterator lit = listeners.begin();
+        while (lit != listeners.end()) {
+            ProtectedObserverListener pol = *lit;
+            if ((*pol)->enabled == false) {
+                ++lit;
+                continue;
+            }
+            listenersLock.Unlock(MUTEX_CONTEXT);
+            (*pol)->listener->ObjectLost(proxy);
+            listenersLock.Lock(MUTEX_CONTEXT);
+            lit = listeners.upper_bound(pol);
+        }
+        listenersLock.Unlock(MUTEX_CONTEXT);
+    }
+}
+
+Observer::Observer(BusAttachment& bus,
+                   const char* mandatoryInterfaces[],
+                   size_t numMandatoryInterfaces,
+                   const char* optionalInterfaces[],
+                   size_t numOptionalInterfaces)
+{
+    set<qcc::String> mandatory;
+    set<qcc::String> optional;
+    bool inError = false;
+
+    if (mandatoryInterfaces) {
+        for (size_t i = 0; i < numMandatoryInterfaces; ++i) {
+            const InterfaceDescription* intf = bus.GetInterface(mandatoryInterfaces[i]);
+            if (NULL == intf) {
+                QCC_LogError(ER_FAIL, ("Interface %s does not exist", mandatoryInterfaces[i]));
+                inError = true;
+            } else {
+                mandatory.insert(mandatoryInterfaces[i]);
+            }
+        }
+    }
+    if (optionalInterfaces) {
+        for (size_t i = 0; i < numOptionalInterfaces; ++i) {
+            const InterfaceDescription* intf = bus.GetInterface(optionalInterfaces[i]);
+            if (NULL == intf) {
+                QCC_LogError(ER_FAIL, ("Interface %s does not exist", optionalInterfaces[i]));
+                inError = true;
+            } else {
+                optional.insert(optionalInterfaces[i]);
+            }
+        }
+    }
+
+    if (mandatory.empty()) {
+        QCC_LogError(ER_FAIL, ("There must be at least one mandatory interface."));
+    }
+
+    if (inError) {
+        internal = NULL;
+    } else {
+        internal = new Internal(bus, this, mandatory, optional);
+    }
+}
+
+Observer::~Observer()
+{
+    if (internal) {
+        internal->Detach();
+        internal = NULL;
+    }
+}
+
+void Observer::RegisterListener(Listener& listener, bool triggerOnExisting)
+{
+    if (!internal) {
+        return;
+    }
+    internal->RegisterListener(listener, triggerOnExisting);
+}
+
+void Observer::UnregisterListener(Listener& listener)
+{
+    if (!internal) {
+        return;
+    }
+    internal->UnregisterListener(listener);
+}
+
+void Observer::UnregisterAllListeners()
+{
+    if (!internal) {
+        return;
+    }
+    internal->UnregisterAllListeners();
+}
+
+ManagedProxyBusObject Observer::Get(const ObjectId& oid)
+{
+    if (!internal) {
+        return ManagedProxyBusObject();
+    }
+    return internal->Get(oid);
+}
+
+ManagedProxyBusObject Observer::GetFirst()
+{
+    if (!internal) {
+        return ManagedProxyBusObject();
+    }
+    return internal->GetFirst();
+}
+
+ManagedProxyBusObject Observer::GetNext(const ObjectId& oid)
+{
+    if (!internal) {
+        return ManagedProxyBusObject();
+    }
+    return internal->GetNext(oid);
+}
+
+}
