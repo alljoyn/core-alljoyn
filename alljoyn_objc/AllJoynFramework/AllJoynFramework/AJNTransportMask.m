e78e8c60eec724222f65142b01414d37b89e8403
Craig Dowell <cdowell@qce.qualcomm.com>
2015-02-06 15:16:29 -0800

    ASACORE-1504: Deprecate old transport mask bits
    ASACORE-1504: Deprecate old and confusing transport mask bits
    
    Change-Id: Ia28b2164f2cac864191f85113ca20beb7387dd75
    Signed-off-by: Ry Jones <c_lawren@qce.qualcomm.com>

--- alljoyn_objc/AllJoynFramework/AllJoynFramework/AJNTransportMask.m
+++ alljoyn_objc/AllJoynFramework/AllJoynFramework/AJNTransportMask.m
@@ -15,14 +15,25 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #import "AJNTransportMask.h"
+#import "AvailabilityMacros.h"
 
 const AJNTransportMask kAJNTransportMaskNone      = 0x0000;   /**< no transports */
 const AJNTransportMask kAJNTransportMaskLocal     = 0x0001;   /**< Local (same device) transport */
-const AJNTransportMask kAJNTransportMaskWLAN      = 0x0004;   /**< Wireless local-area network transport */
-const AJNTransportMask kAJNTransportMaskWWAN      = 0x0008;   /**< Wireless wide-area network transport */
-const AJNTransportMask kAJNTransportMaskLAN       = 0x0010;   /**< Wired local-area network transport */
-const AJNTransportMask kAJNTransportMaskWiFiDirect= 0x0080;   /**< Transport using Wi-Fi Direct transport */
 const AJNTransportMask kAJNTransportMaskTCP       = 0x0004;   /**< TCP/IP transport */
 const AJNTransportMask kAJNTransportMaskUDP       = 0x0100;   /**< UDP/IP transport */
-const AJNTransportMask kAJNTransportMaskIP        = 0x0104;   /**< IP transport (system chooses betwen TCP and UDP) */
-const AJNTransportMask kAJNTransportMaskAny       = 0xFF7F;   /**< ANY transport (but Wi-Fi Direct) */
+const AJNTransportMask kAJNTransportMaskIP        = (kAJNTransportMaskTCP + kAJNTransportMaskUDP); /**< An IP-based transport */
+const AJNTransportMask kAJNTransportMaskAny       = (kAJNTransportMaskLocal + kAJNTransportMaskIP); /**< Any non-experimental transport */
+
+/*
+ * QCC_DEPRECATED is a macro defined in platform.h on Linux and Windows for C++
+ * that is used to mark functions, etc., as deprecated. Windows compilers use
+ * the __declspec(deprecated) prefix notation and various flavors of gcc use the
+ * __attribute__ ((deprecated)) postfix notation.  Objective-C on the other hand
+ * uses a postfix to the initialization value for attribtues which isn't
+ * compatible with the use of QCC_DEPRECATED.  Therefore we use the functionality
+ * defined for the purpose directly in Objective-C: DEPRECATED_ATTRIBUTE.
+ */
+const AJNTransportMask kAJNTransportMaskWLAN      = 0x0004 DEPRECATED_ATTRIBUTE;   /**< Wireless local-area network transport */
+const AJNTransportMask kAJNTransportMaskWWAN      = 0x0008 DEPRECATED_ATTRIBUTE;   /**< Wireless wide-area network transport */
+const AJNTransportMask kAJNTransportMaskLAN       = 0x0010 DEPRECATED_ATTRIBUTE;   /**< Wired local-area network transport */
+const AJNTransportMask kAJNTransportMaskWiFiDirect= 0x0080 DEPRECATED_ATTRIBUTE;   /**< Transport using Wi-Fi Direct transport */
