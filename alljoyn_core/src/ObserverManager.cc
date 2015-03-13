bea023610fb496ece12a9e19eb49612c2708412f
Meggan Ehret <meggan@qeo.org>
2015-03-13 16:08:17 +0100

    Improve ObserverManager shutdown behavior.
    Change-Id: Ie779ddd071bc367d81367de9c12f88630e40d786
    Signed-off-by: Meggan Ehret <meggan@qeo.org>

--- alljoyn_core/src/ObserverManager.cc
+++ alljoyn_core/src/ObserverManager.cc
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2015, AllSeen Alliance. All rights reserved.
+ * Copyright AllSeen Alliance. All rights reserved.
  *
  *    Permission to use, copy, modify, and/or distribute this software for any
  *    purpose with or without fee is hereby granted, provided that the above
@@ -15,6 +15,7 @@
  ******************************************************************************/
 
 #include <qcc/Mutex.h>
+#include <qcc/Condition.h>
 
 #include <alljoyn/Observer.h>
 #include <alljoyn/AutoPinger.h>
@@ -220,26 +221,48 @@ error:
 
 ObserverManager::ObserverManager(BusAttachment& bus) :
     bus(bus),
-    pinger(bus),
-    processingWork(false)
+    pinger(new AutoPinger(bus)),
+    processingWork(false),
+    stopping(false)
 {
     bus.RegisterAboutListener(*this);
-    pinger.AddPingGroup(PING_GROUP, *this, PING_INTERVAL);
+    pinger->AddPingGroup(PING_GROUP, *this, PING_INTERVAL);
 }
 
 ObserverManager::~ObserverManager()
 {
     QCC_DbgTrace(("ObserverManager::~ObserverManager"));
-    pinger.RemovePingGroup(PING_GROUP);
-    bus.UnregisterAboutListener(*this);
+    Stop();
+}
 
-    /* drop all remaining work from the queue */
+void ObserverManager::Stop() {
     wqLock.Lock(MUTEX_CONTEXT);
+    if (stopping) {
+        wqLock.Unlock(MUTEX_CONTEXT);
+        return;
+    }
+    stopping = true;
+
+    /* wait for any in-flight work item to land */
+    while (processingWork) {
+        processingDone.Wait(wqLock);
+    }
+
+    /* clear the work queue */
     while (!work.empty()) {
         delete work.front();
         work.pop();
     }
+
     wqLock.Unlock(MUTEX_CONTEXT);
+
+    /* stop and destroy the AutoPinger */
+    pinger->RemovePingGroup(PING_GROUP);
+    delete pinger;
+    pinger = NULL;
+
+    /* unregister for About callbacks */
+    bus.UnregisterAboutListener(*this);
 }
 
 void ObserverManager::RegisterObserver(CoreObserver* observer)
@@ -422,7 +445,7 @@ void ObserverManager::HandleActivePeerAnnouncement(DiscoveryMap::iterator peerit
          * active peer list */
         QCC_DbgPrintf(("not relevant"));
         bus.LeaveJoinedSessionAsync(peerit->first.sessionid, this, NULL);
-        pinger.RemoveDestination(PING_GROUP, peerit->first.busname);
+        pinger->RemoveDestination(PING_GROUP, peerit->first.busname);
         active.erase(peerit);
     } else {
         /* update the set of discovered objects */
@@ -464,7 +487,7 @@ void ObserverManager::CheckRelevanceAllPeers()
     for (std::vector<DiscoveryMap::iterator>::iterator iit = irrelevant.begin();
          iit != irrelevant.end(); ++iit) {
         bus.LeaveJoinedSessionAsync((*iit)->first.sessionid, this, NULL);
-        pinger.RemoveDestination(PING_GROUP, (*iit)->first.busname);
+        pinger->RemoveDestination(PING_GROUP, (*iit)->first.busname);
         active.erase(*iit);
     }
 }
@@ -473,35 +496,26 @@ void ObserverManager::ScheduleWork(WorkItem* workitem)
 {
     QCC_DbgTrace(("%s", __FUNCTION__));
     wqLock.Lock(MUTEX_CONTEXT);
-    workitem->mgr = this;
-    work.push(workitem);
+    if (!stopping) {
+        workitem->mgr = this;
+        work.push(workitem);
+    } else {
+        delete workitem;
+    }
     wqLock.Unlock(MUTEX_CONTEXT);
 }
 
 void ObserverManager::DoWork()
 {
     QCC_DbgTrace(("%s", __FUNCTION__));
-    wqLock.Lock(MUTEX_CONTEXT);
-    if (processingWork || work.empty()) {
-        /* no work to do, or some other thread is alreadly doing it */
-        QCC_DbgPrintf(("No work to be done (processing = %d, work.size() = %d)", (int) processingWork, (int) work.size()));
-        wqLock.Unlock(MUTEX_CONTEXT);
-        return;
-    } else {
-        /* tell the world we're going to do the work */
-        processingWork = true;
-    }
-    wqLock.Unlock(MUTEX_CONTEXT);
 
     while (true) {
         WorkItem* workitem = NULL;
         wqLock.Lock(MUTEX_CONTEXT);
-        if (!work.empty()) {
+        if (!processingWork && !work.empty() && !stopping) {
             workitem = work.front();
             work.pop();
-        } else {
-            /* tell the world we won't do any more work */
-            processingWork = false;
+            processingWork = true;
         }
         wqLock.Unlock(MUTEX_CONTEXT);
 
@@ -513,6 +527,11 @@ void ObserverManager::DoWork()
 
         workitem->Execute();
         delete workitem;
+
+        wqLock.Lock(MUTEX_CONTEXT);
+        processingWork = false;
+        processingDone.Broadcast();
+        wqLock.Unlock(MUTEX_CONTEXT);
     }
 }
 
@@ -596,7 +615,7 @@ void ObserverManager::ProcessSessionEstablished(const ObserverManager::Peer& pee
         /* move peer from pending set to active set */
         DiscoveryMap::iterator newit = active.insert(std::make_pair(peer, peerit->second)).first;
         pending.erase(peerit);
-        pinger.AddDestination(PING_GROUP, peer.busname);
+        pinger->AddDestination(PING_GROUP, peer.busname);
 
         QCC_DbgPrintf(("Moving peer %s from pending to active state.", peer.busname.c_str()));
         /* notify interested observers of the newly announced objects */
@@ -645,7 +664,7 @@ void ObserverManager::ProcessSessionLost(SessionId sessionid)
             cit->second->ObjectsLost(peerit->second);
         }
 
-        pinger.RemoveDestination(PING_GROUP, peerit->first.busname);
+        pinger->RemoveDestination(PING_GROUP, peerit->first.busname);
         active.erase(peerit);
     } else {
         QCC_LogError(ER_FAIL, ("Unexpected: lost a session we didn't ask for to begin with"));
@@ -669,7 +688,7 @@ void ObserverManager::ProcessDestinationLost(const qcc::String& busname)
 {
     QCC_DbgTrace(("%s", __FUNCTION__));
     /* we no longer care about this bus name */
-    pinger.RemoveDestination(PING_GROUP, busname);
+    pinger->RemoveDestination(PING_GROUP, busname);
 
     DiscoveryMap::iterator peerit;
     for (peerit = active.begin(); peerit != active.end(); ++peerit) {
--- alljoyn_core/src/ObserverManager.h
+++ alljoyn_core/src/ObserverManager.h
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2015, AllSeen Alliance. All rights reserved.
+ * Copyright AllSeen Alliance. All rights reserved.
  *
  *    Permission to use, copy, modify, and/or distribute this software for any
  *    purpose with or without fee is hereby granted, provided that the above
@@ -25,6 +25,7 @@
 
 #include <qcc/String.h>
 #include <qcc/Mutex.h>
+#include <qcc/Condition.h>
 
 #include <alljoyn/BusAttachment.h>
 #include <alljoyn/Observer.h>
@@ -74,6 +75,13 @@ class ObserverManager :
      * Destructor.
      */
     ~ObserverManager();
+
+    /**
+     * Stop the ObserverManager and make sure no more work items are scheduled on
+     * the LocalEndpoint dispatcher thread to avoid issues during shutdown of the
+     * BusAttachment.
+     */
+    void Stop();
 
     /**
      * Register a new Observer with the ObserverManager
@@ -252,7 +260,7 @@ class ObserverManager :
     /**
      * An AutoPinger instance to do the periodic liveness checks for us.
      */
-    AutoPinger pinger;
+    AutoPinger* pinger;
 
     /**
      * Process an announcement from a peer with which we're currently in session.
@@ -285,7 +293,9 @@ class ObserverManager :
      ****************************/
     std::queue<WorkItem*> work;
     qcc::Mutex wqLock;
+    qcc::Condition processingDone;
     bool processingWork;
+    bool stopping;
 
     /**
      * Add a work item to the work queue
