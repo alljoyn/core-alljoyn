c30e504e5c997cb56a07194c71a709d7f668f116
Ry Jones <rjones@linuxfoundation.org>
2015-03-30 10:50:56 -0700

    ASACORE-1744: Prep. Release 15.04
    Change-Id: I1a40fb8584202a11c6934cd96c16d89b487d8470
    Signed-off-by: Ry Jones <rjones@linuxfoundation.org>

--- alljoyn_js/jni/npapi/np.cc
+++ alljoyn_js/jni/npapi/np.cc
@@ -99,7 +99,7 @@ NP_EXPORT(char*) NP_GetPluginVersion()
 {
     InitializeDebug();
     QCC_DbgPrintf(("%s", __FUNCTION__));
-    return const_cast<char*>("00.00.01");
+    return const_cast<char*>("15.04.00");
 }
 
 NP_EXPORT(char*) NP_GetMIMEDescription()
