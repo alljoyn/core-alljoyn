035d6ffadd9cc1b156b2e3613ee0dd78f433675e
Daniel Mihai <Daniel.Mihai@microsoft.com>
2015-04-23 12:43:46 -0700

    ASACORE-1921 Avoid sending duplicate mDNS packets
    Signed-off-by: Daniel Mihai <Daniel.Mihai@microsoft.com>
    Change-Id: I33614376186566590ba3b356f73cea5a92bfcab9

--- alljoyn_core/router/ns/IpNameServiceImpl.cc
+++ alljoyn_core/router/ns/IpNameServiceImpl.cc
@@ -4485,8 +4485,26 @@ void IpNameServiceImpl::SendOutboundMessageQuietly(Packet packet)
             //
             QCC_DbgPrintf(("IpNameServiceImpl::SendOutboundMessageQuietly(): SendProtocolMessage()"));
             if (msgVersion == 2) {
-                SendProtocolMessage(m_liveInterfaces[i].m_multicastMDNSsockFd, ipv4address, interfaceAddressPrefixLen,
-                                    flags, interfaceIsIPv4, packet, i);
+                bool sendMessage = true;
+
+                //
+                // Link-local multicast send is directed through an interface index. If the current interface index
+                // is the same as a previously-seen interface index, there is no need to send again here.
+                //
+                for (uint32_t previousIndex = 0; previousIndex < i; ++previousIndex) {
+                    if ((m_liveInterfaces[previousIndex].m_index == m_liveInterfaces[i].m_index) &&
+                        (m_liveInterfaces[previousIndex].m_address.IsIPv6() == m_liveInterfaces[i].m_address.IsIPv6())) {
+                        sendMessage = false;
+                        break;
+                    }
+                }
+
+                if (sendMessage) {
+                    SendProtocolMessage(m_liveInterfaces[i].m_multicastMDNSsockFd, ipv4address, interfaceAddressPrefixLen,
+                                        flags, interfaceIsIPv4, packet, i);
+                } else {
+                    QCC_DbgPrintf(("IpNameServiceImpl::SendOutboundMessageQuietly(): skipping already-handled iface index %u\n", m_liveInterfaces[i].m_index));
+                }
             } else if (m_liveInterfaces[i].m_multicastsockFd != qcc::INVALID_SOCKET_FD) {
                 SendProtocolMessage(m_liveInterfaces[i].m_multicastsockFd, ipv4address, interfaceAddressPrefixLen,
                                     flags, interfaceIsIPv4, packet, i);
@@ -5064,8 +5082,27 @@ void IpNameServiceImpl::SendOutboundMessageActively(Packet packet, const qcc::IP
         // live interface we approved for sending.
         //
         if (msgVersion == 2) {
-            SendProtocolMessage(m_liveInterfaces[i].m_multicastMDNSsockFd, ipv4address, interfaceAddressPrefixLen,
-                                flags, interfaceIsIPv4, packet, i, localAddress);
+            bool sendMessage = true;
+
+            //
+            // Link-local multicast send is directed through an interface index. If the current interface index
+            // is the same as a previously-seen interface index, there is no need to send again here.
+            //
+            for (uint32_t previousIndex = 0; previousIndex < i; ++previousIndex) {
+                if ((m_liveInterfaces[previousIndex].m_index == m_liveInterfaces[i].m_index) &&
+                    (m_liveInterfaces[previousIndex].m_address.IsIPv6() == m_liveInterfaces[i].m_address.IsIPv6())) {
+                    sendMessage = false;
+                    break;
+                }
+            }
+
+            if (sendMessage) {
+                SendProtocolMessage(m_liveInterfaces[i].m_multicastMDNSsockFd, ipv4address, interfaceAddressPrefixLen,
+                                    flags, interfaceIsIPv4, packet, i, localAddress);
+            } else {
+                QCC_DbgPrintf(("IpNameServiceImpl::SendOutboundMessageActively(): skipping already-handled iface index %u\n", m_liveInterfaces[i].m_index));
+            }
+
         } else if (m_liveInterfaces[i].m_multicastsockFd != qcc::INVALID_SOCKET_FD) {
             SendProtocolMessage(m_liveInterfaces[i].m_multicastsockFd, ipv4address, interfaceAddressPrefixLen,
                                 flags, interfaceIsIPv4, packet, i, localAddress);
@@ -7625,7 +7662,7 @@ bool IpNameServiceImpl::RemoveFromPeerInfoMap(const qcc::String& guid)
         }
         QCC_DbgHLPrintf(("Erase from peer info map: guid=%s", guid.c_str()));
         m_peerInfoMap.erase(guid);
-        unordered_map<pair<String, IPEndpoint>, MDNSPacketTrackerEntry, HashPacketTracker, EqualPacketTracker>::iterator it1 = m_mdnsPacketTracker.begin();
+        unordered_map<pair<String, IPEndpoint>, uint16_t, HashPacketTracker, EqualPacketTracker>::iterator it1 = m_mdnsPacketTracker.begin();
         while (it1 != m_mdnsPacketTracker.end()) {
             if (it1->first.first == guid) {
                 m_mdnsPacketTracker.erase(it1++);
@@ -7640,31 +7677,6 @@ bool IpNameServiceImpl::RemoveFromPeerInfoMap(const qcc::String& guid)
     return false;
 }
 
-const uint32_t MDNS_PACKET_TRACKER_PURGE_TIMEOUT = 5 * 1000; /* 5 seconds */
-void IpNameServiceImpl::PurgeMDNSPacketTracker()
-{
-    Timespec now;
-    GetTimeNow(&now);
-    m_mutex.Lock();
-    unordered_map<pair<String, IPEndpoint>, MDNSPacketTrackerEntry, HashPacketTracker, EqualPacketTracker>::iterator it = m_mdnsPacketTracker.begin();
-    while (it != m_mdnsPacketTracker.end()) {
-        if ((now - it->second.timeStamp)  >= MDNS_PACKET_TRACKER_PURGE_TIMEOUT) {
-            m_mdnsPacketTracker.erase(it++);
-        } else {
-            it++;
-        }
-    }
-    m_mutex.Unlock();
-}
-
-bool IpNameServiceImpl::IsMDNSPacketTrackerEmpty()
-{
-    m_mutex.Lock();
-    bool isEmpty = m_mdnsPacketTracker.empty();
-    m_mutex.Unlock();
-    return isEmpty;
-}
-
 bool IpNameServiceImpl::UpdateMDNSPacketTracker(qcc::String guid, IPEndpoint endpoint, uint16_t burstId)
 {
     //QCC_DbgPrintf(("IpNameServiceImpl::UpdateMDNSPacketTracker(%s, %s,%d)", guid.c_str(), endpoint.ToString().c_str(), burstId));
@@ -7675,28 +7687,20 @@ bool IpNameServiceImpl::UpdateMDNSPacketTracker(qcc::String guid, IPEndpoint end
     // If we do not find it we return true that implies that we have not seen a packet from this burst.
     //     We add/update the guid with this burst id
     //
-
     pair<String, IPEndpoint> key(guid, endpoint);
-    unordered_map<pair<String, IPEndpoint>, MDNSPacketTrackerEntry, HashPacketTracker, EqualPacketTracker>::iterator it = m_mdnsPacketTracker.find(key);
+    unordered_map<pair<String, IPEndpoint>, uint16_t, HashPacketTracker, EqualPacketTracker>::iterator it = m_mdnsPacketTracker.find(key);
     // Check if the GUID is present in the Map
     // If Yes check if the incoming burst id is same or lower
     if (it != m_mdnsPacketTracker.end()) {
         // Drop the packet if burst id is lower or same
-        if (it->second.burstId >= burstId) {
+        if (it->second >= burstId) {
             return false;
         }
         // Update the last seen burst id from this guid
-        MDNSPacketTrackerEntry entry(burstId);
-        it->second = entry;
+        it->second = burstId;
     } else {
-        MDNSPacketTrackerEntry entry(burstId);
-        bool wasEmpty = m_mdnsPacketTracker.empty();
         // GUID is not present in the Map so we add the entry
-        m_mdnsPacketTracker[key] = entry;
-
-        if (wasEmpty) {
-            m_packetScheduler.Alert();
-        }
+        m_mdnsPacketTracker[key] = burstId;
     }
     return true;
 }
@@ -8577,7 +8581,7 @@ ThreadReturn STDCALL IpNameServiceImpl::PacketScheduler::Run(void* arg) {
     while (!IsStopping()) {
         Timespec now;
         GetTimeNow(&now);
-        uint32_t timeToSleep = (uint32_t)Event::WAIT_FOREVER;
+        uint32_t timeToSleep = (uint32_t)-1;
         //Step 1: Collect all packets
         std::list<Packet> subsequentBurstpackets;
         std::list<Packet> initialBurstPackets;
@@ -8835,12 +8839,8 @@ ThreadReturn STDCALL IpNameServiceImpl::PacketScheduler::Run(void* arg) {
         m_impl.m_mutex.Lock();
         //Step 3: Wait for a specific amount of time
         if (!IsStopping()) {
-            if ((timeToSleep == Event::WAIT_FOREVER) && !m_impl.IsMDNSPacketTrackerEmpty()) {
-                timeToSleep = MDNS_PACKET_TRACKER_PURGE_TIMEOUT;
-            }
             m_impl.m_mutex.Unlock();
             Event::Wait(Event::neverSet, timeToSleep);
-            m_impl.PurgeMDNSPacketTracker();
             GetStopEvent().ResetEvent();
             m_impl.m_mutex.Lock();
 
