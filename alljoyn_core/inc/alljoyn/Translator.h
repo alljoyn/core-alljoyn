fcc92a0bd3ffa59d40631fba9d2d434df83bb67f
Dave Thaler <dthaler@microsoft.com>
2015-08-11 16:53:30 -0700

    ASACORE-2157 Unify algorithm to get localized text
    Add translator support to AboutData.
    Move the recently-added-but-not-yet-released GetBestLanguage code from
    the AboutData class to the Translator class.
    Make IntrospectWithDescription use GetBestLanguage to resolve the usability
    problem.
    Abstract AboutData and IntrospectWithDescription translation code into
    generic StringTableTranslator and MsgArgTranslator classes to facilitate
    reuse.
    Make IntrospectWithDescription support a default translator like AboutData
    already did, using StringTableTranslator.
    
    Note that C binding support is not part of this ticket, since the C
    binding didn't support translation before this change.  That support is
    tracked by ASACORE-2174.  Also note that the whole reason for the
    MsgArgTranslator comes from the poorly designed AboutData::GetField() API
    which requires AboutData to internally keep a table of MsgArg structures it
    can return. In the future, this could possibly be deprecated (tracked by
    ASACORE-2323), but in this change it must be supported so that no existing
    code will break that may rely on it.
    
    Change-Id: I59a36c037e4c2fcede5c7646131a3bd485387390
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- alljoyn_core/inc/alljoyn/Translator.h
+++ alljoyn_core/inc/alljoyn/Translator.h
@@ -1,7 +1,8 @@
 /**
  * @file
  * Translator is an abstract base class (interface) implemented by users of the
- * AllJoyn API in order to provide org.allseen.Introspectable descriptions in more than just one language
+ * AllJoyn API in order to provide text in more than just one language. This
+ * is used with APIs such as IntrospectWithDescription and GetAboutData.
  */
 
 /******************************************************************************
@@ -27,68 +28,274 @@
 #endif
 
 #include <qcc/String.h>
+#include <alljoyn/MsgArg.h>
+#include <alljoyn/Status.h>
+#include <cctype>
+#include <map>
+#include <set>
 
 namespace ajn {
 
 /**
- * Abstract base class that provides translations of descriptions returned from
- * org.allseen.Introspectable.IntrospectWithDescription.
+ * Abstract base class that provides translations of text.
  */
 class Translator {
   public:
 
+    /** Destructor */
     virtual ~Translator() { }
 
     /**
      * Retrieve the number of target language tags this Translator
      * can translate into.
+     *
      * @return size_t The number of target language tags
      */
     virtual size_t NumTargetLanguages() = 0;
 
     /**
-     * Retrieve the n'th target language tag
-     * @param index The index of the language tag to retrieve
-     * @param ret The returned value
+     * Retrieve the n'th target language tag.
+     *
+     * @param[in] index The index of the language tag to retrieve
+     * @param[out] ret The returned value
      */
     virtual void GetTargetLanguage(size_t index, qcc::String& ret) = 0;
 
     /**
-     * Translate source from sourceLanguage into targetLanguage.
-     * If this Translator does not have a translation for the given
-     * parameters it should return NULL.
+     * Add a language to the set of supported target languages.
      *
-     * @param sourceLanguage The language tag of the text in source
-     * @param targetLanguage The language tag to translate into
-     * @param source The source text to translate
+     * @param[in] language The IETF language tag specified by RFC 5646
+     * @param[out] added Returns true if added, false if already present
+     * @return
+     *  - #ER_OK on success
+     *  - #ER_NOT_IMPLEMENTED if translator does not support adding target languages.
+     */
+    virtual QStatus AddTargetLanguage(const char* language, bool* added = NULL) {
+        QCC_UNUSED(language);
+        QCC_UNUSED(added);
+        return ER_NOT_IMPLEMENTED;
+    }
+
+    /**
+     * Translate an id or source text from sourceLanguage into text in the
+     * given targetLanguage. If this Translator does not have a translation
+     * for the given parameters, it should return NULL.
+     *
+     * @param[in] sourceLanguage The language tag of the text in sourceText. If
+     *  sourceLanguage is NULL or empty, then sourceText is simply an id used
+     *  for lookup.
+     * @param[in] targetLanguage The language tag to translate into
+     * @param[in] sourceText The source text to translate or id to look up
      * @return The translated text or NULL
      */
-    virtual const char* Translate(const char* sourceLanguage, const char* targetLanguage, const char* source) {
+    virtual const char* Translate(const char* sourceLanguage, const char* targetLanguage, const char* sourceText) {
         QCC_UNUSED(sourceLanguage);
         QCC_UNUSED(targetLanguage);
-        QCC_UNUSED(source);
+        QCC_UNUSED(sourceText);
         return NULL;
     }
 
     /**
-     * Translate source from sourceLanguage into targetLanguage.
-     * If this Translator does not have a translation for the given
-     * parameters it should return NULL. This version of the
-     * function is designed for imeplementations that return dynamically allocated strings.
+     * Translate an id or source text from sourceLanguage into text in the
+     * given targetLanguage. If this Translator does not have a translation
+     * for the given parameters, it should return NULL. This version of the
+     * function is designed for implementations that return dynamically allocated strings.
      * The string should be copied into buffer and buffer.c_str() must be returned. When alljoyn
      * finishes using the string buffer will be free'ed
      *
-     * @param sourceLanguage The language tag of the text in source
-     * @param targetLanguage The language tag to translate into
-     * @param source The source text to translate
-     * @param buffer A buffer to hold the dynamically allocated string returned by this function
+     * @param[in] sourceLanguage The language tag of the text in sourceText. If
+     *  sourceLanguage is NULL or empty, then sourceText is simply an id used
+     *  for lookup.
+     * @param[in] targetLanguage The language tag to translate into
+     * @param[in] sourceText The source text to translate or id to look up
+     * @param[out] buffer A buffer to hold the dynamically allocated string returned by this function
      * @return The translated text or NULL
      */
     virtual const char* Translate(const char* sourceLanguage,
-                                  const char* targetLanguage, const char* source, qcc::String& buffer) {
+                                  const char* targetLanguage, const char* sourceText, qcc::String& buffer) {
         QCC_UNUSED(buffer);
-        return Translate(sourceLanguage, targetLanguage, source);
+        return Translate(sourceLanguage, targetLanguage, sourceText);
     }
+
+    /**
+     * Translate an id or source text from sourceLanguage into text in the
+     * given targetLanguage. If this Translator does not support MsgArgs it
+     * should return NULL.
+     * This version of the function is designed for implementations that
+     * return a pointer to a MsgArg that will not go away. This is required
+     * by the AboutData::GetField() API.
+     *
+     * @param[in] sourceLanguage The language tag of the text in sourcetext. If
+     *  sourceLanguage is NULL or empty, then sourceText is simply an id used
+     *  for lookup.
+     * @param[in] targetLanguage The language tag to translate into
+     * @param[in] sourceText The source text to translate
+     * @param[out] msgarg Returns the MsgArg containing the translation
+     * @return
+     *  - #ER_OK on success
+     *  - #ER_NOT_IMPLEMENTED if translator does not support MsgArgs
+     */
+    virtual QStatus TranslateToMsgArg(const char* sourceLanguage,
+                                      const char* targetLanguage, const char* sourceText, MsgArg*& msgarg) {
+        QCC_UNUSED(sourceLanguage);
+        QCC_UNUSED(targetLanguage);
+        QCC_UNUSED(sourceText);
+        QCC_UNUSED(msgarg);
+        return ER_NOT_IMPLEMENTED;
+    }
+
+    /**
+     * Add new localized text.
+     *
+     * @param[in] id The id of the localized text to add.
+     * @param[in] value The localized text to be associated with the id.
+     * @param[in] language The IETF language tag specified by RFC 5646.
+     *
+     * @return
+     *  - #ER_OK on success
+     *  - #ER_NOT_IMPLEMENTED if translator does not support adding target languages.
+     */
+    virtual QStatus AddStringTranslation(const char* id, const char* value, const char* language) {
+        QCC_UNUSED(id);
+        QCC_UNUSED(value);
+        QCC_UNUSED(language);
+        return ER_NOT_IMPLEMENTED;
+    }
+
+    /**
+     * Add new localized text.
+     *
+     * @param[in] id The id of the localized text to add.
+     * @param[in] value The localized text to be associated with the id.
+     * @param[in] language The IETF language tag specified by RFC 5646.
+     *
+     * @return
+     *  - #ER_OK on success
+     *  - #ER_NOT_IMPLEMENTED if translator does not support adding target languages or the MsgArg type.
+     */
+    virtual QStatus AddMsgArgTranslation(const char* id, const MsgArg* value, const char* language) {
+        if (value->typeId != ALLJOYN_STRING) {
+            return ER_NOT_IMPLEMENTED;
+        }
+        return AddStringTranslation(id, value->v_string.str, language);
+    }
+
+    /**
+     * Get the best matching language according to RFC 4647 section 3.4.
+     *
+     * @param[in] The requested IETF language range.
+     * @param[in] The default language to use.
+     * @param[out] ret The returned value
+     */
+    virtual void GetBestLanguage(const char* requested, const qcc::String& defaultLanguage, qcc::String& ret);
+};
+
+/**
+ * Abstract base class that provides translations of text using a lookup
+ * table that maps a "field id" and a target language to a description
+ * in the target language corresponding to the indicated field.  Each
+ * unique string to translate should have its own id.  For example,
+ * AboutData has ids for manufacturer, description, etc.
+ */
+class LookupTableTranslator : public Translator {
+  public:
+    /**
+     * @see Translator::NumTargetLanguages
+     */
+    virtual size_t NumTargetLanguages();
+
+    /**
+     * @see Translator::GetTargetLanguage
+     */
+    virtual void GetTargetLanguage(size_t index, qcc::String& ret);
+
+    /**
+     * @see Translator::AddTargetLanguage
+     */
+    virtual QStatus AddTargetLanguage(const char* language, bool* added);
+
+    /**
+     * Retrieve the number of field ids this Translator has translations for.
+     * @return size_t The number of field ids
+     */
+    virtual size_t NumFields() = 0;
+
+    /**
+     * Retrieve the n'th field id
+     * @param index The index of the field id to retrieve
+     * @return The n'th field id
+     */
+    virtual const char* GetFieldId(size_t index) = 0;
+
+  protected:
+    struct CaseInsensitiveCompare {
+        struct CaseInsensitiveCharCompare {
+            bool operator()(const char& lhs, const char& rhs)
+            {
+                return std::tolower(lhs) < std::tolower(rhs);
+            }
+        };
+
+        bool operator()(const qcc::String& lhs, const qcc::String& rhs) const
+        {
+            return std::lexicographical_compare(lhs.begin(), lhs.end(),
+                                                rhs.begin(), rhs.end(),
+                                                CaseInsensitiveCharCompare());
+        }
+    };
+
+    /**
+     * Local member variable for supported target languages.
+     */
+    std::set<qcc::String, CaseInsensitiveCompare> supportedLanguages;
+
+    /**
+     * typedef supported languages iterator
+     */
+    typedef std::set<qcc::String, CaseInsensitiveCompare>::iterator supportedLanguagesIterator;
+};
+
+/**
+ * Class that provides translations of text using a lookup table where
+ * each translated string is stored as a simple string.  This can be used
+ * for introspection descriptions, and potentially other APIs.
+ */
+class StringTableTranslator : public LookupTableTranslator {
+  public:
+    /**
+     * @see LookupTableTranslator::NumFields
+     */
+    virtual size_t NumFields()
+    {
+        return localizedStore.size();
+    }
+
+    /**
+     * @see LookupTableTranslator::GetFieldId
+     */
+    virtual const char* GetFieldId(size_t index);
+
+    /**
+     * @see Translator::Translate
+     */
+    virtual const char* Translate(const char* sourceLanguage, const char* targetLanguage, const char* sourceText);
+
+    /**
+     * @see Translator::AddStringTranslation
+     */
+    virtual QStatus AddStringTranslation(const char* id, const char* value, const char* language);
+
+  protected:
+    /**
+     * Local member variable mapping a field id to a set of translations
+     * in various languages.
+     */
+    std::map<qcc::String, std::map<qcc::String, qcc::String, CaseInsensitiveCompare> > localizedStore;
+
+    /**
+     * typedef localized store iterator
+     */
+    typedef std::map<qcc::String, std::map<qcc::String, qcc::String, CaseInsensitiveCompare> >::iterator localizedStoreIterator;
 };
 
 }
