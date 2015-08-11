586bdf8499c192657401fda89b264f20b8079b6c
Dan Mihai <Daniel.Mihai@microsoft.com>
2015-08-11 09:27:56 -0700

    ASACORE-2306 Additional Mutex source code sharing
    In preparation for ASACORE-2300:
    
    1. Merge the Posix and Windows versions of mutex.h.
    
    2. Remove the file & line fields from Posix Release builds, since
    these fields are used just on Debug builds.
    
    3. Add Windows support for mutex re-initialization, for consistency
    with the Posix implementation.
    
    4. Remove the Windows dead code related to NO_LOCK_TRACE, and replace
    if with the already-existing Posix code for supporting MUTEX_CONTEXT.
    
    5. Move the shared MUTEX_CONTEXT code into a common mutex.cc file.
    
    6. Remove the contention spin count from the Windows mutex Init,
    because it appeared to use an arbitrary count and the OS is usually
    better at managing lock contention.
    
    Change-Id: If379f7df09a7da287ffe0cf86f2c5d49a7a61ee0
    Signed-off-by: Dan Mihai <Daniel.Mihai@microsoft.com>

--- common/inc/qcc/Mutex.h
+++ common/inc/qcc/Mutex.h
@@ -23,13 +23,145 @@
 #define _QCC_MUTEX_H
 
 #include <qcc/platform.h>
+#include <qcc/String.h>
+#include <alljoyn/Status.h>
 
 #if defined(QCC_OS_GROUP_POSIX)
-#include <qcc/posix/Mutex.h>
+#include <pthread.h>
+#endif
+
+namespace qcc {
+
+/**
+ * a macro that can be passed into the Mutex::Lock/Unlock member functions to
+ * help when debugging Mutex related issues. When running in debug mode, this
+ * will cause the code to log the name of the file and the line number of that
+ * file each time a Mutex lock is obtained and released. Logging must be turned
+ * on to see this information.
+ */
+#ifndef NDEBUG
+#define MUTEX_CONTEXT __FILE__, __LINE__
+#else
+#define MUTEX_CONTEXT
+#endif
+
+/**
+ * The implementation of a Mutex abstraction class.
+ */
+class Mutex {
+  public:
+    /**
+     * The constructor initializes the underlying mutex implementation.
+     */
+    Mutex() : isInitialized(false) { Init(); }
+
+    /**
+     * The destructor will destroy the underlying mutex.
+     */
+    ~Mutex();
+
+    /**
+     * Acquires a lock on the mutex.  If another thread is holding the lock,
+     * then this function will block until the other thread has released its
+     * lock.
+     *
+     * NOTE: Best practice is to call `Mutex::Lock(MUTEX_CONTEXT)`
+     *
+     * @see MUTEX_CONTEXT
+     *
+     * @param file the name of the file this Mutex lock was called from
+     * @param line the line number of the file this Mutex lock was called from
+     *
+     * @return
+     *  - #ER_OK if the lock was acquired.
+     *  - #ER_OS_ERROR if the underlying OS reports an error.
+     */
+    QStatus Lock(const char* file, uint32_t line);
+
+    /**
+     * Acquires a lock on the mutex.  If another thread is holding the lock,
+     * then this function will block until the other thread has released its
+     * lock.
+     *
+     * It is not safe to use static global data from within this function.
+     *
+     * @return
+     * - #ER_OK if the lock was acquired.
+     * - #ER_OS_ERROR if the underlying OS reports an error.
+     */
+    QStatus Lock();
+
+    /**
+     * Releases a lock on the mutex.  This will only release a lock for the
+     * current thread if that thread was the one that aquired the lock in the
+     * first place.
+     *
+     * NOTE: Best practice is to call `Mutex::Unlock(MUTEX_CONTEXT)`
+     *
+     * @see MUTEX_CONTEXT
+     *
+     * @param file the name of the file this Mutex unlock was called from
+     * @param line the line number of the file this Mutex unlock was called from
+     *
+     * @return
+     *  - #ER_OK if the lock was acquired.
+     *  - #ER_OS_ERROR if the underlying OS reports an error.
+     */
+    QStatus Unlock(const char* file, uint32_t line);
+
+    /**
+     * Releases a lock on the mutex.  This will only release a lock for the
+     * current thread if that thread was the one that aquired the lock in the
+     * first place.
+     *
+     * It is not safe to use static global data from within this function.
+     *
+     * @return
+     * - #ER_OK if the lock was acquired.
+     * - #ER_OS_ERROR if the underlying OS reports an error.
+     */
+    QStatus Unlock();
+
+    /**
+     * Attempt to acquire a lock on a mutex. If another thread is holding the lock
+     * this function return false otherwise the lock is acquired and the function returns true.
+     *
+     * @return  True if the lock was acquired.
+     */
+    bool TryLock();
+
+    /**
+     * Mutex copy constructor creates a new mutex.
+     */
+    Mutex(const Mutex& other) { QCC_UNUSED(other); Init(); }
+
+    /**
+     * Mutex assignment operator.
+     */
+    Mutex& operator=(const Mutex& other) { QCC_UNUSED(other); Init(); return *this; }
+
+  private:
+
+#if defined(QCC_OS_GROUP_POSIX)
+    pthread_mutex_t mutex;  ///< The Linux mutex implementation uses pthread mutex's.
 #elif defined(QCC_OS_GROUP_WINDOWS)
-#include <qcc/windows/Mutex.h>
+    CRITICAL_SECTION mutex; ///< The Windows mutex uses a critical section.
 #else
 #error No OS GROUP defined.
 #endif
+
+    bool isInitialized;     ///< true iff mutex was successfully initialized.
+    void Init();            ///< Initialize underlying OS mutex
+
+#ifndef NDEBUG
+    const char* file;
+    uint32_t line;
+#endif
+
+    /* The condition variable class needs access to the underlying private mutex */
+    friend class Condition;
+};
+
+} /* namespace */
 
 #endif
