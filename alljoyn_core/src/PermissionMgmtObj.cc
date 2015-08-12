685fc6208493413c5da218db45da647b2d38fd37
Meggan Ehret <meggan@qeo.org>
2015-08-12 09:37:30 +0200

    ASACORE-2342: Factory Reset deletes manifest template
    Do not remove the manifest template when resetting an application.
    
    Change-Id: I0cce158351ecfbc531be4426d5862a307ef1ae12
    (cherry picked from commit e9ce707f6d6c1abf766fdf655b38f76faae90e4f)
    Signed-off-by: Meggan Ehret <meggan@qeo.org>

--- alljoyn_core/src/PermissionMgmtObj.cc
+++ alljoyn_core/src/PermissionMgmtObj.cc
@@ -2099,6 +2099,12 @@ QStatus PermissionMgmtObj::PerformReset(bool keepForClaim)
         if (status != ER_OK) {
             return status;
         }
+        GetACLKey(ENTRY_MANIFEST_TEMPLATE, key);
+        status = ca->DeleteKey(key);
+        if (ER_OK != status) {
+            return status;
+        }
+
     }
     ca->GetLocalKey(KeyBlob::PEM, key);
     status = ca->DeleteKey(key);
@@ -2124,12 +2130,6 @@ QStatus PermissionMgmtObj::PerformReset(bool keepForClaim)
     }
 
     GetACLKey(ENTRY_MEMBERSHIPS, key);
-    status = ca->DeleteKey(key);
-    if (ER_OK != status) {
-        return status;
-    }
-
-    GetACLKey(ENTRY_MANIFEST_TEMPLATE, key);
     status = ca->DeleteKey(key);
     if (ER_OK != status) {
         return status;
