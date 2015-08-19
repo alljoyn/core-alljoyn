b8686bfc6538fc5cad974925332212cb4e55862d
Marius Niculescu <mariusni@microsoft.com>
2015-08-19 14:26:00 -0700

    ASACORE-1381: Banned API usage on Windows
    Change-Id: I11d1c55b4ba81bbc7efe0083e7c86a78139542e8
    Signed-off-by: Marius Niculescu <mariusni@microsoft.com>

--- common/inc/qcc/windows/mapping.h
+++ common/inc/qcc/windows/mapping.h
@@ -29,16 +29,25 @@
 #if ((_MSC_VER <= 1800) || defined(DO_SNPRINTF_MAPPING))
 
 /**
- * Map snprintf to _snprintf
+ * Map snprintf to _snprintf_s
  *
  * snprintf does not properly map in Windows; this is needed to ensure calls to
  * snprintf(char *str, size_t size, const char *format, ...) will compile in
  * Windows.
  */
-#define snprintf _snprintf
+#define snprintf(buffer, sizeOfBuffer, format, ...) \
+    _snprintf_s(buffer, sizeOfBuffer, format, _TRUNCATE, __VA_ARGS__)
 
 #endif
 /// @endcond
+
+/**
+ * Map strncpy to strncpy_s
+ *
+ * strncpy is deemed unsafe and is banned on Windows.
+ */
+#define strncpy(destination, source, elementCount) \
+    strncpy_s(destination, elementCount, source, _TRUNCATE)
 
 /**
  * Map stroll to _strtoi64
