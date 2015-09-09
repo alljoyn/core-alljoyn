3530f999430ee7db2536789e77584a48eaa00da6
George Nash <georgen@qce.qualcomm.com>
2015-09-09 15:26:28 -0700

    ASACORE-2081 Security 2.0 Authentication tests
    Add tests that verifys authentication between two peers
    when the policy has only one ACL but multiple peers.
    
    Change-Id: If35ed1dd0ad52644ca9ebb2749ba6381dede5c27
    Signed-off-by: George Nash <georgen@qce.qualcomm.com>

--- alljoyn_core/unit_test/SecurityAuthenticationTest.cc
+++ alljoyn_core/unit_test/SecurityAuthenticationTest.cc
@@ -264,7 +264,6 @@ class SecurityAuthenticationAuthListener : public AuthListener {
     bool verifyCredentialsCalled;
     bool authenticationSuccessfull;
     bool securityViolationCalled;
-
 };
 
 static void GetAppPublicKey(BusAttachment& bus, ECCPublicKey& publicKey)
@@ -888,6 +887,29 @@ TEST_F(SecurityAuthenticationTest, authenticate_test2_case4_ECDHE_ECDSA) {
     }
 }
 
+class SecurityAuthentication2AuthListener : public DefaultECDHEAuthListener {
+  public:
+    SecurityAuthentication2AuthListener() {
+        uint8_t psk[16] = { 0xfa, 0xaa, 0x0a, 0xf3,
+                            0xdd, 0x3f, 0x1e, 0x03,
+                            0x79, 0xda, 0x04, 0x6a,
+                            0x3a, 0xb6, 0xca, 0x44 };
+        SetPSK(psk, 16);
+    }
+
+    QStatus RequestCredentialsAsync(const char* authMechanism, const char* authPeer, uint16_t authCount, const char* userId, uint16_t credMask, void* context)
+    {
+        Credentials creds;
+        if (strcmp(authMechanism, "ALLJOYN_SRP_KEYX") == 0) {
+            if (credMask & AuthListener::CRED_PASSWORD) {
+                creds.SetPassword("123456");
+            }
+            return RequestCredentialsResponse(context, true, creds);
+        }
+        return DefaultECDHEAuthListener::RequestCredentialsAsync(authMechanism, authPeer, authCount, userId, credMask, context);
+    }
+};
+
 class SecurityAuthenticationTest2 : public testing::Test {
   public:
     SecurityAuthenticationTest2() :
@@ -1186,7 +1208,7 @@ class SecurityAuthenticationTest2 : public testing::Test {
     // ACL1: Peer type: WITH_MEMBERSHIP, SGID: Living Room
     // ACL2: Peer type: FROM_CERTIFICATE_AUTHORITY: CA1
     // ACL3: Peer Type: WITH_PUBLIC_KEY: Peer C
-    PermissionPolicy CreatePeer2Policy(uint32_t version) {
+    PermissionPolicy CreatePeer2PolicyA(uint32_t version) {
         PermissionPolicy policy;
         policy.SetVersion(version);
         {
@@ -1247,6 +1269,61 @@ class SecurityAuthenticationTest2 : public testing::Test {
         return policy;
     }
 
+    // Peer[0]: FROM_CERTIFICATE_AUTHORITY, Public key of CA1
+    // Peer[1]: WITH_MEMBERSHIP, SGID: Living Room
+    // Peer[2]: WITH_PUBLIC_KEY: Peer C's public key
+    PermissionPolicy CreatePeer2PolicyB(uint32_t version) {
+        PermissionPolicy policy;
+        policy.SetVersion(version);
+        {
+            PermissionPolicy::Acl acls[1];
+
+            // All of the acls are using the same permissive rules
+            PermissionPolicy::Rule rules[1];
+            rules[0].SetObjPath("*");
+            rules[0].SetInterfaceName("*");
+            {
+                PermissionPolicy::Rule::Member members[1];
+                members[0].Set("*",
+                               PermissionPolicy::Rule::Member::NOT_SPECIFIED,
+                               PermissionPolicy::Rule::Member::ACTION_PROVIDE |
+                               PermissionPolicy::Rule::Member::ACTION_MODIFY |
+                               PermissionPolicy::Rule::Member::ACTION_OBSERVE);
+                rules[0].SetMembers(1, members);
+            }
+
+            {
+                PermissionPolicy::Peer peers[3];
+                // Peer[0]: FROM_CERTIFICATE_AUTHORITY, Public key of CA1
+                peers[0].SetType(PermissionPolicy::Peer::PEER_FROM_CERTIFICATE_AUTHORITY);
+                KeyInfoNISTP256 ca1Key;
+                PermissionConfigurator& pcCA1 = busUsedAsCA1.GetPermissionConfigurator();
+                EXPECT_EQ(ER_OK, pcCA1.GetSigningPublicKey(ca1Key));
+                peers[0].SetKeyInfo(&ca1Key);
+                // Peer[1]: WITH_MEMBERSHIP, SGID: Living Room
+                peers[1].SetType(PermissionPolicy::Peer::PEER_WITH_MEMBERSHIP);
+                peers[1].SetSecurityGroupId(livingRoomSGID);
+                ////Get manager key
+                KeyInfoNISTP256 livingRoomKey;
+                PermissionConfigurator& pcLivingRoom = busUsedAsLivingRoom.GetPermissionConfigurator();
+                EXPECT_EQ(ER_OK, pcLivingRoom.GetSigningPublicKey(livingRoomKey));
+                peers[1].SetKeyInfo(&livingRoomKey);
+                // Peer[2]: WITH_PUBLIC_KEY: Peer C's public key
+                peers[2].SetType(PermissionPolicy::Peer::PEER_WITH_PUBLIC_KEY);
+                //Get manager key
+                KeyInfoNISTP256 peerCKey;
+                PermissionConfigurator& pcPeerC = busUsedAsPeerC.GetPermissionConfigurator();
+                EXPECT_EQ(ER_OK, pcPeerC.GetSigningPublicKey(peerCKey));
+                peers[2].SetKeyInfo(&peerCKey); //peerKey of peerC
+
+                acls[0].SetPeers(3, peers);
+                acls[0].SetRules(1, rules);
+            }
+            policy.SetAcls(1, acls);
+        }
+        return policy;
+    }
+
     /**
      * Base for test case 3.
      * Verify that, for authentication between two peers to succeed, identity trust
@@ -1290,7 +1367,7 @@ class SecurityAuthenticationTest2 : public testing::Test {
             EXPECT_EQ(ER_OK, sapWithPeer1.SecureConnection(true));
         }
         {
-            PermissionPolicy policy = CreatePeer2Policy(1);
+            PermissionPolicy policy = CreatePeer2PolicyA(1);
             PermissionPolicy peer2DefaultPolicy;
 
             SecurityApplicationProxy sapWithPeer2(managerBus, peer2Bus.GetUniqueName().c_str(), managerToPeer2SessionId);
@@ -1354,6 +1431,102 @@ class SecurityAuthenticationTest2 : public testing::Test {
         }
     }
 
+    /**
+     * Base for test case 4.
+     * Verify that, for authentication between two peers to succeed, identity
+     * trust of the peer is matched against all the authorities as specified in
+     * the policy. The policy has only one ACL but the ACL has several peers.
+     *
+     * Setup:
+     * Peers A and app. bus are claimed
+     * App. bus's IC is signed by the CA.
+     *
+     * Peer A has a policy with peer type: FROM_CERTIFICATE_AUTHORITY, Public key of CA.
+     *
+     * App. bus has the following ACLs.
+     * ACL1:  Peer type: WITH_MEMBERSHIP, SGID: Living Room
+     * ACL2: Peer type: FROM_CERTIFICATE_AUTHORITY: CA1
+     * ACL3: Peer Type: WITH_PUBLIC_KEY: Peer C
+     *
+     * The ACLs does not have any rules.
+     *
+     *
+     * peer1 == PeerA
+     * peer2 == App. Bus
+     */
+    void BaseAuthenticationTest4(BusAttachment& identityIssuer)
+    {
+        //Get peer1 key
+        KeyInfoNISTP256 peer1Key;
+        PermissionConfigurator& pcPeer1 = peer1Bus.GetPermissionConfigurator();
+        EXPECT_EQ(ER_OK, pcPeer1.GetSigningPublicKey(peer1Key));
+
+        //---------------- Install Policy --------------
+        {
+            PermissionPolicy policy = CreatePeer1Policy(1);
+            PermissionPolicy peer1DefaultPolicy;
+
+            SecurityApplicationProxy sapWithPeer1(managerBus, peer1Bus.GetUniqueName().c_str(), managerToPeer1SessionId);
+            EXPECT_EQ(ER_OK, sapWithPeer1.GetDefaultPolicy(peer1DefaultPolicy));
+            /* peer1 will have new CA but managerBus still retains admin group privilege so it can install identity
+             * cert to peer1 later */
+            SecurityAuthTestHelper::UpdatePolicyWithValuesFromDefaultPolicy(peer1DefaultPolicy, policy, false, true);
+            EXPECT_EQ(ER_OK, sapWithPeer1.UpdatePolicy(policy));
+            EXPECT_EQ(ER_OK, sapWithPeer1.SecureConnection(true));
+        }
+        {
+            PermissionPolicy policy = CreatePeer2PolicyB(1);
+            PermissionPolicy peer2DefaultPolicy;
+
+            SecurityApplicationProxy sapWithPeer2(managerBus, peer2Bus.GetUniqueName().c_str(), managerToPeer2SessionId);
+            EXPECT_EQ(ER_OK, sapWithPeer2.UpdatePolicy(policy));
+            /* From here on the managerBus has no access to peer2 anymore */
+        }
+
+        //Create peer1 identityCert
+
+        // All Inclusive manifest
+        const size_t manifestSize = 1;
+        PermissionPolicy::Rule manifest[manifestSize];
+        manifest[0].SetObjPath("*");
+        manifest[0].SetInterfaceName("*");
+        {
+            PermissionPolicy::Rule::Member member[1];
+            member[0].Set("*",
+                          PermissionPolicy::Rule::Member::NOT_SPECIFIED,
+                          PermissionPolicy::Rule::Member::ACTION_PROVIDE |
+                          PermissionPolicy::Rule::Member::ACTION_MODIFY |
+                          PermissionPolicy::Rule::Member::ACTION_OBSERVE);
+            manifest[0].SetMembers(1, member);
+        }
+
+        GUID128 peer1Guid;
+
+        uint8_t digest[Crypto_SHA256::DIGEST_SIZE];
+        EXPECT_EQ(ER_OK, PermissionMgmtObj::GenerateManifestDigest(busUsedAsCA1,
+                                                                   manifest, manifestSize,
+                                                                   digest, Crypto_SHA256::DIGEST_SIZE)) << " GenerateManifestDigest failed.";
+
+        IdentityCertificate identityCertChainPeer1[1];
+
+        PermissionConfigurator& pcCA = peer1Bus.GetPermissionConfigurator();
+        KeyInfoNISTP256 peer1PublicKey;
+        EXPECT_EQ(ER_OK, pcCA.GetSigningPublicKey(peer1PublicKey));
+
+        EXPECT_EQ(ER_OK, PermissionMgmtTestHelper::CreateIdentityCert(identityIssuer,
+                                                                      "1",
+                                                                      peer1Guid.ToString(),
+                                                                      peer1PublicKey.GetPublicKey(),
+                                                                      "Peer1Alias",
+                                                                      3600,
+                                                                      identityCertChainPeer1[0],
+                                                                      digest, Crypto_SHA256::DIGEST_SIZE)) << "Failed to create identity certificate.";
+
+        SecurityApplicationProxy sapWithPeer1(managerBus, peer1Bus.GetUniqueName().c_str(), managerToPeer1SessionId);
+        EXPECT_EQ(ER_OK, sapWithPeer1.UpdateIdentity(identityCertChainPeer1, 1, manifest, manifestSize))
+            << "Failed to update Identity cert or manifest ";
+    }
+
     BusAttachment managerBus;
     BusAttachment peer1Bus;
     BusAttachment peer2Bus;
@@ -1372,9 +1545,9 @@ class SecurityAuthenticationTest2 : public testing::Test {
     InMemoryKeyStoreListener listener3;
     InMemoryKeyStoreListener listener4;
 
-    DefaultECDHEAuthListener managerAuthListener;
-    DefaultECDHEAuthListener peer1AuthListener;
-    DefaultECDHEAuthListener peer2AuthListener;
+    SecurityAuthentication2AuthListener managerAuthListener;
+    SecurityAuthentication2AuthListener peer1AuthListener;
+    SecurityAuthentication2AuthListener peer2AuthListener;
 
     SecurityAuthenticationTestSessionPortListener managerSessionPortListener;
     SecurityAuthenticationTestSessionPortListener peer1SessionPortListener;
@@ -1588,4 +1761,165 @@ TEST_F(SecurityAuthenticationTest2, authenticate_test3_caseD) {
  */
 TEST_F(SecurityAuthenticationTest2, authenticate_test3_caseE) {
     BaseAuthenticationTest3(busUsedAsPeerC, ER_AUTH_FAIL, "authenticate_test3_caseE");
+}
+
+/*
+ * Purpose:
+ * Verify that, for authentication between two peers to succeed, identity trust
+ * of the peer is matched against all the authorities as specified in the policy.
+ * The policy has only one ACL but the ACL has several peers.
+ *
+ * Setup:
+ * Peer A and app. bus are claimed.
+ * app bus's IC: signed by CA
+ *
+ * Peer A has a local policy with FROM_CERTIFICATE_AUTHORITY peer type, public key of CA
+ *
+ * app bus has the following policy:
+ * ACL 1:
+ * Peer[0]: FROM_CERTIFICATE_AUTHORITY, Public key of CA1
+ * Peer[1]: WITH_MEMBERSHIP, SGID: Living Room
+ * Peer[2]: WITH_PUBLIC_KEY: Peer C's public key
+ *
+ * Case 1: A and app. bus set up a ECDHE_NULL based session.
+ * Case 2: A and app. bus set up a ECDHE_PSK based session.
+ * Case 3: A and app. bus set up a SRP based session.
+ * Case 4: A and app. bus set up a ECDHE_ECDSA based session, IC of A is signed by CA1
+ * Case 5: A and app. bus set up a ECDHE_ECDSA based session, IC of A is signed by Living room authority
+ * Case 6: A and app. bus set up a ECDHE_ECDSA based session, IC of A is signed by Peer C
+ * Case 7: A and app. bus set up a ECDHE_ECDSA based session, IC of A is signed by CA
+ *
+ * Verification:
+ * Cases 1-5: Secure session can be set up successfully.
+ * Cases 6-7: Secure session cannot be set up.
+ *
+ */
+TEST_F(SecurityAuthenticationTest2, authenticate_test4_case1) {
+    BaseAuthenticationTest4(busUsedAsCA);
+    uint32_t sessionId;
+    SessionOpts opts;
+    EXPECT_EQ(ER_OK, peer1Bus.JoinSession(peer2Bus.GetUniqueName().c_str(), peer2SessionPort, NULL, sessionId, opts));
+
+    {
+        EXPECT_EQ(ER_OK, peer1Bus.EnablePeerSecurity("ALLJOYN_ECDHE_NULL", &peer1AuthListener));
+        EXPECT_EQ(ER_OK, peer2Bus.EnablePeerSecurity("ALLJOYN_ECDHE_NULL", &peer2AuthListener));
+        SecurityApplicationProxy proxy(peer1Bus, peer2Bus.GetUniqueName().c_str(), sessionId);
+        EXPECT_EQ(ER_OK, proxy.SecureConnection(true));
+        SecurityApplicationProxy proxy2(peer2Bus, peer1Bus.GetUniqueName().c_str(), sessionId);
+        EXPECT_EQ(ER_OK, proxy2.SecureConnection(true));
+    }
+}
+
+/*
+ * See authenticate_test4_case1 above for full test description
+ */
+TEST_F(SecurityAuthenticationTest2, authenticate_test4_case2) {
+    BaseAuthenticationTest4(busUsedAsCA);
+    uint32_t sessionId;
+    SessionOpts opts;
+    EXPECT_EQ(ER_OK, peer1Bus.JoinSession(peer2Bus.GetUniqueName().c_str(), peer2SessionPort, NULL, sessionId, opts));
+
+    {
+        EXPECT_EQ(ER_OK, peer1Bus.EnablePeerSecurity("ALLJOYN_ECDHE_PSK", &peer1AuthListener));
+        EXPECT_EQ(ER_OK, peer2Bus.EnablePeerSecurity("ALLJOYN_ECDHE_PSK", &peer2AuthListener));
+        SecurityApplicationProxy proxy(peer1Bus, peer2Bus.GetUniqueName().c_str(), sessionId);
+        EXPECT_EQ(ER_OK, proxy.SecureConnection(true));
+        SecurityApplicationProxy proxy2(peer2Bus, peer1Bus.GetUniqueName().c_str(), sessionId);
+        EXPECT_EQ(ER_OK, proxy2.SecureConnection(true));
+    }
+}
+
+/*
+ * See authenticate_test4_case1 above for full test description
+ */
+TEST_F(SecurityAuthenticationTest2, authenticate_test4_case3) {
+    BaseAuthenticationTest4(busUsedAsCA);
+    uint32_t sessionId;
+    SessionOpts opts;
+    EXPECT_EQ(ER_OK, peer1Bus.JoinSession(peer2Bus.GetUniqueName().c_str(), peer2SessionPort, NULL, sessionId, opts));
+
+    {
+        EXPECT_EQ(ER_OK, peer1Bus.EnablePeerSecurity("ALLJOYN_SRP_KEYX", &peer1AuthListener));
+        EXPECT_EQ(ER_OK, peer2Bus.EnablePeerSecurity("ALLJOYN_SRP_KEYX", &peer2AuthListener));
+        SecurityApplicationProxy proxy(peer1Bus, peer2Bus.GetUniqueName().c_str(), sessionId);
+        EXPECT_EQ(ER_OK, proxy.SecureConnection(true));
+        SecurityApplicationProxy proxy2(peer2Bus, peer1Bus.GetUniqueName().c_str(), sessionId);
+        EXPECT_EQ(ER_OK, proxy2.SecureConnection(true));
+    }
+}
+
+/*
+ * See authenticate_test4_case1 above for full test description
+ */
+TEST_F(SecurityAuthenticationTest2, authenticate_test4_case4) {
+    BaseAuthenticationTest4(busUsedAsCA1);
+    uint32_t sessionId;
+    SessionOpts opts;
+    EXPECT_EQ(ER_OK, peer1Bus.JoinSession(peer2Bus.GetUniqueName().c_str(), peer2SessionPort, NULL, sessionId, opts));
+
+    {
+        EXPECT_EQ(ER_OK, peer1Bus.EnablePeerSecurity("ALLJOYN_ECDHE_ECDSA", &peer1AuthListener));
+        EXPECT_EQ(ER_OK, peer2Bus.EnablePeerSecurity("ALLJOYN_ECDHE_ECDSA", &peer2AuthListener));
+        SecurityApplicationProxy proxy(peer1Bus, peer2Bus.GetUniqueName().c_str(), sessionId);
+        EXPECT_EQ(ER_OK, proxy.SecureConnection(true));
+        SecurityApplicationProxy proxy2(peer2Bus, peer1Bus.GetUniqueName().c_str(), sessionId);
+        EXPECT_EQ(ER_OK, proxy2.SecureConnection(true));
+    }
+}
+
+/*
+ * See authenticate_test4_case1 above for full test description
+ */
+TEST_F(SecurityAuthenticationTest2, authenticate_test4_case5) {
+    BaseAuthenticationTest4(busUsedAsLivingRoom);
+    uint32_t sessionId;
+    SessionOpts opts;
+    EXPECT_EQ(ER_OK, peer1Bus.JoinSession(peer2Bus.GetUniqueName().c_str(), peer2SessionPort, NULL, sessionId, opts));
+
+    {
+        EXPECT_EQ(ER_OK, peer1Bus.EnablePeerSecurity("ALLJOYN_ECDHE_ECDSA", &peer1AuthListener));
+        EXPECT_EQ(ER_OK, peer2Bus.EnablePeerSecurity("ALLJOYN_ECDHE_ECDSA", &peer2AuthListener));
+        SecurityApplicationProxy proxy(peer1Bus, peer2Bus.GetUniqueName().c_str(), sessionId);
+        EXPECT_EQ(ER_OK, proxy.SecureConnection(true));
+        SecurityApplicationProxy proxy2(peer2Bus, peer1Bus.GetUniqueName().c_str(), sessionId);
+        EXPECT_EQ(ER_OK, proxy2.SecureConnection(true));
+    }
+}
+
+/*
+ * See authenticate_test4_case1 above for full test description
+ */
+TEST_F(SecurityAuthenticationTest2, authenticate_test4_case6) {
+    BaseAuthenticationTest4(busUsedAsPeerC);
+    uint32_t sessionId;
+    SessionOpts opts;
+    EXPECT_EQ(ER_OK, peer1Bus.JoinSession(peer2Bus.GetUniqueName().c_str(), peer2SessionPort, NULL, sessionId, opts));
+
+    {
+        EXPECT_EQ(ER_OK, peer1Bus.EnablePeerSecurity("ALLJOYN_ECDHE_ECDSA", &peer1AuthListener));
+        EXPECT_EQ(ER_OK, peer2Bus.EnablePeerSecurity("ALLJOYN_ECDHE_ECDSA", &peer2AuthListener));
+        SecurityApplicationProxy proxy(peer1Bus, peer2Bus.GetUniqueName().c_str(), sessionId);
+        EXPECT_EQ(ER_AUTH_FAIL, proxy.SecureConnection(true));
+        SecurityApplicationProxy proxy2(peer2Bus, peer1Bus.GetUniqueName().c_str(), sessionId);
+        EXPECT_EQ(ER_AUTH_FAIL, proxy2.SecureConnection(true));
+    }
+}
+
+/*
+ * See authenticate_test4_case1 above for full test description
+ */
+TEST_F(SecurityAuthenticationTest2, authenticate_test4_case7) {
+    BaseAuthenticationTest4(busUsedAsCA);
+    uint32_t sessionId;
+    SessionOpts opts;
+    EXPECT_EQ(ER_OK, peer1Bus.JoinSession(peer2Bus.GetUniqueName().c_str(), peer2SessionPort, NULL, sessionId, opts));
+
+    {
+        EXPECT_EQ(ER_OK, peer1Bus.EnablePeerSecurity("ALLJOYN_ECDHE_ECDSA", &peer1AuthListener));
+        EXPECT_EQ(ER_OK, peer2Bus.EnablePeerSecurity("ALLJOYN_ECDHE_ECDSA", &peer2AuthListener));
+        SecurityApplicationProxy proxy(peer1Bus, peer2Bus.GetUniqueName().c_str(), sessionId);
+        EXPECT_EQ(ER_AUTH_FAIL, proxy.SecureConnection(true));
+        SecurityApplicationProxy proxy2(peer2Bus, peer1Bus.GetUniqueName().c_str(), sessionId);
+        EXPECT_EQ(ER_AUTH_FAIL, proxy2.SecureConnection(true));
+    }
 }
