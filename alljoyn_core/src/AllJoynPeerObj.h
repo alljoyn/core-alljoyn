06091ca88f206bf554282a36b8703fe6f106c5f8
Greg Zaverucha <gregz@microsoft.com>
2015-09-16 11:35:01 -0700

    ASACORE-2443 Do not hash ExchangeGuids messages
    AllJoyn assumes that the ExchangeGuids message call is idempotent.
    When ASACORE-2068 started hashing the full authentication
    conversation, the state change caused by hashing ExchangeGuids caused
    authentication to fail in certain concurrent authentication scenarios.
    
    This changes the conversation hashing to start only after
    ExchangeGuids has completed, and hashes the GUIDs directly, rather
    than the ExchangeGuids messages.  A corresponding change to the thin
    client will be submitted simultaneously.
    
    Change-Id: I83e63f04f034a16c9e98e2cb847dcb5659cb3d81
    Signed-off-by: Greg Zaverucha <gregz@microsoft.com>

--- alljoyn_core/src/AllJoynPeerObj.h
+++ alljoyn_core/src/AllJoynPeerObj.h
@@ -465,6 +465,17 @@ class AllJoynPeerObj : public BusObject, public BusListener, public qcc::AlarmLi
      * @param msg     The method call message
      */
     void SendMemberships(const InterfaceDescription::Member* member, Message& msg);
+
+    /**
+     *  Compute the hash of the local GUID and remote peer's GUID, using the
+     *  conversation hash state. The authentication protocol version is also
+     *  hashed (before the GUIDs).
+     *
+     *  @param sender      The name of the remote peer.
+     *  @param localFirst  If true compute Hash(local||remote), if false compute Hash(remote||local).
+     */
+    void HashGUIDs(const char* sender, bool localFirst);
+
     /**
      * The peer-to-peer authentication mechanisms available to this object
      */
