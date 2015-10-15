4e3ac9e376c9d1b93f8e94723e1f7a642c590e27
Dan Mihai <Daniel.Mihai@microsoft.com>
2015-10-14 20:30:14 -0700

    ASACORE-2602 Fix trust anchor use-after-free
    1. Synchronize the access to the trust anchor list, using a new lock.
    
    2. Add reference count to trust anchors, because the callers of
    LocateTrustAnchor are referencing copies of these trust anchors
    without holding the list lock.
    
    Change-Id: Ifd3fc1da2830f74c4fdbdd07260bf3a64bd8ed8d
    Signed-off-by: Dan Mihai <Daniel.Mihai@microsoft.com>

--- alljoyn_core/src/PermissionMgmtObj.h
+++ alljoyn_core/src/PermissionMgmtObj.h
@@ -26,6 +26,8 @@
 #error Only include PermissionMgmtObj.h in C++ code.
 #endif
 
+#include <memory>
+
 #include <alljoyn/BusObject.h>
 #include <alljoyn/AllJoynStd.h>
 #include <alljoyn/BusAttachment.h>
@@ -176,7 +178,30 @@ class PermissionMgmtObj : public BusObject {
     /**
      * The list of trust anchors
      */
-    typedef std::vector<TrustAnchor*> TrustAnchorList;
+    class TrustAnchorList {
+      public:
+        typedef std::vector<std::shared_ptr<TrustAnchor> >::iterator iterator;
+        typedef std::vector<std::shared_ptr<TrustAnchor> >::const_iterator const_iterator;
+
+        iterator begin() { return anchors.begin(); }
+        const_iterator begin() const { return anchors.begin(); }
+        iterator end() { return anchors.end(); }
+        const_iterator end() const { return anchors.end(); }
+
+        void push_back(std::shared_ptr<TrustAnchor> ta) { anchors.push_back(ta); }
+        void clear() { anchors.clear(); }
+        bool empty() const { return anchors.empty(); }
+        std::vector<std::shared_ptr<TrustAnchor> >::size_type size() const { return anchors.size(); }
+
+        QStatus Lock(const char* file, uint32_t line) { return lock.Lock(file, line); }
+        QStatus Unlock(const char* file, uint32_t line) { return lock.Unlock(file, line); }
+        QStatus Lock() { return lock.Lock(); }
+        QStatus Unlock() { return lock.Unlock(); }
+
+      private:
+        std::vector<std::shared_ptr<TrustAnchor> > anchors;
+        qcc::Mutex lock;
+    };
 
     /**
      * Constructor
@@ -238,26 +263,16 @@ class PermissionMgmtObj : public BusObject {
      * Is there any trust anchor installed?
      * @return true if there is at least one trust anchors installed; false, otherwise.
      */
-    bool HasTrustAnchors()
-    {
-        return !trustAnchors.empty();
-    }
+    bool HasTrustAnchors();
 
     /**
      * Retrieve the list of trust anchors.
      * @return the list of trust anchors
      */
-
     const TrustAnchorList& GetTrustAnchors()
     {
         return trustAnchors;
     }
-
-    /**
-     * Helper function to release the allocated memory for the trust anchor list.
-     * @param list the list to be clear of allocated memory.
-     */
-    static void ClearTrustAnchorList(TrustAnchorList& list);
 
     /**
      * Help function to store DSA keys in the key store.
@@ -266,7 +281,6 @@ class PermissionMgmtObj : public BusObject {
      * @param publicKey the DSA public key
      * @return ER_OK if successful; otherwise, error code.
      */
-
     static QStatus StoreDSAKeys(CredentialAccessor* ca, const qcc::ECCPrivateKey* privateKey, const qcc::ECCPublicKey* publicKey);
 
     /**
