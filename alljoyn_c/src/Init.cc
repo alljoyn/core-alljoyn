214624207770a35160ccad234e5da6194856f7cd
Krystian Zlomek <kzlomek@affinegy.com>
2016-05-12 19:59:45 +0200

    ASACORE-2747: Static Mutexes should be moved into AllJoynInit/Shutdown
    Change-Id: I512e544fe9cd05790fcca60b0ba5af4823094f5d
    Signed-off-by: Krystian Zlomek <kzlomek@affinegy.com>

--- alljoyn_c/src/Init.cc
+++ alljoyn_c/src/Init.cc
@@ -19,16 +19,56 @@
  *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  ******************************************************************************/
 #include <alljoyn_c/Init.h>
+#include "DeferredCallback.h"
 #include <alljoyn/Init.h>
+#include <qcc/LockLevel.h>
 
 #define QCC_MODULE "ALLJOYN_C"
 
+/*
+ * Disable LockOrderChecker for the Init lock, because this lock is being used
+ * before LockOrderChecker & Thread modules have been initialized.
+ */
+static qcc::Mutex allJoyn_init_lock(qcc::LOCK_LEVEL_CHECKING_DISABLED);
+static uint32_t allJoyn_init_count = 0;
+
 QStatus AJ_CALL alljoyn_init(void)
 {
-    return AllJoynInit();
+    QStatus status = ER_OK;
+
+    allJoyn_init_lock.Lock();
+
+    if (allJoyn_init_count == 0) {
+        status = AllJoynInit();
+        if (status == ER_OK) {
+            ajn::DeferredCallback::Init();
+            allJoyn_init_count = 1;
+        }
+    } else if (allJoyn_init_count < 0xFFFFFFFF) {
+        allJoyn_init_count++;
+    }
+
+    allJoyn_init_lock.Unlock();
+
+    return status;
 }
 
 QStatus AJ_CALL alljoyn_shutdown(void)
 {
-    return AllJoynShutdown();
+    QStatus status = ER_OK;
+
+    allJoyn_init_lock.Lock();
+
+    if (allJoyn_init_count > 0) {
+        allJoyn_init_count--;
+
+        if (allJoyn_init_count == 0) {
+            ajn::DeferredCallback::Shutdown();
+            status =  AllJoynShutdown();
+        }
+    }
+
+    allJoyn_init_lock.Unlock();
+
+    return status;
 }
