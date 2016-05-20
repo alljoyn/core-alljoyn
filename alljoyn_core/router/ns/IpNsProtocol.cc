9c860beda16c8fe64a007bfe832d0f920076b035
Brad Kemp <brad@beechwoods.com>
2016-05-20 13:49:16 -0400

    ASACORE-131: Initial IPV6 support
    Update to resolve comments
    Provide better support for multi-homed hosts with multiple addresses per interface
    Fixed windows compile error
    Change-Id: If66e833a3d4c4080e694c3da3a26e203b650f630
    Signed-off-by: Brad Kemp <brad@beechwoods.com>

--- alljoyn_core/router/ns/IpNsProtocol.cc
+++ alljoyn_core/router/ns/IpNsProtocol.cc
@@ -3014,6 +3014,25 @@ void MDNSSenderRData::SetIPV4ResponseAddr(qcc::String ipv4Addr)
 {
     MDNSTextRData::SetValue("ipv4", ipv4Addr);
 }
+uint16_t MDNSSenderRData::GetIPV6ResponsePort()
+{
+    return MDNSTextRData::GetU16Value("upcv6");
+}
+
+void MDNSSenderRData::SetIPV6ResponsePort(uint16_t ipv6Port)
+{
+    MDNSTextRData::SetValue("upcv6", U32ToString(ipv6Port));
+}
+
+qcc::String MDNSSenderRData::GetIPV6ResponseAddr()
+{
+    return MDNSTextRData::GetValue("ipv6");
+}
+
+void MDNSSenderRData::SetIPV6ResponseAddr(qcc::String ipv6Addr)
+{
+    MDNSTextRData::SetValue("ipv6", ipv6Addr);
+}
 
 //MDNSHeader
 MDNSHeader::MDNSHeader() :
@@ -3368,6 +3387,26 @@ void _MDNSPacket::RemoveAdditionalRecord(qcc::String str, MDNSResourceRecord::RR
             m_header.SetARCount(m_additional.size());
             return;
         }
+        it1++;
+    }
+}
+
+void _MDNSPacket::dump()
+{
+    std::vector<MDNSResourceRecord>::iterator it1 = m_additional.begin();
+    QCC_DbgPrintf(("Dumping MDNSPacket additional\n"));
+    while (it1 != m_additional.end()) {
+        int t;
+        t = (int)((*it1).GetRRType());
+        QCC_DbgPrintf((" %s %d\n", (*it1).GetDomainName().c_str(), t));
+        it1++;
+    }
+    it1 = m_answers.begin();
+    QCC_DbgPrintf(("Dumping MDNSPacket answers\n"));
+    while (it1 != m_answers.end()) {
+        int t;
+        t = (int)((*it1).GetRRType());
+        QCC_DbgPrintf((" %s %d\n", (*it1).GetDomainName().c_str(), t));
         it1++;
     }
 }
