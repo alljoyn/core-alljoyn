e9545e62d81af9a68ad2c4386e3785456df4c40e
Krystian Zlomek <kzlomek@affinegy.com>
2016-06-29 12:15:14 +0200

    ASACORE-3034: Remove private headers from CustomKeyStoreListener sample
    Change-Id: I67d1ec9608fc3a9e44a22f9b0c9b23cf78cdc9f8
    Signed-off-by: Krystian Zlomek <kzlomek@affinegy.com>

--- alljoyn_core/samples/secure/CustomKeyStoreListener.cc
+++ alljoyn_core/samples/secure/CustomKeyStoreListener.cc
@@ -21,11 +21,16 @@
 
 #include <qcc/platform.h>
 #include <qcc/String.h>
-#include <qcc/Util.h>
-#include <qcc/FileStream.h>
+#include <qcc/Debug.h>
 
 #include <alljoyn/KeyStoreListener.h>
 #include <alljoyn/Status.h>
+
+#include "ExclusiveFile.h"
+
+#if defined(QCC_OS_ANDROID)
+#include <stdlib.h>
+#endif
 
 #define QCC_MODULE "ALLJOYN_AUTH"
 
@@ -34,7 +39,28 @@ using namespace ajn;
 
 static String FixKeyStoreFilePath(const char* fname)
 {
-    String path = GetHomeDir();
+    String path;
+#if !defined(QCC_OS_GROUP_WINDOWS)
+    char* val = getenv("HOME");
+    if (val) {
+        path = val;
+    }
+#else
+    String key = "LOCALAPPDATA";
+    DWORD len = GetEnvironmentVariableA(key, nullptr, 0);
+    if (len == 0) {
+        key = "USERPROFILE";
+        len = GetEnvironmentVariableA(key, nullptr, 0);
+    }
+    if (len != 0) {
+        char* val = new char[len];
+        DWORD readLen = GetEnvironmentVariableA(key.c_str(), val, len);
+        if (readLen != 0) {
+            path = val;
+        }
+        delete[] val;
+    }
+#endif
     path += fname;
     return path;
 }
@@ -43,20 +69,8 @@ class CustomKeyStoreListener : public KeyStoreListener {
 
   public:
 
-    CustomKeyStoreListener(const char* fname) : fileName(FixKeyStoreFilePath(fname)), fileLocker(fileName.c_str())
+    CustomKeyStoreListener(const char* fname) : fileName(FixKeyStoreFilePath(fname)), exclusiveFile(fileName.c_str())
     {
-        FileLock readLock;
-        /* 'readLock' is released when goes out of scope. */
-        QStatus status = fileLocker.GetFileLockForRead(&readLock);
-        if (status == ER_EOF) {
-            /* The file does not exist. Create one here by simply acquiring Write lock. */
-            status = fileLocker.AcquireWriteLock();
-            if (status == ER_OK) {
-                fileLocker.ReleaseWriteLock();
-            } else {
-                QCC_LogError(status, ("FileLocker::AcquireWriteLock() failed, cannot write file (%s)", fileName.c_str()));
-            }
-        }
     }
 
     QStatus AcquireExclusiveLock(const char* file, uint32_t line)
@@ -66,93 +80,73 @@ class CustomKeyStoreListener : public KeyStoreListener {
             QCC_LogError(status, ("KeyStoreListener::AcquireExclusiveLock failed"));
             return status;
         }
-        status = fileLocker.AcquireWriteLock();
+        status = exclusiveFile.AcquireExclusiveLock();
         if (status != ER_OK) {
-            QCC_LogError(status, ("fileLocker.AcquireWriteLock() failed"));
+            QCC_LogError(status, ("exclusiveFile.AcquireExclusiveLock() failed"));
         }
         return status;
     }
 
     void ReleaseExclusiveLock(const char* file, uint32_t line)
     {
-        QCC_ASSERT(fileLocker.HasWriteLock());
-        fileLocker.ReleaseWriteLock();
+        QCC_ASSERT(exclusiveFile.HasExclusiveLock());
+        exclusiveFile.ReleaseExclusiveLock();
         KeyStoreListener::ReleaseExclusiveLock(file, line);
     }
 
     QStatus LoadRequest(KeyStore& keyStore)
     {
-        FileLock readLock;
-        QStatus status = fileLocker.GetFileLockForRead(&readLock);
+        int64_t size = 0;
+        QStatus status = exclusiveFile.GetSize(size);
         if (status == ER_OK) {
-            FileSource* source = readLock.GetSource();
-            QCC_ASSERT(source != nullptr);
-            if (source != nullptr) {
-                int64_t size = 0;
-                status = source->GetSize(size);
-                if (status == ER_OK) {
-                    String keysToLoad;
-                    if ((size > 0) && (static_cast<size_t>(size) < SIZE_MAX)) {
-                        char* buffer = new char[size];
-                        size_t pulled = 0;
-                        status = source->PullBytes(buffer, size, pulled);
-                        QCC_ASSERT(static_cast<size_t>(size) == pulled);
-                        keysToLoad = String(buffer, pulled);
-                        delete[] buffer;
-                    } else if (static_cast<size_t>(size) >= SIZE_MAX) {
-                        QCC_LogError(ER_READ_ERROR, ("Too big key store file %s, loading empty key store", fileLocker.GetFileName()));
-                    }
-                    status = PutKeys(keyStore, keysToLoad, fileLocker.GetFileName());
-                    if (status != ER_OK) {
-                        QCC_LogError(status, ("PutKeys failed"));
-                    }
-                } else {
-                    QCC_LogError(status, ("Failed to get the size of %s\n", fileLocker.GetFileName()));
-                }
-            } else {
-                status = ER_FAIL;
-                QCC_LogError(status, ("Internal error has occurred, cannot get a handle to %s\n", fileLocker.GetFileName()));
+            String keysToLoad;
+            if ((size > 0) && (static_cast<size_t>(size) < SIZE_MAX)) {
+                char* buffer = new char[size];
+                size_t pulled = 0;
+                status = exclusiveFile.Read(buffer, size, pulled);
+                QCC_ASSERT(static_cast<size_t>(size) == pulled);
+                keysToLoad = String(buffer, pulled);
+                delete[] buffer;
+            } else if (static_cast<size_t>(size) >= SIZE_MAX) {
+                QCC_LogError(ER_READ_ERROR, ("Too big key store file %s, loading empty key store", fileName.c_str()));
+            }
+            status = PutKeys(keyStore, keysToLoad, fileName.c_str());
+            if (status != ER_OK) {
+                QCC_LogError(status, ("PutKeys failed"));
             }
         } else {
-            QCC_LogError(status, ("Failed to access read lock on file %s\n", fileLocker.GetFileName()));
+            QCC_LogError(status, ("Failed to get the size of %s\n", fileName.c_str()));
         }
         if (status == ER_OK) {
-            QCC_DbgHLPrintf(("LoadRequest from %s done", fileLocker.GetFileName()));
+            QCC_DbgHLPrintf(("LoadRequest from %s done", fileName.c_str()));
         }
         return status;
     }
 
     QStatus StoreRequest(KeyStore& keyStore)
     {
-        FileLock writeLock;
-        QStatus status = fileLocker.GetFileLockForWrite(&writeLock);
-        if (status == ER_OK) {
-            String sink;
-            status = GetKeys(keyStore, sink);
-            if (status != ER_OK) {
-                QCC_LogError(status, ("GetKeys failed"));
-                return status;
-            }
-            size_t pushed = 0;
-            status = writeLock.GetSink()->PushBytes(sink.data(), sink.size(), pushed);
-            if (status != ER_OK) {
-                QCC_LogError(status, ("StoreRequest error during data saving"));
-                return status;
-            }
-            if (pushed != sink.size()) {
-                status = ER_BUS_CORRUPT_KEYSTORE;
-                QCC_LogError(status, ("StoreRequest failed to save data correctly"));
-                return status;
-            }
-            if (!writeLock.GetSink()->Truncate()) {
-                QCC_LogError(status, ("FileSink::Truncate failed"));
-            }
-        } else {
-            QCC_LogError(status, ("Failed to store request - write lock has not been taken"));
-            QCC_ASSERT(!"write lock has not been taken");
+        String sink;
+        QStatus status = GetKeys(keyStore, sink);
+        if (status != ER_OK) {
+            QCC_LogError(status, ("GetKeys failed"));
+            return status;
+        }
+        size_t pushed = 0;
+        status = exclusiveFile.Write(sink.data(), sink.size(), pushed);
+        if (status != ER_OK) {
+            QCC_LogError(status, ("StoreRequest error during data saving"));
+            return status;
+        }
+        if (pushed != sink.size()) {
+            status = ER_BUS_CORRUPT_KEYSTORE;
+            QCC_LogError(status, ("StoreRequest failed to save data correctly"));
+            return status;
+        }
+        if (!exclusiveFile.Truncate()) {
+            QCC_LogError(status, ("FileSink::Truncate failed"));
         }
         if (status == ER_OK) {
-            QCC_DbgHLPrintf(("StoreRequest to %s done", fileLocker.GetFileName()));
+            QCC_DbgHLPrintf(("StoreRequest to %s done", fileName.c_str()));
         }
         return status;
     }
@@ -160,7 +154,7 @@ class CustomKeyStoreListener : public KeyStoreListener {
   private:
 
     String fileName;
-    FileLocker fileLocker;
+    ExclusiveFile exclusiveFile;
 };
 
 KeyStoreListener* CreateKeyStoreListenerInstance(const char* fname)
