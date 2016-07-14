541ff2ea9a92017fa63c75412289339948e74836
Jorge Martinez Hernandez <jmartinez@affinegy.com>
2016-07-14 14:09:32 -0500

    eSACORE-3101 Update OSX AllJoynChat Sample
    Change-Id: I792a2d0e3ab7b714ec5f8e87cdec13402ea8c858
    Signed-off-by: Jorge Martinez Hernandez <jmartinez@affinegy.com>

--- alljoyn_objc/samples/OSX/AllJoynChat/AllJoynChat/AJNCBusObjectImpl.mm
+++ alljoyn_objc/samples/OSX/AllJoynChat/AllJoynChat/AJNCBusObjectImpl.mm
@@ -24,17 +24,17 @@ AJNCBusObjectImpl::AJNCBusObjectImpl(ajn::BusAttachment &bus, const char *path, 
     const ajn::InterfaceDescription* chatIntf = bus.GetInterface([kInterfaceName UTF8String]);
     assert(chatIntf);
     AddInterface(*chatIntf);
-    
+
     /* Store the Chat signal member away so it can be quickly looked up when signals are sent */
     chatSignalMember = chatIntf->GetMember("Chat");
     assert(chatSignalMember);
 }
 
 /* send a chat signal */
-QStatus AJNCBusObjectImpl::SendChatSignal(const char* msg, ajn::SessionId sessionId) 
+QStatus AJNCBusObjectImpl::SendChatSignal(const char* msg, ajn::SessionId sessionId)
 {
     NSLog(@"SendChatSignal( %s, %u)", msg, sessionId);
-    
+
     ajn::MsgArg chatArg("s", msg);
     uint8_t flags = 0;
     return Signal(NULL, sessionId, *chatSignalMember, &chatArg, 1, 0, flags);
