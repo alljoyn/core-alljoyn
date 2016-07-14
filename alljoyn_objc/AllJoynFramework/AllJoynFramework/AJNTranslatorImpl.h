6ff061f01262ee4dfdeb96b31c4673dc42814e84
Jorge Martinez Hernandez <jmartinez@affinegy.com>
2016-07-14 13:57:58 -0500

    ASACORE-3096 AJNTranslator
    Change-Id: I1b0287bfaa11117b6e8183630dd01a5ae2c3c419
    Signed-off-by: Jorge Martinez Hernandez <jmartinez@affinegy.com>

--- alljoyn_objc/AllJoynFramework/AllJoynFramework/AJNTranslatorImpl.h
+++ alljoyn_objc/AllJoynFramework/AllJoynFramework/AJNTranslatorImpl.h
@@ -19,6 +19,7 @@
 
 #import <Foundation/Foundation.h>
 #import "alljoyn/Translator.h"
+#import <alljoyn/MsgArg.h>
 #import "AJNTranslator.h"
 
 class AJNTranslatorImpl : public ajn::Translator {
@@ -29,15 +30,25 @@ class AJNTranslatorImpl : public ajn::Translator {
   public:
 
     AJNTranslatorImpl(id<AJNTranslator> aDelegate);
-    
+
     virtual ~AJNTranslatorImpl();
- 
+
     virtual size_t NumTargetLanguages();
 
     virtual void GetTargetLanguage(size_t index, qcc::String& ret);
 
-    virtual const char* Translate(const char* sourceLanguage, const char* targetLanguage, const char* source, qcc::String& buffer);
-    
+    virtual const char* Translate(const char *sourceLanguage, const char* targetLanguage, const char* source, qcc::String& buffer);
+
+    virtual QStatus AddTargetLanguage(const char *language, bool *added=NULL);
+
+    virtual QStatus TranslateToMsgArg(const char *sourceLanguage, const char *targetLanguage, const char* sourceText, ajn::MsgArg *&msgarg);
+
+    virtual QStatus AddStringTranslation(const char *id, const char *value, const char *language);
+
+    virtual QStatus AddMsgArgTranslation(const char *id, const ajn::MsgArg *value, const char* language);
+
+    virtual void GetBestLanguage(const char *requested, const qcc::String &defaultLanguage, qcc::String &ret);
+
     id<AJNTranslator> getDelegate();
 
     void setDelegate(id<AJNTranslator> delegate);
