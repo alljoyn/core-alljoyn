f517ae6f6e861eb490122e3e5a8a89f3d157f71d
Marcello Lioy <mlioy@qce.qualcomm.com>
2016-08-01 14:08:18 -0700

    Replace AllSeenAlliance License with Apache License Version 2.0
    http://www.apache.org/licenses/LICENSE-2.0
    
    * Executed in batch by
      https://cgit.allseenalliance.org/extras/automation.git/tree/relicense/bin/do-relicense
    * Patch review took place here:
      https://git.allseenalliance.org/gerrit/#/c/9377/
    
    * For each branch of each project of the AllJoyn Open Source Project
      gerrit repository
    - search / replace old license with new in all matching files
    - search / replace old copyright with new in all files which only
      include copyright
    - assign copyright to Open Connectivity Foundation, AllJoyn Open
      Source Project Contributors and others.
    - propose changes for review by commiters
    - include SPDX
    
    Change-Id: Ie5b29b8fcafeb036a933e73f5bdd3ca861fdf258
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- build_core/tools/bin/make_status.py
+++ build_core/tools/bin/make_status.py
@@ -1,227 +1,31 @@
 #!/usr/bin/python
 
-# Copyright AllSeen Alliance. All rights reserved.
+#    Copyright (c) Open Connectivity Foundation (OCF) and AllJoyn Open
+#    Source Project (AJOSP) Contributors and others.
 #
-#    Permission to use, copy, modify, and/or distribute this software for any
-#    purpose with or without fee is hereby granted, provided that the above
-#    copyright notice and this permission notice appear in all copies.
+#    SPDX-License-Identifier: Apache-2.0
 #
-#    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-#    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-#    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-#    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-#    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-#    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-#    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-# 
-
-import sys
-import os
-import getopt
-import copy
-from xml.dom import minidom
-
-if sys.version_info[:3] < (2,4,0):
-    from sets import Set as set
-
-includeSet = set()
-
-def openFile(name, type):
-    try:
-        return open(name, type)
-    except IOError, e:
-        errno, errStr = e
-        print "I/O Operation on %s failed" % name
-        print "I/O Error(%d): %s" % (errno, errStr)
-        raise e
-
-
-def main(argv=None):
-    """
-    make_status --header <header_file> --code <code_file> --prefix <prefix> --base <base_dir> 
-                [--commenCode<comment_code_file>] [--deps <dep_file>] [--help]
-    Where:
-      <header_file>       - Output "C" header file
-      <code_file>         - Output "C" code
-      <comment_code_file> - Output code that maps status to comment
-      <prefix>            - Prefix which is unique across all projects (see Makefile XXX_DIR)
-      <base_dir>          - Root directory for xi:include directives
-      <dep_file>          - Ouput makefile dependency file
-
-    """
-    global headerOut
-    global codeOut
-    global depOut
-    global isFirst
-    global fileArgs
-    global baseDir
-    global prefix
-    global CommentCodeOut
-
-    headerOut = None
-    codeOut = None
-    depOut = None
-    baseDir = ""
-    prefix = ""
-    CommentCodeOut = None
-
-    if argv is None:
-        argv = sys.argv[1:]
-
-    try:
-        opts, fileArgs = getopt.getopt(argv, "h", ["help", "header=", "code=", "dep=", "base=", "prefix=", "commentCode="])
-        for o, a in opts:
-            if o in ("-h", "--help"):
-                print __doc__
-                return 0
-            if o in ("--header"):
-                headerOut = openFile(a, 'w')
-            if o in ("--code"):
-                codeOut = openFile(a, 'w')
-            if o in ("--dep"):
-                depOut = openFile(a, 'w')
-            if o in ("--base"):
-                baseDir = a
-            if o in ("--prefix"):
-                prefix = a
-            if o in ("--commentCode"):
-                CommentCodeOut = openFile(a, 'w')
-
-        if None == headerOut or None == codeOut:
-            raise Error("Must specify both --header and --code")
-            
-        isFirst = True
-        includeSet.clear()
-
-        writeHeaders()
-
-        for arg in fileArgs:
-            ret = parseAndWriteDocument(arg)
-
-        writeFooters()
-                                
-        if None != headerOut:
-            headerOut.close()
-        if None != codeOut:
-            codeOut.close()
-        if None != CommentCodeOut:
-            CommentCodeOut.close()
-        if None != depOut:
-            depOut.close()
-    except getopt.error, msg:
-        print msg
-        print "for help use --help"
-        return 1
-    except Exception, e:
-        print "ERROR: %s" % e
-        if None != headerOut:
-            os.unlink(headerOut.name)
-        if None != codeOut:
-            os.unlink(codeOut.name)
-        if None != CommentCodeOut:
-            os.unlink(CommentCodeOut.name)
-        if None != depOut:
-            os.unlink(depOut.name)
-        return 1
-    
-    return 0
-
-def writeHeaders():
-    global headerOut
-    global codeOut
-    global depOut
-    global fileArgs
-    
-    if None != depOut:
-        depOut.write("%s %s %s:" % (depOut.name, codeOut.name, headerOut.name))
-        for arg in fileArgs:
-            depOut.write(" \\\n %s" % arg)
-    if None != headerOut:
-        headerOut.write("""
-/**
- * @file
- * This file contains an enumerated list values that QStatus can return
- *
- * Note: This file is generated during the make process.
- */
-/******************************************************************************
- * Copyright AllSeen Alliance. All rights reserved.
- *
- *    Permission to use, copy, modify, and/or distribute this software for any
- *    purpose with or without fee is hereby granted, provided that the above
- *    copyright notice and this permission notice appear in all copies.
- *
- *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- ******************************************************************************/ 
-#ifndef _STATUS_H
-#define _STATUS_H
-/**
- * This @#define allows for setting of visibility support on relevant platforms
- */
-#ifndef AJ_API
-#  if defined(QCC_OS_GROUP_POSIX)
-#    define AJ_API __attribute__((visibility("default")))
-#  else
-#    define AJ_API
-#  endif
-#endif
-
-/** This @#define allows for calling convention redefinition on relevant platforms */
-#ifndef AJ_CALL
-#  if defined(QCC_OS_GROUP_WINDOWS)
-#    define AJ_CALL __stdcall
-#  else
-#    define AJ_CALL
-#  endif
-#endif
-
-/** This @#define allows for calling convention redefinition on relevant platforms */
-#ifndef CDECL_CALL
-#  if defined(QCC_OS_GROUP_WINDOWS)
-#    define CDECL_CALL __cdecl
-#  else
-#    define CDECL_CALL
-#  endif
-#endif
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/**
- * Enumerated list of values QStatus can return
- */
-typedef enum {""")
-
-    if None != codeOut:
-        codeOut.write("""
-/**
- * @file
- * This file contains an enumerated list values that QStatus can return
- *
- * Note: This file is generated during the make process.
- */
-
-/******************************************************************************
- * Copyright AllSeen Alliance. All rights reserved.
- *
- *    Permission to use, copy, modify, and/or distribute this software for any
- *    purpose with or without fee is hereby granted, provided that the above
- *    copyright notice and this permission notice appear in all copies.
- *
- *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#    All rights reserved. This program and the accompanying materials are
+#    made available under the terms of the Apache License, Version 2.0
+#    which accompanies this distribution, and is available at
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Copyright (c) Open Connectivity Foundation and Contributors to AllSeen
+#    Alliance. All rights reserved.
+#
+#    Permission to use, copy, modify, and/or distribute this software for
+#    any purpose with or without fee is hereby granted, provided that the
+#    above copyright notice and this permission notice appear in all
+#    copies.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+#    WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+#    WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+#    AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+#    DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+#    PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+#    TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+#    PERFORMANCE OF THIS SOFTWARE.
  ******************************************************************************/
 
 #include <stdio.h>
