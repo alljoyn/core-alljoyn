0a5c46c94b6fb67661e7c3f83fc5e20cfd4b721a
Jorge Martinez Hernandez <jmartinez@affinegy.com>
2016-08-15 10:52:11 -0500

    ASACORE-3181 Implement various Security 2.0 abstract data types in...
    Objective-C
    
    Change-Id: I4adc0e3275127f8491e2af77d55bfe637c726817
    Signed-off-by: Jorge Martinez Hernandez <jmartinez@affinegy.com>

---
+++ alljoyn_objc/AllJoynFramework/AllJoynFramework/AJNKeyInfoECC.mm
@@ -0,0 +1,116 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright AllSeen Alliance. All rights reserved.
+//
+//    Permission to use, copy, modify, and/or distribute this software for any
+//    purpose with or without fee is hereby granted, provided that the above
+//    copyright notice and this permission notice appear in all copies.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+//    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+//    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+//    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+//    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+//    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+//    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+////////////////////////////////////////////////////////////////////////////////
+
+#import <Foundation/Foundation.h>
+#import "AJNKeyInfoECC.h"
+#import <qcc/KeyInfoECC.h>
+
+@interface AJNObject(Private)
+
+@property (nonatomic) BOOL shouldDeleteHandleOnDealloc;
+
+@end
+
+@interface AJNECCPublicKey(Private)
+
+@property (nonatomic, readonly) qcc::ECCPublicKey *publicKey;
+
+@end
+
+using namespace qcc;
+
+@implementation AJNKeyInfoNISTP256
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        self.handle = new KeyInfoNISTP256();
+    }
+
+    return self;
+}
+
+- (void)dealloc
+{
+    if (self.shouldDeleteHandleOnDealloc) {
+        KeyInfoNISTP256 *pArg = static_cast<KeyInfoNISTP256*>(self.handle);
+        delete pArg;
+        self.handle = nil;
+    }
+}
+
+- (KeyInfoNISTP256*)keyInfo
+{
+    return static_cast<KeyInfoNISTP256*>(self.handle);
+}
+
+- (AJNECCPublicKey*)publicKey
+{
+    return [[AJNECCPublicKey alloc] initWithHandle:(AJNHandle)self.keyInfo->GetPublicKey()];
+}
+
+- (void)setPublicKey:(AJNECCPublicKey *)publicKey
+{
+    self.keyInfo->SetPublicKey(publicKey.publicKey);
+}
+
+- (size_t)publicSize
+{
+    return self.keyInfo->GetPublicSize();
+}
+
+- (size_t)exportSize
+{
+    return self.keyInfo->GetExportSize();
+}
+
+- (BOOL)empty
+{
+    return self.keyInfo->empty() ? YES : NO;
+}
+
+- (QStatus)export:(uint8_t *)buf
+{
+    return self.keyInfo->Export(buf);
+}
+
+- (QStatus)import:(uint8_t *)buf count:(size_t)count
+{
+    return self.keyInfo->Import(buf, count);
+}
+- (BOOL)isEqual:(AJNKeyInfoNISTP256 *)toKeyInfo
+{
+    return self.keyInfo->operator==(*toKeyInfo.keyInfo) ? YES : NO;
+}
+
+- (BOOL)isNotEqual:(AJNKeyInfoNISTP256 *)toKeyInfo
+{
+    return self.keyInfo->operator!=(*toKeyInfo.keyInfo) ? YES : NO;
+}
+
+- (BOOL)isLessThan:(AJNKeyInfoNISTP256 *)thisKeyInfo
+{
+    return self.keyInfo->operator<(*thisKeyInfo.keyInfo) ? YES : NO;
+}
+
+- (NSString*)description
+{
+    return [NSString stringWithCString:self.keyInfo->ToString().c_str() encoding:NSUTF8StringEncoding];
+}
+
+
+@end
