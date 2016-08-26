f9f9c8de4549570152cc074db7b7575f5e2530fc
George Tang <gtang@affinegy.com>
2016-08-26 07:57:45 -0500

    ASACORE-3214 Partial implementation of CryptoECC
    partial implementation of java CryptoECC
    Functions implemented are:
    generateDSAKeyPair
    generateSPEKEKeyPair
    generateDHKeyPair
    getDHPublicKey
    getDHPrivateKey
    getDSAPrivateKey
    getDSAPublicKey
    
    Change-Id: I01c7c86e23d0235bed007797eda9f258ea3c84b2
    Signed-off-by: George Tang <gtang@affinegy.com>

---
+++ alljoyn_java/src/org/alljoyn/bus/common/CryptoECC.java
@@ -0,0 +1,207 @@
+/*
+ * Copyright AllSeen Alliance. All rights reserved.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for any
+ *    purpose with or without fee is hereby granted, provided that the above
+ *    copyright notice and this permission notice appear in all copies.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+package org.alljoyn.bus.common;
+
+import org.alljoyn.bus.BusException;
+import java.util.UUID;
+
+public class CryptoECC {
+
+    /**
+     * The NIST recommended elliptic curve P-256
+     */
+    public static final byte ECC_NIST_P256 = 0;
+
+    /**
+     * Allocate native resources.
+     */
+    public native void create() throws Exception;
+    
+    /** Release native resources. */
+    private synchronized native void destroy();
+
+    /**
+     * Default Constructor
+     */
+    public CryptoECC() {
+        try {
+            create();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Generates the Ephemeral Diffie-Hellman key pair.
+     *
+     * @throws BusException
+     *      ER_OK if the key pair is successfully generated.
+     *      ER_FAIL otherwise
+     *      Other error status.
+     */
+    public native void generateDHKeyPair() throws BusException;
+
+    /**
+     * Generates the ephemeral key pair for EC-SPEKE. The key pair
+     * can then be used with the other DH APIs to compute the shared secret.
+     * @param pw          Password and additional data to use during key generation.
+     * @param pwLen       The byte length of pw.
+     * @param clientGUID  The client's GUID
+     * @param serviceGUID The service's GUID
+     * @throws BusException
+     *      ER_OK if the key pair is successfully generated.
+     *      ER_FAIL otherwise
+     *      Other error status.
+     */
+    public native void generateSPEKEKeyPair(byte[] pw, long pwLen, UUID clientGUID, UUID serviceGUID) throws BusException;
+
+    /**
+     * Generates the Diffie-Hellman shared secret.
+     * @param   peerPublicKey the peer's public key
+     * @param   secret the output shared secret
+     * @throws BusException
+     *      ER_OK if the shared secret is successfully generated.
+     *      ER_FAIL otherwise
+     *      Other error status.
+     */
+    public native void generateSharedSecret(ECCPublicKey peerPublicKey, ECCSecret secret) throws BusException;
+
+    /**
+     * Retrieve the DH public key
+     * @return  the DH public key.  It's a pointer to an internal buffer. Its lifetime is the same as the object's lifetime.
+     * @throws BusException
+     */
+    public native ECCPublicKey getDHPublicKey() throws BusException;
+
+    /**
+     * Assign the DH public key
+     * @param pubKey the public key to copy
+     * @throws BusException
+     */
+    public native void setDHPublicKey(ECCPublicKey pubKey) throws BusException;
+
+    /**
+     * Retrieve the DH private key
+     * @return  the DH private key.  Same lifetime as the object.
+     * @throws BusException
+     */
+    public native ECCPrivateKey getDHPrivateKey() throws BusException;
+
+    /**
+     * Assign the DH private key
+     * @param privateKey the private key to copy
+     * @throws BusException
+     */
+    public native void setDHPrivateKey(ECCPrivateKey privateKey) throws BusException;
+
+    /**
+     * Retrieve the DSA public key
+     * @return  the DSA public key.  Same lifetime as the object.
+     * @throws BusException
+     */
+    public native ECCPublicKey getDSAPublicKey() throws BusException;
+
+    /**
+     * Assign the DSA public key
+     * @param pubKey the public key to copy
+     * @throws BusException
+     */
+    public native void setDSAPublicKey(ECCPublicKey pubKey) throws BusException;
+
+    /**
+     * Retrieve the DSA private key
+     * @return  the DSA private key.  Same lifetime as the object.
+     * @throws BusException
+     */
+    public native ECCPrivateKey getDSAPrivateKey() throws BusException;
+
+    /**
+     * Assign the DSA private key
+     * @param privateKey the private key to copy
+     * @throws BusException
+     */
+    public native void setDSAPrivateKey(ECCPrivateKey privateKey) throws BusException;
+
+    /**
+     * Generates the DSA key pair.
+     *
+     * @throws BusException
+     *      ER_OK if the key pair is successfully generated.
+     *      ER_FAIL otherwise
+     *      Other error status.
+     */
+    public native void generateDSAKeyPair() throws BusException;
+
+    /**
+     * Sign a digest using the DSA key
+     * @param digest The digest to sign
+     * @param len The digest len
+     * @param sig The output signature
+     * @throws BusException
+     *      ER_OK if the signing process succeeds
+     *      ER_FAIL otherwise
+     *      Other error status.
+     */
+    public native void DSASignDigest(byte digest, short len, ECCSignature sig) throws BusException;
+
+    /**
+     * Sign a buffer using the DSA key
+     * @param buf The buffer to sign
+     * @param len The buffer len
+     * @param sig The output signature
+     * @throws BusException
+     *      ER_OK if the signing process succeeds
+     *      ER_FAIL otherwise
+     *      Other error status.
+     */
+    public native void DSASign(byte buf, short len, ECCSignature sig) throws BusException;
+
+    /**
+     * Verify DSA signature of a digest
+     * @param digest The digest to sign
+     * @param len The digest len
+     * @param sig The signature
+     * @throws BusException
+     *          - ER_OK if the signature verification succeeds
+     *          - ER_FAIL otherwise
+     *          - Other error status.
+     */
+    public native void DSAVerifyDigest(byte digest, short len, ECCSignature sig) throws BusException;
+
+    /**
+     * Verify DSA signature of a buffer
+     * @param buf The buffer to sign
+     * @param len The buffer len
+     * @param sig The signature
+     * @throws BusException
+     *      ER_OK if the signature verification succeeds
+     *      ER_FAIL otherwise
+     *      Other error status.
+     */
+    public native void DSAVerify(byte buf, short len, ECCSignature sig) throws BusException;
+
+    /**
+     * Retrieve the ECC curve type.
+     * @return the ECC curve type
+     */
+    public byte getCurveType()
+    {
+        return ECC_NIST_P256;
+    }
+
+    /** The native connection handle. */
+    private long handle;
+}
