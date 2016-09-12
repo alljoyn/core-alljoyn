55f22f8db640dbf23393ad5b330da0f2e41ae0a2
Paul Sigurdson <psigurdson@affinegy.com>
2016-09-12 02:27:54 -0500

    ASACORE-3217: Define interface def classes to support dynamic interfaces
    Implemented bus object/interface definition classes to contain meta info
    to describe interfaces that are defined dynamically (e.g. info could be
    discovered via introspection).
    
    This is in support of the Dynamic Interface feature. This will allow
    bus interfaces to be defined at runtime, where as the Java-binding
    had previously only suported definition of bus interfaces at compile-
    time (via Java class annotations).
    
    Change-Id: I12a10c478d80d07afd30026542e774f9ef2f9541
    Signed-off-by: Paul Sigurdson <psigurdson@affinegy.com>

---
+++ alljoyn_java/src/org/alljoyn/bus/defs/BaseDef.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright AllSeen Alliance. All rights reserved.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for any
+ *    purpose with or without fee is hereby granted, provided that the above
+ *    copyright notice and this permission notice appear in all copies.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package org.alljoyn.bus.defs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Abstract base definition used by interface, method, property, signal, and arg defs.
+ */
+public abstract class BaseDef {
+
+    /* Common annotation name constants */
+
+    /** Deprecated annotation name (used in methods, signals, properties). Valid values: true, false. */
+    public static final String ANNOTATION_DEPRECATED                    = "org.freedesktop.DBus.Deprecated";
+
+    /** NoReply annotation name (used in methods). Valid values: true, false. */
+    public static final String ANNOTATION_METHOD_NOREPLY                = "org.freedesktop.DBus.Method.NoReply";
+
+    /** Signal-emission annotation names (used in signals). Valid values: true, false. */
+    public static final String ANNOTATION_SIGNAL_SESSIONLESS            = "org.alljoyn.Bus.Signal.Sessionless";
+    public static final String ANNOTATION_SIGNAL_SESSIONCAST            = "org.alljoyn.Bus.Signal.Sessioncast";
+    public static final String ANNOTATION_SIGNAL_UNICAST                = "org.alljoyn.Bus.Signal.Unicast";
+    public static final String ANNOTATION_SIGNAL_GLOBAL_BROADCAST       = "org.alljoyn.Bus.Signal.GlobalBroadcast";
+
+    /** EmitsChangedSignal annotation name (used in properties). Valid values: false, true, invalidates, const. */
+    public static final String ANNOTATION_PROPERTY_EMITS_CHANGED_SIGNAL = "org.freedesktop.DBus.Property.EmitsChangedSignal";
+
+    /** VariantTypes annotation name (used in arguments within methods and signals). */
+    public static final String ANNOTATION_ARG_VARIANT_TYPES             = "org.alljoyn.Bus.Arg.VariantTypes";
+
+    /** Secure annotation name (used in interfaces). Valid values: inherit, required, off. */
+    public static final String ANNOTATION_SECURE                        = "org.alljoyn.Bus.Secure";
+
+    /** DocString annotation name-prefix. Full name will include a language tag suffix; e.g. DocString.en */
+    public static final String ANNOTATION_DOC_STRING                    = "org.alljoyn.Bus.DocString";
+
+
+    final private String name;
+    final private Map<String,String> annotationList;  // annotation name/values
+
+
+    /**
+     * Constructor.
+     *
+     * @param name the name of this object.
+     * @throws IllegalArgumentException one or more arguments is invalid.
+     */
+    protected BaseDef(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("Null name");
+        }
+        this.name = name;
+        this.annotationList = new HashMap<String,String>();
+    }
+
+    /**
+     * @return the name of this object.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the list of annotations defined for this object.
+     */
+    public Map<String,String> getAnnotationList() {
+        return annotationList;
+    }
+
+    public void setAnnotationList(Map<String,String> annotations) {
+        annotationList.clear();
+        annotationList.putAll(annotations);
+    }
+
+    /**
+     * Retrieve the value of the named annotation.
+     *
+     * @param name the annotation name
+     * @return the matching annotation value; null if not found.
+     */
+    public String getAnnotation(String name) {
+        return annotationList.get(name);
+    }
+
+    /**
+     * Add the given annotation name/value. If already present then old value is replaced.
+     *
+     * @param name the annotation name
+     * @param value the annotation value
+     */
+    public void addAnnotation(String name, String value) {
+        annotationList.put(name,value);
+    }
+
+    /**
+     * Retrieve the description (a DocString annotation value) for the given language tag.
+     *
+     * @param languageTag IETF abbreviated language tag.
+     * @return the description for the given language.
+     */
+    public String getDescription(String languageTag) {
+        String suffix = (languageTag==null || languageTag.isEmpty()) ? "" : "."+languageTag;
+        return annotationList.get(ANNOTATION_DOC_STRING+suffix);
+    }
+
+    /**
+     * Set the description for the given language.
+     *
+     * @param description the description text rendered in the given language.
+     * @param languageTag IETF abbreviated language tag.
+     */
+    public void setDescription(String description, String languageTag) {
+        String suffix = (languageTag==null || languageTag.isEmpty()) ? "" : "."+languageTag;
+        annotationList.put(ANNOTATION_DOC_STRING+suffix, description);
+    }
+
+}
---
+++ alljoyn_java/src/org/alljoyn/bus/defs/InterfaceDef.java
@@ -0,0 +1,269 @@
+/**
+ * Copyright AllSeen Alliance. All rights reserved.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for any
+ *    purpose with or without fee is hereby granted, provided that the above
+ *    copyright notice and this permission notice appear in all copies.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package org.alljoyn.bus.defs;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
+
+/**
+ * Interface definition used to describe the interface.
+ * Annotations commonly used: DocString, Secure.
+ */
+public class InterfaceDef extends BaseDef {
+
+    /**
+     * Values for the Secure annotation. One of: "inherit", "true", or "off".
+     * Indicates that a given interface is to be secured by an authentication mechanism. If the Secure
+     * annotation is absent, or an unknown value is given, the interface will use a security of inherit.
+     */
+    public enum SecurityPolicy {
+        /** Inherit the security of the object that implements the interface */
+        INHERIT("inherit"),
+        /** Security is required for an interface */
+        TRUE("true"),
+        /** Security does not apply to this interface */
+        OFF("off");
+
+        final public String text;
+
+        SecurityPolicy(String text) {
+            this.text = text;
+        }
+
+        public static SecurityPolicy fromString(String text) {
+            for (SecurityPolicy item: SecurityPolicy.values()) {
+                if (item.text.equalsIgnoreCase(text)) {
+                    return item;
+                }
+            }
+            return null;
+        }
+    }
+
+    final private List<MethodDef> methods;
+    final private List<SignalDef> signals;
+    final private List<PropertyDef> properties;
+
+    private boolean announced;
+    final private String introspectXml; // xml description of the interface (optional)
+
+
+    /**
+     * Constructor.
+     *
+     * @param name the name of the bus interface.
+     * @throws IllegalArgumentException one or more arguments is invalid.
+     */
+    public InterfaceDef(String name) {
+        this(name, false, null);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param name the name of the bus interface.
+     * @param isAnnounced whether the interface is announced
+     * @param introspectXml [optional] XML describing the interface and its methods, signals, properties.
+     * @throws IllegalArgumentException one or more arguments is invalid.
+     */
+    public InterfaceDef(String name, boolean isAnnounced, String introspectXml) {
+        super(name);
+        this.methods = new ArrayList<MethodDef>();
+        this.signals = new ArrayList<SignalDef>();
+        this.properties = new ArrayList<PropertyDef>();
+        this.announced = isAnnounced;
+        this.introspectXml = introspectXml;
+    }
+
+    /**
+     * @return whether the bus interface is secure (indicated via a Secure annotation).
+     */
+    public boolean isSecureRequired() {
+        String value = getAnnotation(ANNOTATION_SECURE);
+        return value != null && value.equalsIgnoreCase(SecurityPolicy.TRUE.text);
+    }
+
+    /**
+     * Returns whether the security policy is inherit (indicated via a Secure annotation).
+     * If the Secure annotation is absent, or an unknown value is present, the interface
+     * will also use a security of inherit.
+     *
+     * @return whether the bus interface will inherit the security of the object that
+     *         implements the interface.
+     */
+    public boolean isSecureInherit() {
+        String value = getAnnotation(ANNOTATION_SECURE);
+        return value == null || value.equalsIgnoreCase(SecurityPolicy.INHERIT.text) ||
+                !(value.equalsIgnoreCase(SecurityPolicy.TRUE.text)
+                        || value.equalsIgnoreCase(SecurityPolicy.OFF.text)
+                        || value.equalsIgnoreCase("false"));  // accepts 'false' as an alias for 'off'
+    }
+
+    /**
+     * @return whether the bus interface is announced.
+     */
+    public boolean isAnnounced() {
+        return announced;
+    }
+
+    public void setAnnounced(boolean isAnnounced) {
+        this.announced = isAnnounced;
+    }
+
+    /**
+     * @return the interface's contained method definitions.
+     */
+    public List<MethodDef> getMethods() {
+        return methods;
+    }
+
+    public void setMethods(List<MethodDef> list) {
+        methods.clear();
+        methods.addAll(list);
+    }
+
+    public void addMethod(MethodDef method) {
+        methods.add(method);
+    }
+
+    /**
+     * @return the interface's contained signal definitions.
+     */
+    public List<SignalDef> getSignals() {
+        return signals;
+    }
+
+    public void setSignals(List<SignalDef> list) {
+        signals.clear();
+        signals.addAll(list);
+    }
+
+    public void addSignal(SignalDef signal) {
+        signals.add(signal);
+    }
+
+    /**
+     * @return the interface's contained property definitions.
+     */
+    public List<PropertyDef> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(List<PropertyDef> list) {
+        properties.clear();
+        properties.addAll(list);
+    }
+
+    public void addProperty(PropertyDef property) {
+        properties.add(property);
+    }
+
+    /**
+     * @return the xml description of the interface. Null if not provided.
+     */
+    public String getIntrospectXml() {
+        return introspectXml;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("InterfaceDef {name=");
+        builder.append(getName());
+        for ( MethodDef m : methods ) {
+            builder.append("\n\t" + m.toString());
+        }
+        for ( SignalDef s : signals ) {
+            builder.append("\n\t" + s.toString());
+        }
+        for ( PropertyDef p : properties ) {
+            builder.append("\n\t" + p.toString());
+        }
+        builder.append("}");
+        return builder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        InterfaceDef that = (InterfaceDef) o;
+
+        if (!getName().equals(that.getName())) return false;
+        if (methods.size() != that.methods.size()) return false;
+        if (signals.size() != that.signals.size()) return false;
+        if (properties.size() != that.properties.size()) return false;
+
+        // Sort and test whether methods lists are equal (use copy to preserve original order)
+        List<MethodDef> thisMethods = new ArrayList<MethodDef>(methods);
+        List<MethodDef> thatMethods = new ArrayList<MethodDef>(that.methods);
+        Collections.sort(thisMethods, new Comparator<MethodDef>() {
+            public int compare(MethodDef one, MethodDef two){
+                return one.getName().compareTo(two.getName());
+            }
+        });
+        Collections.sort(thatMethods, new Comparator<MethodDef>() {
+            public int compare(MethodDef one, MethodDef two){
+                return one.getName().compareTo(two.getName());
+            }
+        });
+        if (!thisMethods.equals(thatMethods)) return false;
+
+        // Sort and test whether signals lists are equal (use copy to preserve original order)
+        List<SignalDef> thisSignals = new ArrayList<SignalDef>(signals);
+        List<SignalDef> thatSignals = new ArrayList<SignalDef>(that.signals);
+        Collections.sort(thisSignals, new Comparator<SignalDef>() {
+            public int compare(SignalDef one, SignalDef two){
+                return one.getName().compareTo(two.getName());
+            }
+        });
+        Collections.sort(thatSignals, new Comparator<SignalDef>() {
+            public int compare(SignalDef one, SignalDef two){
+                return one.getName().compareTo(two.getName());
+            }
+        });
+        if (!thisSignals.equals(thatSignals)) return false;
+
+        // Sort and test whether properties lists are equal (use copy to preserve original order)
+        List<PropertyDef> thisProperties = new ArrayList<PropertyDef>(properties);
+        List<PropertyDef> thatProperties = new ArrayList<PropertyDef>(that.properties);
+        Collections.sort(thisProperties, new Comparator<PropertyDef>() {
+            public int compare(PropertyDef one, PropertyDef two){
+                return one.getName().compareTo(two.getName());
+            }
+        });
+        Collections.sort(thatProperties, new Comparator<PropertyDef>() {
+            public int compare(PropertyDef one, PropertyDef two){
+                return one.getName().compareTo(two.getName());
+            }
+        });
+        return thisProperties.equals(thatProperties);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName().hashCode();
+        result = 31 * result + methods.hashCode();
+        result = 31 * result + signals.hashCode();
+        result = 31 * result + properties.hashCode();
+        return result;
+    }
+
+}
