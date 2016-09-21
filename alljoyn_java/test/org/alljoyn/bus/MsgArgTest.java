a086babd41014004be248e48397d044fa340386b
Paul Sigurdson <psigurdson@affinegy.com>
2016-09-21 00:45:14 -0500

    ASACORE-3223: Allow dynamic types (not preknown) to support dynamic interfaces
    MsgArg:
    
    Add support for unmarshal of unspecified array type. The Type argument
    would be given as Object, and the actual msgArg type would then be
    determined based on the msgArg signature.
    
    Add support for unmarshal of unspecified struct type. The Type argument
    would be given as Object[], and the struct's individual field types would
    then be determined based on the msgArg signature.
    
    Add method to determine an object Type given its signature. Method converts
    a given signature to its corresponding Type class.
    
    Signature:
    
    Need to support an unspecified struct given as an object array; i.e. Object[].
    This is necessary since the actual struct type is not necessarily pre-known
    or even defined within the Java VM.
    
    Variant:
    
    Get wrapped object value by using signature to then unmarshal the native
    msgarg (if object value not available directly).
    
    Change-Id: I043a759a9cb6108189264d9e760ab3028aa3ec91
    Signed-off-by: Paul Sigurdson <psigurdson@affinegy.com>

---
+++ alljoyn_java/test/org/alljoyn/bus/MsgArgTest.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright AllSeen Alliance. All rights reserved.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for any
+ *    purpose with or without fee is hereby granted, provided that the above
+ *    copyright notice and this permission notice appear in all copies.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package org.alljoyn.bus;
+
+import org.alljoyn.bus.MsgArg;
+import org.alljoyn.bus.Variant;
+
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.lang.reflect.ParameterizedType;
+
+import junit.framework.TestCase;
+
+public class MsgArgTest extends TestCase {
+    public MsgArgTest(String name) {
+        super(name);
+    }
+
+    static {
+        System.loadLibrary("alljoyn_java");
+    }
+
+    public void testToTypeFromSignature() throws BusException {
+        assertEquals(Object[].class, MsgArg.toType("sbi"));   // struct
+        assertEquals(Object[].class, MsgArg.toType("(sbi)")); // struct
+
+        ParameterizedType type = (ParameterizedType) MsgArg.toType("a{sv}"); // dict-entry array (map)
+        assertEquals(HashMap.class, type.getRawType());
+        assertEquals(String.class, type.getActualTypeArguments()[0]);
+        assertEquals(Variant.class, type.getActualTypeArguments()[1]);
+
+        type = (ParameterizedType) MsgArg.toType("{sv}"); // dict entry
+        assertEquals(AbstractMap.SimpleEntry.class, type.getRawType());
+        assertEquals(String.class, type.getActualTypeArguments()[0]);
+        assertEquals(Variant.class, type.getActualTypeArguments()[1]);
+
+        assertEquals(Boolean.class, MsgArg.toType("b"));
+        assertEquals(Byte.class, MsgArg.toType("y"));
+        assertEquals(Short.class, MsgArg.toType("n"));      // int16
+        assertEquals(Short.class, MsgArg.toType("q"));      // uint16
+        assertEquals(Integer.class, MsgArg.toType("i"));    // int32
+        assertEquals(Integer.class, MsgArg.toType("u"));    // uint32
+        assertEquals(Long.class, MsgArg.toType("x"));       // int64
+        assertEquals(Long.class, MsgArg.toType("t"));       // uint64
+        assertEquals(Double.class, MsgArg.toType("d"));
+        assertEquals(String.class, MsgArg.toType("s"));
+        assertEquals(String.class, MsgArg.toType("g"));     // signature
+        assertEquals(String.class, MsgArg.toType("o"));     // object path
+        assertEquals(Variant.class, MsgArg.toType("v"));
+        assertEquals(Long.class, MsgArg.toType("h"));       // handle
+
+        assertEquals(Boolean[].class, MsgArg.toType("ab"));
+        assertEquals(Byte[].class, MsgArg.toType("ay"));
+        assertEquals(Short[].class, MsgArg.toType("an"));   // array int16
+        assertEquals(Short[].class, MsgArg.toType("aq"));   // array uint16
+        assertEquals(Integer[].class, MsgArg.toType("ai")); // array int32
+        assertEquals(Integer[].class, MsgArg.toType("au")); // array uint32
+        assertEquals(Long[].class, MsgArg.toType("ax"));    // array int64
+        assertEquals(Long[].class, MsgArg.toType("at"));    // array uint64
+        assertEquals(Double[].class, MsgArg.toType("ad"));
+
+        // Be careful of the MsgArg.ALLJOYN_*** constants; they are int values (actually char stored in int constants)
+        assertEquals(String.class, MsgArg.toType(""+(char)MsgArg.ALLJOYN_STRING));
+        assertEquals(Boolean.class, MsgArg.toType(""+(char)MsgArg.ALLJOYN_BOOLEAN));
+        assertEquals(Integer.class, MsgArg.toType(""+(char)MsgArg.ALLJOYN_INT32));
+
+        try {
+            MsgArg.toType("");
+            fail("Expected BusException due to empty signature");
+        } catch (BusException e) {
+        }
+
+        try {
+            MsgArg.toType(null);
+            fail("Expected BusException due to null signature");
+        } catch (BusException e) {
+        }
+
+        try {
+            MsgArg.toType("Z");
+            fail("Expected BusException due to unimplemented signature");
+        } catch (BusException e) {
+        }
+
+        try {
+            MsgArg.toType("115");
+            fail("Expected BusException due to unimplemented signature");
+        } catch (BusException e) {
+        }
+    }
+
+}
