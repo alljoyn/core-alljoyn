f74cd67e5205e3d2342437ee58b0efb046d4be41
Torsten May <t.may@technisat.de>
2016-10-21 13:04:05 +0200

    ASACORE-3200 Thread safty improved
    In order to to remove various possible data rase situations I added:
    - AtomicFetch(..) for all supported platforms
    - atomic class wrapper for int32_t, uint32_t and bool
    - make use of atomic_bool when necessary
    
    I'm going to add relevant thread sanitizer outputs to the gerit
    change set.
    
    Change-Id: I29bab5d5c2617bc75d4c236526c2fdb1675d1571
    Signed-off-by: Torsten May <t.may@technisat.de>

--- common/inc/qcc/atomic.h
+++ common/inc/qcc/atomic.h
@@ -34,4 +34,140 @@
 #error No OS GROUP defined.
 #endif
 
+#include <stdio.h>
+
+namespace qcc {
+
+class atomic_int32 {
+  public:
+    atomic_int32() {
+        atomic_int32::operator=(0);
+    };
+
+    atomic_int32(const int32_t val) {
+        atomic_int32::operator=(val);
+    };
+
+    atomic_int32& operator=(int32_t val) {
+        AtomicSet(&mVal, val);
+        return *this;
+    };
+
+    int32_t operator++(int) {
+        int32_t old = AtomicFetch(&mVal);
+        IncrementAndFetch(&mVal);
+        return old;
+    };
+
+    atomic_int32& operator++() {
+        IncrementAndFetch(&mVal);
+        return *this;
+    };
+
+    int32_t operator--(int) {
+        int32_t old = AtomicFetch(&mVal);
+        DecrementAndFetch(&mVal);
+        return old;
+    };
+
+    atomic_int32& operator--() {
+        DecrementAndFetch(&mVal);
+        return *this;
+    };
+
+    explicit operator int32_t() {
+        return AtomicFetch(&mVal);
+    };
+
+  private:
+    atomic_int32& operator=(const atomic_int32&) = delete;
+    atomic_int32& operator=(const atomic_int32&) volatile = delete;
+    atomic_int32(atomic_int32&) = delete;
+    atomic_int32(atomic_int32 &&) = delete;
+
+    int32_t mVal;
+};
+
+class atomic_uint32 {
+  public:
+    atomic_uint32() {
+        atomic_uint32::operator=(0);
+    };
+
+    atomic_uint32(const uint32_t val) {
+        atomic_uint32::operator=(val);
+    };
+
+    atomic_uint32& operator=(uint32_t val) {
+        AtomicSet(&mVal, val);
+        return *this;
+    };
+
+    uint32_t operator++(int) {
+        uint32_t old = AtomicFetch(&mVal);
+        IncrementAndFetch(&mVal);
+        return old;
+    };
+
+    atomic_uint32& operator++() {
+        IncrementAndFetch(&mVal);
+        return *this;
+    };
+
+    uint32_t operator--(int) {
+        uint32_t old = AtomicFetch(&mVal);
+        DecrementAndFetch(&mVal);
+        return old;
+    };
+
+    atomic_uint32& operator--() {
+        DecrementAndFetch(&mVal);
+        return *this;
+    };
+
+    explicit operator uint32_t() {
+        return AtomicFetch(&mVal);
+    };
+
+  private:
+    atomic_uint32& operator=(const atomic_uint32&) = delete;
+    atomic_uint32& operator=(const atomic_uint32&) volatile = delete;
+    atomic_uint32(atomic_int32&) = delete;
+    atomic_uint32(atomic_int32 &&) = delete;
+
+    int32_t mVal;
+};
+
+
+class atomic_bool {
+  public:
+    atomic_bool() {
+        atomic_bool::operator=(false);
+    };
+
+    atomic_bool(const bool val) {
+        atomic_bool::operator=(val);
+    };
+
+    atomic_bool& operator=(bool val) {
+        volatile int32_t v = val;
+        AtomicSet(&mVal, v);
+        return *this;
+    };
+
+    explicit operator bool() const {
+        return AtomicFetch(&mVal);
+    };
+
+  private:
+    atomic_bool& operator=(const atomic_bool&) = delete;
+    atomic_bool& operator=(const atomic_bool&) volatile = delete;
+    atomic_bool(atomic_bool&) = delete;
+    atomic_bool(atomic_bool &&) = delete;
+
+    int32_t mVal;
+};
+
+} //namespace qcc
+
 #endif
