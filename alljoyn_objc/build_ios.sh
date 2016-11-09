e11cef970cbafb23d55fced0e00fc8483be146fb
Alexey Solovyov <alexeys@affinegy.com>
2016-11-09 13:12:58 +0600

    ASACORE-3392 ASACORE-3393 Create script for building Core for iOS/OSX
    Apple has four operation systems based on darwin: macOS, iOS, tvOS,
    watchOS. Last three run on arm-processors, and simulators for these
    OS run on x86. Also executable files usually should have universal
    format (multiarchitecture binary), for iOS it is armv7+arm64.
    Therefore, darwin + CPU architecture do not identify the operating
    system uniquely.
    
    This commit adds OS=iOS and CPU=universal to SConscript files and
    SH-scripts for easy AllJoyn Core building.
    
    alljoyn_objc/build_ios.sh - build AllJoyn Core for iOS as universal
    binary (armv7, arm64) with bitcode support
    
    alljoyn_objc/build_macos.sh - build AllJoyn Core for macOS (x86_64)
    
    Change-Id: I9ac4ed476d4e8e3720bfb9cd5cf5f64eead48f50
    Signed-off-by: Alexey Solovyov <alexeys@affinegy.com>

---
+++ alljoyn_objc/build_ios.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# Copyright AllSeen Alliance. All rights reserved.
+#
+#    Permission to use, copy, modify, and/or distribute this software for any
+#    purpose with or without fee is hereby granted, provided that the above
+#    copyright notice and this permission notice appear in all copies.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+#    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+#    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+#    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+#    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+#    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+#    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+cd ..
+SDKROOT_IOS_SIMULATOR=`xcodebuild -version -sdk iphonesimulator Path`
+SDKROOT_IOS=`xcodebuild -version -sdk iphoneos Path`
+CPU_NUM=`sysctl -n hw.ncpu`
+
+echo "Building AllJoyn Core for iOS"
+echo "SDKROOT_IOS_SIMULATOR: $SDKROOT_IOS_SIMULATOR"
+echo "SDKROOT_IOS: $SDKROOT_IOS"
+echo "CPU_NUM: $CPU_NUM"
+
+set -e
+
+export CONFIGURATION=release
+export PLATFORM_NAME=iphoneos
+scons -u --jobs $CPU_NUM OS=iOS VARIANT=release CPU=universal CRYPTO=builtin BR=on BINDINGS="cpp" WS=off SDKROOT=$SDKROOT_IOS
+export PLATFORM_NAME=iphonesimulator
+scons -u --jobs $CPU_NUM OS=iOS VARIANT=release CPU=universal CRYPTO=builtin BR=on BINDINGS="cpp" WS=off SDKROOT=$SDKROOT_IOS_SIMULATOR
+
+export CONFIGURATION=debug
+export PLATFORM_NAME=iphoneos
+scons -u --jobs $CPU_NUM OS=iOS VARIANT=debug CPU=universal CRYPTO=builtin BR=on BINDINGS="cpp" WS=off SDKROOT=$SDKROOT_IOS
+export PLATFORM_NAME=iphonesimulator
+scons -u --jobs $CPU_NUM OS=iOS VARIANT=debug CPU=universal CRYPTO=builtin BR=on BINDINGS="cpp" WS=off SDKROOT=$SDKROOT_IOS_SIMULATOR
