4c2d0ed9fcf978509236b944260237081604a107
George Tang <gtang@affinegy.com>
2016-11-23 20:59:47 -0600

    ASACORE-3472 PermissionConfigurator Statement Test
    Fixed a typo in the JNI code.
    
    Unit tests cover all functions of PermissionConfigurator
    except getConnectedPeerPublicKey, since this function is used with
    CredentialAccessor and since CredentialAccessor is not available in the
    Java API. Credential Accessor extracts the bus guid and the bus guid
    is used as a parameter for getConnectedPeerPublicKey.
    
    Change-Id: Ie23d80e3864538413d63efb16104888270a556d9
    Signed-off-by: George Tang <gtang@affinegy.com>

---
+++ alljoyn_java/test/org/alljoyn/bus/ManifestTestHelper.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright AllSeen Alliance. All rights reserved.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for any
+ *    purpose with or without fee is hereby granted, provided that the above
+ *    copyright notice and this permission notice appear in all copies.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package org.alljoyn.bus;
+
+public class ManifestTestHelper {
+    public static final String MANIFEST_ALL_INCLUSIVE =
+        "<manifest>" +
+        "<node name=\"*\">" +
+        "<interface name=\"*\">" +
+        "<method name=\"*\">" +
+        "<annotation name=\"org.alljoyn.Bus.Action\" value=\"Provide\"/>" +
+        "<annotation name=\"org.alljoyn.Bus.Action\" value=\"Modify\"/>" +
+        "</method>" +
+        "<signal name=\"*\">" +
+        "<annotation name=\"org.alljoyn.Bus.Action\" value=\"Provide\"/>" +
+        "<annotation name=\"org.alljoyn.Bus.Action\" value=\"Observe\"/>" +
+        "</signal>" +
+        "<property name=\"*\">" +
+        "<annotation name=\"org.alljoyn.Bus.Action\" value=\"Provide\"/>" +
+        "<annotation name=\"org.alljoyn.Bus.Action\" value=\"Observe\"/>" +
+        "<annotation name=\"org.alljoyn.Bus.Action\" value=\"Modify\"/>" +
+        "</property>" +
+        "</interface>" +
+        "</node>" +
+        "</manifest>";
+}
