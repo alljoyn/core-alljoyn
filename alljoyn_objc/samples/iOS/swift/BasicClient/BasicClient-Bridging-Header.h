98c93dc2d44a016c47a74de67dd50c7129561df9
Olga Konoreva <olgak@affinegy.com>
2016-11-30 16:18:24 +0600

    Implement swift BasicClient sample
    Change-Id: Ie58b0f8826b7d52760be0c07031fdba08718d488
    Signed-off-by: Olga Konoreva <olgak@affinegy.com>

---
+++ alljoyn_objc/samples/iOS/swift/BasicClient/BasicClient-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+#import "AJNInit.h"
+#import "AJNBusAttachment.h"
+#import "AJNInterfaceDescription.h"
+#import "AJNSessionOptions.h"
+#import "AJNVersion.h"
+#import "AJNBasicObject.h"
