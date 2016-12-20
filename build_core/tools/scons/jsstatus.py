5a41c07d9456040aa85aa8e0ba6eb7d59cd34f2a
C.J. Collier <cjcollier@linuxfoundation.org>
2016-12-20 07:24:02 -0800

    Replace AllSeenAlliance License with Apache License Version 2.0
    http://www.apache.org/licenses/LICENSE-2.0
    
    * Executed in batch by
      https://cgit.allseenalliance.org/extras/automation.git/tree/relicense/bin/do-relicense
    * Patch review took place here:
      https://git.allseenalliance.org/gerrit/#/c/9377/
    
    * For each branch of each project of the AllJoyn Open Source Project
      gerrit repository
    - search / replace old license with new in all matching files
    - search / replace old copyright with new in all files which only
      include copyright
    - assign copyright to Open Connectivity Foundation, AllJoyn Open
      Source Project Contributors and others.
    - propose changes for review by commiters
    - include SPDX
    
    Change-Id: I6879408832146ffec68fb7ce914698b14dab28eb
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- build_core/tools/scons/jsstatus.py
+++ build_core/tools/scons/jsstatus.py
@@ -1,129 +1,31 @@
 #!/usr/bin/python
 
-# Copyright AllSeen Alliance. All rights reserved.
+#    Copyright (c) Open Connectivity Foundation (OCF) and AllJoyn Open
+#    Source Project (AJOSP) Contributors and others.
 #
-#    Permission to use, copy, modify, and/or distribute this software for any
-#    purpose with or without fee is hereby granted, provided that the above
-#    copyright notice and this permission notice appear in all copies.
+#    SPDX-License-Identifier: Apache-2.0
 #
-#    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-#    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-#    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-#    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-#    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-#    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-#    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-# 
-
-import sys
-import os
-import getopt
-from xml.dom import minidom
-
-if sys.version_info[:3] < (2,4,0):
-    from sets import Set as set
-
-includeSet = set()
-
-def openFile(name, type):
-    try:
-        return open(name, type)
-    except IOError, e:
-        errno, errStr = e
-        print "I/O Operation on %s failed" % name
-        print "I/O Error(%d): %s" % (errno, errStr)
-        raise e
-
-
-def main(argv=None):
-    """
-    make_status --code <code_file> --base <base_dir> [--deps <dep_file>] [--help]
-    Where:
-    <code_file>   - Output "C++" code
-    <base_dir>    - Root directory for xi:include directives
-    <dep_file>    - Ouput makefile dependency file
-
-    """
-    global codeOut
-    global depOut
-    global isFirst
-    global fileArgs
-    global baseDir
-
-    codeOut = None
-    depOut = None
-    isFirst = True
-    baseDir = ""
-
-    if argv is None:
-        argv = sys.argv[1:]
-
-    try:
-        opts, fileArgs = getopt.getopt(argv, "h", ["help", "code=", "dep=", "base="])
-        for o, a in opts:
-            if o in ("-h", "--help"):
-                print __doc__
-                return 0
-            if o in ("--code"):
-                codeOut = openFile(a, 'w')
-            if o in ("--dep"):
-                depOut = openFile(a, 'w')
-            if o in ("--base"):
-                baseDir = a
-
-        if None == codeOut:
-            raise Error("Must specify --code")
-
-        writeHeaders()
-
-        for arg in fileArgs:
-            ret = parseDocument(arg)
-            
-        writeFooters()
-
-        if None != codeOut:
-            codeOut.close()
-        if None != depOut:
-            depOut.close()
-    except getopt.error, msg:
-        print msg
-        print "for help use --help"
-        return 1
-    except Exception, e:
-        print "ERROR: %s" % e
-        if None != codeOut:
-            os.unlink(codeOut.name)
-        if None != depOut:
-            os.unlink(depOut.name)
-        return 1
-    
-    return 0
-
-def writeHeaders():
-    global codeOut
-    global depOut
-    global fileArgs
-
-    if None != depOut:
-        depOut.write("%s %s %s:" % (depOut.name, codeOut.name))
-        for arg in fileArgs:
-            depOut.write(" \\\n %s" % arg)
-    if None != codeOut:
-        codeOut.write("""/* This file is auto-generated.  Do not modify. */
-/*
- * Copyright AllSeen Alliance. All rights reserved.
- *
- *    Permission to use, copy, modify, and/or distribute this software for any
- *    purpose with or without fee is hereby granted, provided that the above
- *    copyright notice and this permission notice appear in all copies.
- *
- *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#    All rights reserved. This program and the accompanying materials are
+#    made available under the terms of the Apache License, Version 2.0
+#    which accompanies this distribution, and is available at
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Copyright (c) Open Connectivity Foundation and Contributors to AllSeen
+#    Alliance. All rights reserved.
+#
+#    Permission to use, copy, modify, and/or distribute this software for
+#    any purpose with or without fee is hereby granted, provided that the
+#    above copyright notice and this permission notice appear in all
+#    copies.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+#    WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+#    WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+#    AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+#    DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+#    PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+#    TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+#    PERFORMANCE OF THIS SOFTWARE.
  */
 #include "BusErrorInterface.h"
 
