9d673e6c0a401733fba655d79b5c8df9c3295c54
C.J. Collier <cjcollier@linuxfoundation.org>
2016-12-20 07:23:06 -0800

    Replace AllSeenAlliance License with Apache License Version 2.0
    http://www.apache.org/licenses/LICENSE-2.0
    
    * Executed in batch by
      https://cgit.allseenalliance.org/extras/automation.git/tree/relicense/bin/do-relicense
    * Patch review took place here:
      https://git.allseenalliance.org/gerrit/#/c/9377/
    
    * For each branch of each project of the AllJoyn Open Source Project
      gerrit repository
    - search / replace old license with new in all matching files
    - search / replace old copyright with new in all files which only
      include copyright
    - assign copyright to Open Connectivity Foundation, AllJoyn Open
      Source Project Contributors and others.
    - propose changes for review by commiters
    - include SPDX
    
    Change-Id: I029722aa414d24c369780301985472c7c65a0c25
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- alljoyn_core/test/scripts/CAPI_Test_Cert_Generation.cmd
+++ alljoyn_core/test/scripts/CAPI_Test_Cert_Generation.cmd
@@ -1,18 +1,31 @@
 @echo off
 
-REM Copyright AllSeen Alliance. All rights reserved.
+REM    Copyright (c) Open Connectivity Foundation (OCF) and AllJoyn Open
+REM    Source Project (AJOSP) Contributors and others.
 REM
-REM    Permission to use, copy, modify, and/or distribute this software for any
-REM    purpose with or without fee is hereby granted, provided that the above
-REM    copyright notice and this permission notice appear in all copies.
+REM    SPDX-License-Identifier: Apache-2.0
 REM
-REM    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-REM    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-REM    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-REM    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-REM    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-REM    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-REM    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+REM    All rights reserved. This program and the accompanying materials are
+REM    made available under the terms of the Apache License, Version 2.0
+REM    which accompanies this distribution, and is available at
+REM    http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM    Copyright (c) Open Connectivity Foundation and Contributors to AllSeen
+REM    Alliance. All rights reserved.
+REM
+REM    Permission to use, copy, modify, and/or distribute this software for
+REM    any purpose with or without fee is hereby granted, provided that the
+REM    above copyright notice and this permission notice appear in all
+REM    copies.
+REM
+REM    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+REM    WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+REM    WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+REM    AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+REM    DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+REM    PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+REM    TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+REM    PERFORMANCE OF THIS SOFTWARE.
 
 REM This script generates a CA and client certificate and associated private keys, and then
 REM exports them as PKCS#12 PFX files. Unfortunately there aren't any convenient built-in
