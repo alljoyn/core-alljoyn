0684d784eca3e9b7d30553e0c2979c4403174547
Vincent Du <vdu@affinegy.com>
2017-01-19 13:02:22 -0600

    ASACORE-3501: Remove unsafe type cast to Crypto_AES:Block
    Added a new constructor for Crypto_AES::Block class so the type cast
    from byte array or void* could be avoided. Note that this change would
    introduce small resource and performance penalty as data are being
    copied into a Block object instance, comparing to type cast.
    
    Also added a Crypto_AES class static variable to stipulate the Block
    length since usage of size(Block) equally depends on the implementaion
    of Block.
    
    Change-Id: I18bed2b606ef8c9286d24e0b219736192a567c47
    Signed-off-by: Vincent Du <vdu@affinegy.com>

--- common/crypto/openssl/CryptoAES.cc
+++ common/crypto/openssl/CryptoAES.cc
@@ -58,6 +58,9 @@ static void Trace(const char* tag, void* data, size_t len)
 #define Trace(x, y, z)
 #endif
 
+/* definition of CryptoAES_BLOCK_LEN */
+const size_t Crypto_AES::BLOCK_LEN;
+
 struct Crypto_AES::KeyState {
     AES_KEY key;
 };
@@ -136,17 +139,18 @@ QStatus Crypto_AES::Encrypt(const void* in, size_t len, Block* out, uint32_t num
     /*
      * Check for a partial final block
      */
-    size_t partial = len % sizeof(Block);
+    size_t partial = len % Crypto_AES::BLOCK_LEN;
+    Block inBlock(static_cast<const uint8_t*>(in), 16);
     if (partial) {
         numBlocks--;
-        status = Encrypt((Block*)in, out, numBlocks);
+        status = Encrypt(&inBlock, out, numBlocks);
         if (status == ER_OK) {
             Block padBlock;
-            memcpy(&padBlock, (static_cast<const uint8_t*>(in) + numBlocks * sizeof(Block)), partial);
+            memcpy(padBlock.data, (static_cast<const uint8_t*>(in) + numBlocks * Crypto_AES::BLOCK_LEN), partial);
             status = Encrypt(&padBlock, out + numBlocks, 1);
         }
     } else {
-        status = Encrypt((const Block*)in, out, numBlocks);
+        status = Encrypt(&inBlock, out, numBlocks);
     }
     return status;
 }
@@ -216,11 +220,11 @@ static void Compute_CCM_AuthField(AES_KEY* key, Crypto_AES::Block& T, uint8_t M,
          */
         AES_cbc_encrypt(A.data, T.data, sizeof(T.data), key, ivec.data, AES_ENCRYPT);
         Trace("After AES: ", T.data, sizeof(T.data));
-        while (aadLen >= sizeof(Crypto_AES::Block)) {
+        while (aadLen >= Crypto_AES::BLOCK_LEN) {
             AES_cbc_encrypt(aadData, T.data, sizeof(T.data), key, ivec.data, AES_ENCRYPT);
             Trace("After AES: ", T.data, sizeof(T.data));
-            aadData += sizeof(Crypto_AES::Block);
-            aadLen -= sizeof(Crypto_AES::Block);
+            aadData += Crypto_AES::BLOCK_LEN;
+            aadLen -= Crypto_AES::BLOCK_LEN;
         }
         if (aadLen) {
             memcpy(A.data, aadData, aadLen);
@@ -234,11 +238,11 @@ static void Compute_CCM_AuthField(AES_KEY* key, Crypto_AES::Block& T, uint8_t M,
      * Continue computing CBC-MAC over the message data.
      */
     if (mLen) {
-        while (mLen >= sizeof(Crypto_AES::Block)) {
+        while (mLen >= Crypto_AES::BLOCK_LEN) {
             AES_cbc_encrypt(mData, T.data, sizeof(T.data), key, ivec.data, AES_ENCRYPT);
             Trace("After AES: ", T.data, sizeof(T.data));
-            mData += sizeof(Crypto_AES::Block);
-            mLen -= sizeof(Crypto_AES::Block);
+            mData += Crypto_AES::BLOCK_LEN;
+            mLen -= Crypto_AES::BLOCK_LEN;
         }
         if (mLen) {
             Crypto_AES::Block final;
