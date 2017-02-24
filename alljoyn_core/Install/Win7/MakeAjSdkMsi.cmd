edc6128573f32d68cca69ea022d34921a0e3abe1
C.J. Collier <cjcollier@linuxfoundation.org>
2017-02-23 23:12:48 -0800

    Replace AllSeenAlliance License with Apache License Version 2.0
    http://www.apache.org/licenses/LICENSE-2.0
    
    * Executed in batch by
      https://cgit.allseenalliance.org/extras/automation.git/tree/relicense/bin/do-relicense
    * Patch review took place here:
      https://git.allseenalliance.org/gerrit/#/c/9377/
    
    * For each branch of each project of the AllJoyn Open Source Project
      gerrit repository
    - search / replace old license with new in all matching files
    - search / replace old copyright with new in all files which only
      include copyright
    - assign copyright to Open Connectivity Foundation, AllJoyn Open
      Source Project Contributors and others.
    - propose changes for review by commiters
    - include SPDX
    
    Change-Id: Id93586a5b83ce0a1ae455b0c067169a6de61a8bd
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- alljoyn_core/Install/Win7/MakeAjSdkMsi.cmd
+++ alljoyn_core/Install/Win7/MakeAjSdkMsi.cmd
@@ -3,19 +3,32 @@ Rem @file
 Rem @brief  Make the .cab file for Win7 installation.
 
 Rem ****************************************************************************
-Rem  Copyright (c) 2013, AllSeen Alliance. All rights reserved.
-Rem 
-Rem     Permission to use, copy, modify, and/or distribute this software for any
-Rem     purpose with or without fee is hereby granted, provided that the above
-Rem     copyright notice and this permission notice appear in all copies.
-Rem 
-Rem     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-Rem     WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-Rem     MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-Rem     ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-Rem     WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-Rem     ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-Rem     OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+Rem     Copyright (c) Open Connectivity Foundation (OCF), AllJoyn Open Source
+Rem     Project (AJOSP) Contributors and others.
+Rem     
+Rem     SPDX-License-Identifier: Apache-2.0
+Rem     
+Rem     All rights reserved. This program and the accompanying materials are
+Rem     made available under the terms of the Apache License, Version 2.0
+Rem     which accompanies this distribution, and is available at
+Rem     http://www.apache.org/licenses/LICENSE-2.0
+Rem     
+Rem     Copyright (c) Open Connectivity Foundation and Contributors to AllSeen
+Rem     Alliance. All rights reserved.
+Rem     
+Rem     Permission to use, copy, modify, and/or distribute this software for
+Rem     any purpose with or without fee is hereby granted, provided that the
+Rem     above copyright notice and this permission notice appear in all
+Rem     copies.
+Rem     
+Rem     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+Rem     WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+Rem     WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+Rem     AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+Rem     DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+Rem     PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+Rem     TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+Rem     PERFORMANCE OF THIS SOFTWARE.
 Rem ****************************************************************************
 
 setlocal ENABLEDELAYEDEXPANSION 
