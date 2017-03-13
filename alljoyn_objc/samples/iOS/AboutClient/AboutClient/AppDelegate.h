6ffeb5682c03a93e6218657a2a14c563a785cf19
Olga Konoreva <olgak@affinegy.com>
2017-03-13 17:51:31 +0600

    ASACORE-3404 Update iOS AboutClient sample
    Recreated sample in current XCode version (8.1).
    Moved alljoyn initialization to AddDelegate.
    Removed needless adding interface description.
    Refactored visible messages.
    Fixed warnings, refactored code.
    
    ASACORE-3408 Update iOS DoorObserver sample
    
    Recreated sample in current XCode version (8.1).
    
    Signed-off-by: Olga Konoreva <olgak@affinegy.com>
    Change-Id: I851b8e27460dcacd5ffa35fe3ef3c6fb0a159eae

--- alljoyn_objc/samples/iOS/AboutClient/AboutClient/AppDelegate.h
+++ alljoyn_objc/samples/iOS/AboutClient/AboutClient/AppDelegate.h
@@ -1,22 +1,22 @@
 ////////////////////////////////////////////////////////////////////////////////
 //    Copyright (c) Open Connectivity Foundation (OCF), AllJoyn Open Source
 //    Project (AJOSP) Contributors and others.
-//    
+//
 //    SPDX-License-Identifier: Apache-2.0
-//    
+//
 //    All rights reserved. This program and the accompanying materials are
 //    made available under the terms of the Apache License, Version 2.0
 //    which accompanies this distribution, and is available at
 //    http://www.apache.org/licenses/LICENSE-2.0
-//    
+//
 //    Copyright (c) Open Connectivity Foundation and Contributors to AllSeen
 //    Alliance. All rights reserved.
-//    
+//
 //    Permission to use, copy, modify, and/or distribute this software for
 //    any purpose with or without fee is hereby granted, provided that the
 //    above copyright notice and this permission notice appear in all
 //    copies.
-//    
+//
 //    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
 //    WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
 //    WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
@@ -27,27 +27,12 @@
 //    PERFORMANCE OF THIS SOFTWARE.
 ////////////////////////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////////////////////////
-//
-//  BasicObject.m
-//
-////////////////////////////////////////////////////////////////////////////////
+#import <UIKit/UIKit.h>
 
-#import "BasicObject.h"
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-////////////////////////////////////////////////////////////////////////////////
-//
-//  Objective-C Bus Object implementation for BasicObject
-//
-////////////////////////////////////////////////////////////////////////////////
+@property (strong, nonatomic) UIWindow *window;
 
-@implementation BasicObject
-
-- (NSString*)concatenateString:(NSString*)str1 withString:(NSString*)str2
-{
-    return [str1 stringByAppendingString:str2];
-}
 
 @end
 
-////////////////////////////////////////////////////////////////////////////////
--- alljoyn_objc/samples/iOS/AboutClient/AboutClient/ViewController.m
+++ alljoyn_objc/samples/iOS/AboutClient/AboutClient/ViewController.m
@@ -1,22 +1,22 @@
 ////////////////////////////////////////////////////////////////////////////////
 //    Copyright (c) Open Connectivity Foundation (OCF), AllJoyn Open Source
 //    Project (AJOSP) Contributors and others.
-//    
+//
 //    SPDX-License-Identifier: Apache-2.0
-//    
+//
 //    All rights reserved. This program and the accompanying materials are
 //    made available under the terms of the Apache License, Version 2.0
 //    which accompanies this distribution, and is available at
 //    http://www.apache.org/licenses/LICENSE-2.0
-//    
+//
 //    Copyright (c) Open Connectivity Foundation and Contributors to AllSeen
 //    Alliance. All rights reserved.
-//    
+//
 //    Permission to use, copy, modify, and/or distribute this software for
 //    any purpose with or without fee is hereby granted, provided that the
 //    above copyright notice and this permission notice appear in all
 //    copies.
-//    
+//
 //    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
 //    WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
 //    WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
@@ -38,36 +38,29 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (IBAction)didTouchCallServiceButton:(id)sender
-{
     self.aboutClient = [[AboutClient alloc] init];
     self.aboutClient.delegate = self;
+}
+
+- (IBAction)didTouchCallServiceButton:(id)sender {
     [self.aboutClient sendHelloMessage];
 }
 
 - (void)didReceiveStatusUpdateMessage:(NSString *)message
 {
     dispatch_async(dispatch_get_main_queue(), ^{
+        NSLog(@"%@", message);
         NSMutableString *string = self.eventsTextView.text.length ? [self.eventsTextView.text mutableCopy] : [[NSMutableString alloc] init];
         NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
         [formatter setTimeStyle:NSDateFormatterMediumStyle];
         [formatter setDateStyle:NSDateFormatterShortStyle];
         [string appendFormat:@"[%@] ",[formatter stringFromDate:[NSDate date]]];
         [string appendString:message];
+        [string appendString:@"\n"];
         [self.eventsTextView setText:string];
-        NSLog(@"%@",string);
+        NSLog(@"%@", string);
     });
 }
 
