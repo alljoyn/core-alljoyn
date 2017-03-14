0f83ee7c08223394f914fdca4e9fe195e3b8f305
Eugene Komarov <eugenek@affinegy.com>
2017-03-14 12:42:55 +0600

    ASACORE-3404,3408 Update iOS AboutClient and DoorObserver sample
    ASACORE-3404 AboutClient
    
    Recreated sample in current XCode version (8.1).
    Moved alljoyn initialization to AddDelegate.
    Removed needless adding interface description.
    Refactored visible messages.
    Fixed warnings, refactored code.
    
    ASACORE-3408 DoorObserver
    
    Recreated sample in current XCode version (8.1).
    Move alljoyn part of sample to DoorObserverAllJoynService class.
    Set DoorObserverAllJoynService as data source for
    UITableView with proxy doors.
    
    Signed-off-by: Olga Konoreva <olgak@affinegy.com>
    Signed-off-by: Eugene Komarov <eugenek@affinegy.com>
    Change-Id: I851b8e27460dcacd5ffa35fe3ef3c6fb0a159eae

--- alljoyn_objc/samples/iOS/AboutClient/AboutClient/ViewController.h
+++ alljoyn_objc/samples/iOS/AboutClient/AboutClient/ViewController.h
@@ -1,22 +1,22 @@
 ////////////////////////////////////////////////////////////////////////////////
 //    Copyright (c) Open Connectivity Foundation (OCF), AllJoyn Open Source
 //    Project (AJOSP) Contributors and others.
-//    
+//
 //    SPDX-License-Identifier: Apache-2.0
-//    
+//
 //    All rights reserved. This program and the accompanying materials are
 //    made available under the terms of the Apache License, Version 2.0
 //    which accompanies this distribution, and is available at
 //    http://www.apache.org/licenses/LICENSE-2.0
-//    
+//
 //    Copyright (c) Open Connectivity Foundation and Contributors to AllSeen
 //    Alliance. All rights reserved.
-//    
+//
 //    Permission to use, copy, modify, and/or distribute this software for
 //    any purpose with or without fee is hereby granted, provided that the
 //    above copyright notice and this permission notice appear in all
 //    copies.
-//    
+//
 //    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
 //    WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
 //    WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
@@ -28,34 +28,11 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #import <UIKit/UIKit.h>
-#import <XCTest/XCTest.h>
 
-@interface DoorObserverTests : XCTestCase
+@interface ViewController : UIViewController
 
-@end
+@property (weak, nonatomic) IBOutlet UITextView *eventsTextView;
 
-@implementation DoorObserverTests
-
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
 
 @end
+
--- alljoyn_objc/samples/iOS/DoorObserver/DoorObserver/DoorObserverAllJoynService.h
+++ alljoyn_objc/samples/iOS/DoorObserver/DoorObserver/DoorObserverAllJoynService.h
@@ -1,22 +1,22 @@
 ////////////////////////////////////////////////////////////////////////////////
 //    Copyright (c) Open Connectivity Foundation (OCF), AllJoyn Open Source
 //    Project (AJOSP) Contributors and others.
-//    
+//
 //    SPDX-License-Identifier: Apache-2.0
-//    
+//
 //    All rights reserved. This program and the accompanying materials are
 //    made available under the terms of the Apache License, Version 2.0
 //    which accompanies this distribution, and is available at
 //    http://www.apache.org/licenses/LICENSE-2.0
-//    
+//
 //    Copyright (c) Open Connectivity Foundation and Contributors to AllSeen
 //    Alliance. All rights reserved.
-//    
+//
 //    Permission to use, copy, modify, and/or distribute this software for
 //    any purpose with or without fee is hereby granted, provided that the
 //    above copyright notice and this permission notice appear in all
 //    copies.
-//    
+//
 //    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
 //    WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
 //    WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
@@ -27,12 +27,27 @@
 //    PERFORMANCE OF THIS SOFTWARE.
 ////////////////////////////////////////////////////////////////////////////////
 
+#import <Foundation/Foundation.h>
+#import "alljoyn/Status.h"
+#import "DoorObject.h"
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+typedef enum {
+    STARTED,
+    STOPED,
+    UNDEFINED
+} ServiceState;
 
-@property (weak, nonatomic) IBOutlet UITextView *eventsTextView;
+@interface DoorObserverAllJoynService <UITableViewDataSource> : NSObject
 
-- (IBAction)didTouchCallServiceButton:(id)sender;
+@property (nonatomic, readonly) BOOL isServiceStarted;
+
+- (id)init;
+- (BOOL)startService;
+- (void)stopService;
+- (void)setTableView:(UITableView *) uiTableView;
+- (void)clearTableView;
+- (QStatus)produceDoorAtLocation:(NSString *)location;
+- (BOOL)clickOnProxyDoorAtIndex:(NSInteger)index;
 
 @end
