8beb949321c52b84f23db17baffa682fb52a95de
Olga Konoreva <olgak@affinegy.com>
2017-03-15 16:06:07 +0600

    ASACORE-3404, ASACORE-3405 ASACORE-3536 Update iOS About samples Remove sync JoinSession from callbacks in samples
    Recreated samples in current XCode version (8.1).
    Moved alljoyn initialization to AddDelegate.
    Removed needless adding interface description in Client.
    Removed unused establishing connection via well-known name.
    Removed parts of code copied from Basic samples.
    Refactored visible messages.
    Improved logic of client/service connection.
    Used async session joining instead of sync to avoid deadlock.
    Fixed warnings.
    Refactored code.
    
    Signed-off-by: Olga Konoreva <olgak@affinegy.com>
    Change-Id: I4f0c46b1c28df8c73d20131ec60c9e2535c384ca

---
+++ alljoyn_objc/samples/iOS/AboutClient/AboutClient/AppDelegate.m
@@ -0,0 +1,83 @@
+////////////////////////////////////////////////////////////////////////////////
+//    Copyright (c) Open Connectivity Foundation (OCF), AllJoyn Open Source
+//    Project (AJOSP) Contributors and others.
+//
+//    SPDX-License-Identifier: Apache-2.0
+//
+//    All rights reserved. This program and the accompanying materials are
+//    made available under the terms of the Apache License, Version 2.0
+//    which accompanies this distribution, and is available at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Copyright (c) Open Connectivity Foundation and Contributors to AllSeen
+//    Alliance. All rights reserved.
+//
+//    Permission to use, copy, modify, and/or distribute this software for
+//    any purpose with or without fee is hereby granted, provided that the
+//    above copyright notice and this permission notice appear in all
+//    copies.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+//    WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+//    WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+//    AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+//    DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+//    PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+//    TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+//    PERFORMANCE OF THIS SOFTWARE.
+////////////////////////////////////////////////////////////////////////////////
+
+#import "AppDelegate.h"
+#import "AJNInit.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    QStatus status = [AJNInit alljoynInit];
+    if (status == ER_OK) {
+        status = [AJNInit alljoynRouterInit];
+        if (status != ER_OK) {
+            NSLog(@"alljoynRouterInit fails with error: %u", status);
+            [AJNInit alljoynShutdown];
+        }
+    } else {
+        NSLog(@"alljoynInit fails with error: %u", status);
+    }
+    return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    [AJNInit alljoynRouterShutdown];
+    [AJNInit alljoynShutdown];
+}
+
+
+@end
