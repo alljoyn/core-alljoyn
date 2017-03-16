d9b036c0165a533db1d6583dd560d54c10245c3e
Eugene Komarov <eugenek@affinegy.com>
2017-03-16 16:08:23 +0600

    ASACORE-3552 Groom ObjC API changes
    Comment AJNBusObject registerInterfacesWithBus: completely.
    Return AJNApplicationStateListener state:appPublicKeyInfo:state:
    method, comment it as deprecated, add support for both methods
    (state:appPublicKeyInfo:state: and
    appStateChangedForRemoteBusAttachment:appPublicKeyInfo:state:)
    to AJNApplicationStateListenerImpl.
    Return AJNAboutData getSupportedLanguages:num: as deprecated.
    Fix getSupportedLanguages:num: implementation.
    
    Change-Id: Id6aadd7f9082c16b27e7505eb23869bdf0ad10d2
    Signed-off-by: Eugene Komarov <eugenek@affinegy.com>

--- alljoyn_objc/AllJoynFramework/AllJoynFramework/include/AJNAboutData.h
+++ alljoyn_objc/AllJoynFramework/AllJoynFramework/include/AJNAboutData.h
@@ -351,8 +351,21 @@
  *
  * The language is specified in a Locale using IETF language tags specified by the RFC 5646.
  *
+ * @return A NSArray with supported languages
+ *
  */
 - (NSArray<NSString *> *)getSupportedLanguages;
+
+/**
+ * Deprecated API. Use getSupportedLanguages: instead of this.
+ *
+ * Get and length of supported languages
+ *
+ * @return length of locals containing languageTags.
+ */
+
+//TODO: 17.10 annotate as deprecated
+- (size_t)getSupportedLanguages:(NSString**)languageTags num:(size_t)num;
 
 /**
  * Set the Description to the AboutData
