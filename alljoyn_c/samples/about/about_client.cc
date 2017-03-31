71fc0f103fdaa6809cdf353c234c37a93dbc8711
Krystian Zlomek <kzlomek@affinegy.com>
2017-03-31 18:08:42 +0200

    ASACORE-3533: Remove synchronous JoinSession from callbacks
    Also fixed memory leaks.
    
    Change-Id: I88131d41ce179aa6722689729ab69f378758a8e0
    Signed-off-by: Krystian Zlomek <kzlomek@affinegy.com>

--- alljoyn_c/samples/about/about_client.cc
+++ alljoyn_c/samples/about/about_client.cc
@@ -74,7 +74,7 @@ static void printAboutData(alljoyn_aboutdata aboutData, const char* language, in
         }
         printf("Key: %s", fields[i]);
 
-        alljoyn_msgarg tmp = alljoyn_msgarg_create();
+        alljoyn_msgarg tmp;
         alljoyn_aboutdata_getfield(aboutData, fields[i], &tmp, language);
         printf("\t");
         char signature[256] = { 0 };
@@ -85,7 +85,7 @@ static void printAboutData(alljoyn_aboutdata aboutData, const char* language, in
             printf("%s", tmp_s);
         } else if (!strncmp(signature, "as", 2)) {
             size_t las;
-            alljoyn_msgarg as_arg = alljoyn_msgarg_create();
+            alljoyn_msgarg as_arg;
             alljoyn_msgarg_get(tmp, "as", &las, &as_arg);
 
             for (size_t j = 0; j < las; ++j) {
@@ -111,7 +111,7 @@ static void printAboutData(alljoyn_aboutdata aboutData, const char* language, in
 typedef struct my_about_listener_t {
     alljoyn_sessionlistener sessionlistener;
     alljoyn_aboutlistener aboutlistener;
-}my_about_listener;
+} my_about_listener;
 
 static void AJ_CALL alljoyn_sessionlistener_connect_lost_cb(const void* context,
                                                             alljoyn_sessionid sessionId,
@@ -123,14 +123,170 @@ static void AJ_CALL alljoyn_sessionlistener_connect_lost_cb(const void* context,
 
 static alljoyn_sessionlistener create_my_alljoyn_sessionlistener()
 {
-    alljoyn_sessionlistener_callbacks* callbacks =
-        (alljoyn_sessionlistener_callbacks*)
-        malloc(sizeof(alljoyn_sessionlistener_callbacks));
-    callbacks->session_member_added = NULL;
-    callbacks->session_member_removed = NULL;
-    callbacks->session_lost = alljoyn_sessionlistener_connect_lost_cb;
+    alljoyn_sessionlistener_callbacks callbacks = {
+        &alljoyn_sessionlistener_connect_lost_cb,
+        NULL,
+        NULL
+    };
+    return alljoyn_sessionlistener_create(&callbacks, NULL);
+}
 
-    return alljoyn_sessionlistener_create(callbacks, NULL);
+struct about_metadata_t {
+    char* busName;
+    alljoyn_aboutobjectdescription objectDescription;
+};
+
+static void AJ_CALL print_about_info_cb(alljoyn_sessionid sessionId, about_metadata_t* about_metadata)
+{
+    QStatus status;
+    size_t path_num;
+    const char** paths;
+    alljoyn_aboutdata aboutData;
+    alljoyn_aboutproxy aboutProxy =
+        alljoyn_aboutproxy_create(g_bus, about_metadata->busName, sessionId);
+    alljoyn_msgarg objArg = alljoyn_msgarg_create();
+    /*
+     * alljoyn_busattachment_enableconcurrentcallbacks should be avoided as
+     * it may cause a deadlock.
+     * It is used here to not introduce synchronization mechanism required
+     * to be able to process about data without concurrent callbacks.
+     */
+    alljoyn_busattachment_enableconcurrentcallbacks(g_bus);
+    alljoyn_aboutproxy_getobjectdescription(aboutProxy, objArg);
+    printf("*********************************************************************************\n");
+    printf("AboutProxy.GetObjectDescription:\n");
+    alljoyn_aboutobjectdescription aod2 =
+        alljoyn_aboutobjectdescription_create();
+    alljoyn_aboutobjectdescription_createfrommsgarg(aod2, objArg);
+
+    path_num = alljoyn_aboutobjectdescription_getpaths(aod2, NULL, 0);
+    paths = (const char**) malloc(sizeof(const char*) * path_num);
+    alljoyn_aboutobjectdescription_getpaths(aod2, paths, path_num);
+
+    for (size_t i = 0; i < path_num; ++i) {
+        printf("\t\t%s\n", paths[i]);
+        size_t intf_num =
+            alljoyn_aboutobjectdescription_getinterfaces(aod2,
+                                                         paths[i],
+                                                         NULL, 0);
+        const char** intfs =
+            (const char**) malloc(sizeof(const char*) * intf_num);
+        alljoyn_aboutobjectdescription_getinterfaces(aod2, paths[i],
+                                                     intfs, intf_num);
+
+        for (size_t j = 0; j < intf_num; ++j) {
+            printf("\t\t\t%s\n", intfs[j]);
+        }
+        free((void*)intfs);
+        intfs = NULL;
+    }
+    free((void*) paths);
+    paths = NULL;
+
+    alljoyn_msgarg aArg = alljoyn_msgarg_create();
+    alljoyn_aboutproxy_getaboutdata(aboutProxy, "en", aArg);
+    printf("*********************************************************************************\n");
+    printf("AboutProxy.GetAboutData: (Default Language)\n");
+    aboutData = alljoyn_aboutdata_create("en");
+    alljoyn_aboutdata_createfrommsgarg(aboutData, aArg, "en");
+    printAboutData(aboutData, NULL, 1);
+    size_t lang_num =
+        alljoyn_aboutdata_getsupportedlanguages(aboutData, NULL, 0);
+    if (lang_num > 1) {
+        const char** langs =
+            (const char**) malloc(sizeof(char*) * lang_num);
+        alljoyn_aboutdata_getsupportedlanguages(aboutData,
+                                                langs,
+                                                lang_num);
+        char* defaultLanguage;
+        alljoyn_aboutdata_getdefaultlanguage(aboutData,
+                                             &defaultLanguage);
+        /*
+         * Print out the AboutData for every language but the
+         * default it has already been printed.
+         */
+        for (size_t i = 0; i < lang_num; ++i) {
+            if (strcmp(defaultLanguage, langs[i]) != 0) {
+                status = alljoyn_aboutproxy_getaboutdata(aboutProxy,
+                                                         langs[i],
+                                                         aArg);
+                if (ER_OK == status) {
+                    alljoyn_aboutdata_createfrommsgarg(aboutData,
+                                                       aArg,
+                                                       langs[i]);
+                    printAboutData(aboutData, langs[i], 1);
+                }
+            }
+        }
+        free((void*)langs);
+        langs = NULL;
+        uint16_t ver;
+        alljoyn_aboutproxy_getversion(aboutProxy, &ver);
+        printf("*********************************************************************************\n");
+        printf("AboutProxy.GetVersion %hd\n", ver);
+        printf("*********************************************************************************\n");
+
+        const char* path;
+        alljoyn_aboutobjectdescription_getinterfacepaths(about_metadata->objectDescription,
+                                                         INTERFACE_NAME,
+                                                         &path, 1);
+
+        alljoyn_proxybusobject proxyObject =
+            alljoyn_proxybusobject_create(g_bus, about_metadata->busName,
+                                          path, sessionId);
+
+        status =
+            alljoyn_proxybusobject_introspectremoteobject(proxyObject);
+
+        if (status != ER_OK) {
+            printf("Failed to introspect remote object.\n");
+        }
+        alljoyn_msgarg arg =
+            alljoyn_msgarg_create_and_set("s", "ECHO Echo echo...\n");
+        alljoyn_message replyMsg = alljoyn_message_create(g_bus);
+
+        alljoyn_proxybusobject_methodcall(proxyObject,
+                                          INTERFACE_NAME,
+                                          "Echo", arg,
+                                          1, replyMsg,
+                                          25000, 0);
+        if (status != ER_OK) {
+            printf("Failed to call Echo method.\n");
+            return;
+        }
+
+        char* echoReply;
+        alljoyn_msgarg reply_msgarg =
+            alljoyn_message_getarg(replyMsg, 0);
+        status = alljoyn_msgarg_get(reply_msgarg, "s", &echoReply);
+        if (status != ER_OK) {
+            printf("Failed to read Echo method reply.\n");
+        }
+        printf("Echo method reply: %s\n", echoReply);
+        alljoyn_message_destroy(replyMsg);
+        alljoyn_msgarg_destroy(arg);
+        alljoyn_proxybusobject_destroy(proxyObject);
+    }
+
+    alljoyn_aboutdata_destroy(aboutData);
+    alljoyn_msgarg_destroy(aArg);
+    alljoyn_aboutobjectdescription_destroy(aod2);
+    alljoyn_msgarg_destroy(objArg);
+    alljoyn_aboutproxy_destroy(aboutProxy);
+    alljoyn_aboutobjectdescription_destroy(about_metadata->objectDescription);
+    free(about_metadata->busName);
+    free(about_metadata);
+}
+
+void AJ_CALL my_joinsessioncb(QStatus status, alljoyn_sessionid sessionId, const alljoyn_sessionopts opts, void* context)
+{
+    QCC_UNUSED(opts);
+    if (ER_OK == status) {
+        printf("JoinSession SUCCESS (Session id=%u).\n", sessionId);
+        print_about_info_cb(sessionId, (about_metadata_t*)context);
+    } else {
+        printf("JoinSession failed (status=%s).\n", QCC_StatusText(status));
+    }
 }
 
 static void AJ_CALL announced_cb(const void* context,
@@ -178,172 +334,53 @@ static void AJ_CALL announced_cb(const void* context,
     printf("*********************************************************************************\n");
     QStatus status;
     if (g_bus != NULL) {
-        alljoyn_sessionid sessionId;
         alljoyn_sessionopts sessionOpts =
             alljoyn_sessionopts_create(ALLJOYN_TRAFFIC_TYPE_MESSAGES,
                                        QCC_FALSE, ALLJOYN_PROXIMITY_ANY,
                                        ALLJOYN_TRANSPORT_ANY);
+        if (NULL != sessionOpts) {
+            about_metadata_t* about_metadata = (about_metadata_t*) malloc(sizeof(about_metadata_t));
+            size_t len = strlen(busName);
+            about_metadata->busName = (char*) malloc(len);
+            strcpy(about_metadata->busName, busName);
+            about_metadata->objectDescription = objectDescription;
+            printf("calling alljoyn_busattachment_joinsessionasync...\n");
+            status = alljoyn_busattachment_joinsessionasync(g_bus, busName, port, mylistener->sessionlistener, sessionOpts, &my_joinsessioncb, about_metadata);
 
-        alljoyn_busattachment_enableconcurrentcallbacks(g_bus);
-        status =
-            alljoyn_busattachment_joinsession(g_bus, busName, port,
-                                              mylistener->sessionlistener,
-                                              &sessionId, sessionOpts);
-        if (ER_OK == status && 0 != sessionId) {
-            alljoyn_aboutproxy aboutProxy =
-                alljoyn_aboutproxy_create(g_bus, busName, sessionId);
-            alljoyn_msgarg objArg = alljoyn_msgarg_create();
-            alljoyn_aboutproxy_getobjectdescription(aboutProxy, objArg);
-            printf("*********************************************************************************\n");
-            printf("AboutProxy.GetObjectDescription:\n");
-            alljoyn_aboutobjectdescription aod2 =
-                alljoyn_aboutobjectdescription_create();
-            alljoyn_aboutobjectdescription_createfrommsgarg(aod2, objArg);
-
-            path_num = alljoyn_aboutobjectdescription_getpaths(aod2, NULL, 0);
-            paths = (const char**) malloc(sizeof(const char*) * path_num);
-            alljoyn_aboutobjectdescription_getpaths(aod2, paths, path_num);
-
-            for (size_t i = 0; i < path_num; ++i) {
-                printf("\t\t%s\n", paths[i]);
-                size_t intf_num =
-                    alljoyn_aboutobjectdescription_getinterfaces(aod2,
-                                                                 paths[i],
-                                                                 NULL, 0);
-                const char** intfs =
-                    (const char**) malloc(sizeof(const char*) * intf_num);
-                alljoyn_aboutobjectdescription_getinterfaces(aod2, paths[i],
-                                                             intfs, intf_num);
-
-                for (size_t j = 0; j < intf_num; ++j) {
-                    printf("\t\t\t%s\n", intfs[j]);
-                }
-                free((void*)intfs);
-                intfs = NULL;
+            if (ER_OK != status) {
+                printf("alljoyn_busattachment_joinsessionasync failed (status=%s)\n", QCC_StatusText(status));
+                free(about_metadata->busName);
+                free(about_metadata);
             }
-            free((void*) paths);
-            paths = NULL;
-
-            alljoyn_msgarg aArg = alljoyn_msgarg_create();
-            alljoyn_aboutproxy_getaboutdata(aboutProxy, "en", aArg);
-            printf("*********************************************************************************\n");
-            printf("AboutProxy.GetAboutData: (Default Language)\n");
-            aboutData = alljoyn_aboutdata_create("en");
-            alljoyn_aboutdata_createfrommsgarg(aboutData, aArg, "en");
-            printAboutData(aboutData, NULL, 1);
-            size_t lang_num =
-                alljoyn_aboutdata_getsupportedlanguages(aboutData, NULL, 0);
-            if (lang_num > 1) {
-                const char** langs =
-                    (const char**) malloc(sizeof(char*) * lang_num);
-                alljoyn_aboutdata_getsupportedlanguages(aboutData,
-                                                        langs,
-                                                        lang_num);
-                char* defaultLanguage;
-                alljoyn_aboutdata_getdefaultlanguage(aboutData,
-                                                     &defaultLanguage);
-                /*
-                 * Print out the AboutData for every language but the
-                 * default it has already been printed.
-                 */
-                for (size_t i = 0; i < lang_num; ++i) {
-                    if (strcmp(defaultLanguage, langs[i]) != 0) {
-                        status = alljoyn_aboutproxy_getaboutdata(aboutProxy,
-                                                                 langs[i],
-                                                                 aArg);
-                        if (ER_OK == status) {
-                            alljoyn_aboutdata_createfrommsgarg(aboutData,
-                                                               aArg,
-                                                               langs[i]);
-                            printAboutData(aboutData, langs[i], 1);
-                        }
-                    }
-                }
-                free((void*)langs);
-                langs = NULL;
-                uint16_t ver;
-                alljoyn_aboutproxy_getversion(aboutProxy, &ver);
-                printf("*********************************************************************************\n");
-                printf("AboutProxy.GetVersion %hd\n", ver);
-                printf("*********************************************************************************\n");
-
-                const char* path;
-                alljoyn_aboutobjectdescription_getinterfacepaths(objectDescription,
-                                                                 INTERFACE_NAME,
-                                                                 &path, 1);
-
-                alljoyn_proxybusobject proxyObject =
-                    alljoyn_proxybusobject_create(g_bus, busName,
-                                                  path, sessionId);
-
-                status =
-                    alljoyn_proxybusobject_introspectremoteobject(proxyObject);
-
-                if (status != ER_OK) {
-                    printf("Failed to introspect remote object.\n");
-                }
-                alljoyn_msgarg arg =
-                    alljoyn_msgarg_create_and_set("s", "ECHO Echo echo...\n");
-                alljoyn_message replyMsg = alljoyn_message_create(g_bus);
-
-                alljoyn_proxybusobject_methodcall(proxyObject,
-                                                  INTERFACE_NAME,
-                                                  "Echo", arg,
-                                                  1, replyMsg,
-                                                  25000, 0);
-                if (status != ER_OK) {
-                    printf("Failed to call Echo method.\n");
-                    return;
-                }
-
-                char* echoReply;
-                alljoyn_msgarg reply_msgarg =
-                    alljoyn_message_getarg(replyMsg, 0);
-                status = alljoyn_msgarg_get(reply_msgarg, "s", &echoReply);
-                if (status != ER_OK) {
-                    printf("Failed to read Echo method reply.\n");
-                }
-                printf("Echo method reply: %s\n", echoReply);
-                alljoyn_message_destroy(replyMsg);
-                alljoyn_msgarg_destroy(arg);
-                alljoyn_proxybusobject_destroy(proxyObject);
-            }
-
-            alljoyn_msgarg_destroy(aArg);
-            alljoyn_aboutobjectdescription_destroy(aod2);
-            alljoyn_msgarg_destroy(objArg);
-            alljoyn_aboutproxy_destroy(aboutProxy);
+            alljoyn_sessionopts_destroy(sessionOpts);
         }
-        alljoyn_sessionopts_destroy(sessionOpts);
     } else {
         printf("BusAttachment is NULL\n");
     }
     alljoyn_aboutdata_destroy(aboutData);
     alljoyn_aboutobjectdescription_destroy(aod);
-    alljoyn_aboutobjectdescription_destroy(objectDescription);
 }
 
 static my_about_listener* create_my_alljoyn_aboutlistener()
 {
     my_about_listener* result =
         (my_about_listener*) malloc(sizeof(my_about_listener));
-    alljoyn_aboutlistener_callback* callback =
-        (alljoyn_aboutlistener_callback*)
-        malloc(sizeof(alljoyn_aboutlistener_callback));
-    callback->about_listener_announced = announced_cb;
-    result->aboutlistener = alljoyn_aboutlistener_create(callback, result);
+    alljoyn_aboutlistener_callback callbacks = {
+        &announced_cb
+    };
+    result->aboutlistener = alljoyn_aboutlistener_create(&callbacks, result);
     result->sessionlistener = create_my_alljoyn_sessionlistener();
     return result;
 }
 
 static void destroy_my_alljoyn_aboutlistener(my_about_listener* listener)
 {
-    if (listener) {
-        if (listener->aboutlistener) {
+    if (listener != NULL) {
+        if (listener->aboutlistener != NULL) {
             alljoyn_aboutlistener_destroy(listener->aboutlistener);
             listener->aboutlistener = NULL;
         }
-        if (listener->sessionlistener) {
+        if (listener->sessionlistener != NULL) {
             alljoyn_sessionlistener_destroy(listener->sessionlistener);
             listener->sessionlistener = NULL;
         }
@@ -417,11 +454,10 @@ int CDECL_CALL main(void)
         }
     }
 
+    alljoyn_busattachment_unregisteraboutlistener(g_bus, listener->aboutlistener);
     destroy_my_alljoyn_aboutlistener(listener);
-
     alljoyn_busattachment_stop(g_bus);
     alljoyn_busattachment_join(g_bus);
-
     alljoyn_busattachment_destroy(g_bus);
 
 #ifdef ROUTER
